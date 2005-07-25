597a40d24d1dc4069e8417ea34ec28213d47e8aa
Michael Gauthier <mike@silverorange.com>
2005-07-25 20:03:55 +0000

    Made this extend SwatDBDataObject.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@1174 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreCustomer.php
+++ Store/StoreCustomer.php
@@ -1,60 +1,64 @@
 <?php
 
+require_once('SwatDB/SwatDB.php');
+require_once('SwatDB/SwatDBDataObject.php');
+
 /**
  * Do database loading in StoreDB::loadCustomer($customerId) method
  * Do database saving in  StoreDB::saveCustomer($customer) method
  *
  * contains data like name. Can have multiple StoreAddress objects.
  */
-class StoreCustomer {
+class StoreCustomer extends SwatDBDataObject
+{
 
 	/**
 	 * An array of StoreAddress objects. The array is associative of the form
 	 *     id => StoreAddress
 	 *
-	 * @var arrray
+	 * @arrray
 	 * @access private
 	 */
-	private var $addresses;
+	private $addresses;
 	
-	public var $id;
-	public var $fullname;
-	public var $email;
-	public var $phone;
-	public var $emailupdate;
+	public $id;
+	public $fullname;
+	public $email;
+	public $phone;
+	public $emailupdate;
 
 	/**
-	 * @var Date
+	 * @Date
 	 * @access public
 	 */
-	public var $createdate;
+	public $createdate;
 	
 	/**
-	 * @var Date
+	 * @Date
 	 * @access public
 	 */
-	public var $lastlogin;
-	public var $simple;
+	public $lastlogin;
+	public $simple;
 
-	public function save() {
-		StoreCustomerWrapper::saveToDB($this);
+
+	private $db_field_blacklist = array('addresses');
+
+	/**
+	 * loads a StoreCustomer from the database into this object
+	 */
+	public function loadFromDB($id)
+	{
+		$fields = array_diff(array_keys($this->getProperties()), $this->db_field_blacklist);
+		$values = SwatDB::queryRow($this->app->db, 'customers', $fields, 'customer_id', $id);
+		$this->setValues($values);
+		$this->generatePropertyHashes();
+
+		// TODO: load complex properties here (like $addresses)
+	}
+
+	public function saveToDB()
+	{
 	}
 }
-
-class StoreCustomerWrapper {
-
-	/**
-	 * returns a StoreCustomer object loaded from the database
-	 */
-	public static function loadFromDB($id);
-
-	
-	public static function saveToDB($customer);
-}
-
-$customer = StoreCustomerWrapper::loadFromDB(1234);
-$customer->name = 'smith';
-$customer->save();
-//StoreCustomerWrapper::saveToDB($customer);
 
 ?>
