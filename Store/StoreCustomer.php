27289c6f25ac811d4bc3db8bc8944235799a366c
Michael Gauthier <mike@silverorange.com>
2005-08-01 14:31:15 +0000

    Clean up and documentation
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@1204 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreCustomer.php
+++ Store/StoreCustomer.php
@@ -1,26 +1,35 @@
 <?php
 
-require_once('SwatDB/SwatDB.php');
-require_once('SwatDB/SwatDBDataObject.php');
+require_once 'SwatDB/SwatDB.php';
+require_once 'SwatDB/SwatDBDataObject.php';
 
 /**
- * Do database loading in StoreDB::loadCustomer($customerId) method
- * Do database saving in  StoreDB::saveCustomer($customer) method
+ * A customer for an e-commerce web application
  *
- * contains data like name. Can have multiple StoreAddress objects.
+ * StoreCustomer objects contain data like name and email that correspond
+ * directly to database fields. StoreCustomer objects can have multiple
+ * StoreAddress objects.
+ *
+ * There are three typical ways to use a StoreCustomer object:
+ *
+ * 1. Create a new StoreCustomer object with a blank constructor. Modify some
+ *    properties of the customer object and call the saveToDB() method. A new
+ *    row is inserted into the database.
+ * 2. Create a new StoreCustomer object with a blank constructor. Call the
+ *    loadFromDB() method on the object instance passing in a database id.
+ *    Modify some properties and call the saveToDB() method. The modified 
+ *    properties are updated in the database.
+ * 3. Create a new StoreCustomer object passing a record set into the
+ *    constructor. The first row of the record set will be loaded as the data
+ *    for the object instance. Modify some properties and call the saveToDB()
+ *    method. The modified properties are updated in the database.
+ *
+ * @package   Store
+ * @copyright 2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreCustomer extends SwatDBDataObject
 {
-
-	/**
-	 * An array of StoreAddress objects. The array is associative of the form
-	 *     id => StoreAddress
-	 *
-	 * @arrray
-	 * @access private
-	 */
-	private $addresses;
-	
 	public $id;
 	public $fullname;
 	public $email;
@@ -28,23 +37,32 @@ class StoreCustomer extends SwatDBDataObject
 	public $emailupdate;
 
 	/**
-	 * @Date
-	 * @access public
+	 * @var Date
 	 */
 	public $createdate;
 	
 	/**
-	 * @Date
-	 * @access public
+	 * @var Date
 	 */
 	public $lastlogin;
 	public $simple;
 
-
+	/**
+	 * An array of StoreAddress objects. The array is associative of the form
+	 *     id => StoreAddress
+	 *
+	 * @var arrray
+	 */
+	private $addresses;
 	private $db_field_blacklist = array('addresses');
 
 	/**
-	 * loads a StoreCustomer from the database into this object
+	 * Loads a customer from the database into this object
+	 *
+	 * @param integer $id the database id of the customer to load.
+	 *
+	 * @return boolean true if the customer was found in the database and false
+	 *                  if the customer was not found in the database.
 	 */
 	public function loadFromDB($id)
 	{
