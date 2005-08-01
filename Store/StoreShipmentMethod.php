bf1752bb887338a6735eac257e417a5fd7c9c292
Michael Gauthier <mike@silverorange.com>
2005-08-01 15:43:03 +0000

    Split this into its own file and update documentation.
    svn commit r1218

---
+++ Store/StoreShipmentMethod.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * A shipment method for an e-commerce web application
+ *
+ * @package   Store
+ * @copyright 2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class StoreShipmentMethod
+{
+	/**
+	 * An approximation of how long it takes to ship items with this method.
+	 *
+	 * @return Date_Span the approximate time it will take to deliver a
+	 *                    shipment using this method.
+	 */
+	public function getTimeToDeliver($address)
+	{
+	}
+}
+
+?>
