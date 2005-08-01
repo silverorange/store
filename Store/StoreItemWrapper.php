612ce71b7c996d6fed940fd86b0c1d8864be405d
Michael Gauthier <mike@silverorange.com>
2005-08-01 15:06:35 +0000

    Documentation and cleanup.
    svn commit r1210

--- Store/StoreItemWrapper.php
+++ Store/StoreItemWrapper.php
@@ -1,44 +1,78 @@
 <?php
 
-class StoreItemWrapper extends DBWrapper {
+/**
+ * A utility class that loads StoreItem objects from the database
+ *
+ * If there are many StoreItem objects that must be loaded for a page request,
+ * this class should be used to load the objects.
+ *
+ * The typical usage of this object is:
+ *
+ *  $item_wrapper = new StoreItemWrapper($rs);
+ *  while ($item = $item_wrapper->getNextItem()) {
+ *      // do something with each item object here ...
+ *  }
+ *
+ * @package   Store
+ * @copyright 2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class StoreItemWrapper extends DBWrapper
+{
 	/**
-	 * An array of StoreItems this wrapper returns
+	 * An array of StoreItem objects this wrapper returns
 	 *
 	 * @var array
-	 * @access private
 	 */
-	private var $items;
+	private $items;
 
 	/**
 	 * An internal array pointer pointing to the current array element
 	 *
-	 * @var int
+	 * @var integer
 	 */
-	private var $current_item = 0;
-	
+	private $current_item = 0;
+
 	/**
 	 * Creates a new wrapper object
 	 *
-	 * The constructor here takes the result set of a MDB2 query and translates
+	 * The constructor takes the result set of a MDB2 query and translates
 	 * it into an array of objects.
 	 *
-	 * @param $rs resource the result set of the MDB2 query to get the items.
+	 * @param resource $rs the record set of the MDB2 query to get the items.
 	 *
 	 * @access public
 	 */
-	public function __construct($rs);
+	public function __construct($rs)
+	{
+	}
 
 	/**
 	 * Gets the next item from the items array
 	 *
 	 * @return the next item in the array, or null if there are no more items.
 	 */
-	public function getNextItem();
+	public function getNextItem()
+	{
+	}
 	
-	public function isEmpty();
+	/**
+	 * Returns whether or not this wrapper contains any items
+	 *
+	 * @return boolean
+	 */
+	public function isEmpty()
+	{
+	}
 
-	public function getItemCount();
-
+	/**
+	 * Gets the number of items this wrapper contains
+	 *
+	 * @return integer the number of items this wrapper contains.
+	 */
+	public function getItemCount()
+	{
+	}
 }
 
 ?>
