81d273cc60eed3f52b2ee16fdb0d7d94ec55a21c
Michael Gauthier <mike@silverorange.com>
2005-08-02 17:15:19 +0000

    Add class level docblock tags.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@1263 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreCartEntryNotFoundException.php
+++ Store/StoreCartEntryNotFoundException.php
@@ -2,6 +2,10 @@
 
 /**
  * Thrown when a StoreCartEntry lookup in the StoreCart class fails
+ *
+ * @package   Store
+ * @copyright 2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreCartEntryNotFoundException
 {
