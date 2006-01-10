069f994def627472fd2e761e3fb266bb695d0673
Nathan Fredrickson <nathan@silverorange.com>
2006-01-10 21:36:57 +0000

    add a column and cellrenderer for quantity entry/display in a table view of items
    svn commit r2890

---
+++ Store/StoreItemViewQuantityColumn.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once('Store/StoreQuantityCellRenderer.php');
+require_once('Swat/SwatTableViewColumn.php');
+require_once('Swat/SwatTableViewCheckAllRow.php');
+require_once('Swat/SwatHtmlTag.php');
+
+/**
+ * A quantity entry column.
+ *
+ * @package   Store
+ * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class StoreItemViewQuantityColumn extends SwatTableViewColumn
+{
+	private $items = null;
+
+	private $quantity_renderer = null;
+
+	public function init()
+	{
+		// TODO: autogenerate an id here
+		if ($this->id === null)
+			$this->id == 'quantity';
+
+		/*
+		if ($this->view !== null)
+			$this->view->addJavaScript(
+				'store/javascript/store-item-view-quantity-column.js');
+		*/
+	}
+
+	public function process()
+	{
+		$name = $this->getRendererName();
+
+		if (isset($_POST[$name]) && is_array($_POST[$name]))
+			$this->items = $_POST[$name];
+	}
+
+	public function getItems()
+	{
+		return $this->items;
+	}
+
+	private function getRendererName()
+	{
+		$renderer = $this->getQuantityRenderer();
+
+		return $renderer->id;
+	}
+
+	private function getQuantityRenderer()
+	{
+		foreach ($this->renderers as $renderer) 
+			if ($renderer instanceof StoreQuantityCellRenderer)
+				return $renderer;
+
+		throw new SwatException("The column '{$this->id}' must contain a ".
+			'quantity cell renderer.');
+	}
+}
+
+?>
---
+++ Store/StoreQuantityCellRenderer.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once 'Swat/SwatCellRenderer.php';
+
+/**
+ * A renderer for a column of quantity input fields.
+ *
+ * @package   Store
+ * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class StoreQuantityCellRenderer extends SwatCellRenderer
+{
+	/**
+	 * Id
+	 *
+	 * The name attribute in the HTML input tag.
+	 *
+	 * @var string
+	 */
+	public $id;
+
+	/**
+	 * Value
+	 *
+	 * The value attribute in the HTML input tag.
+	 *
+	 * @var string
+	 */
+	public $value;
+
+	public function render()
+	{
+		$input_tag = new SwatHtmlTag('input');
+		$input_tag->type = 'text';
+		$input_tag->name = $this->id.'[]';
+		$input_tag->value = $this->value;
+
+		if (isset($_POST[$this->id]))
+			if (in_array($this->value, $_POST[$this->id]))
+				$input_tag->checked = 'checked';
+
+		$input_tag->display();
+	}
+
+	/**
+	 * Gets TD-tag attributes
+	 *
+	 * @return array an array of attributes to apply to the TD tag of this cell
+	 *                renderer.
+	 *
+	 * @see SwatCellRenderer::getTdAttributes()
+	 */
+	public function getTdAttributes()
+	{
+		return array('class' => 'store-quantity-input-cell-renderer');
+	}
+}
+
+?>
