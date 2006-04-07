9a49926333829d4f0bf8e4afc83d9b435b58de67
Michael Gauthier <mike@silverorange.com>
2006-04-07 21:33:07 +0000

    Declare class as abstract.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@4621 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreCartModule.php
+++ Store/StoreCartModule.php
@@ -13,7 +13,7 @@ require_once 'Store/dataobjects/StoreCartEntryWrapper.php';
  * @copyright 2005 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class StoreCartModule extends SwatApplicationModule
+abstract class StoreCartModule extends SwatApplicationModule
 {
     // {{{ public function init()
 
