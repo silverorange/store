a1f1578fda2135e824590d812f582d8f52ce2d4f
Nathan Fredrickson <nathan@silverorange.com>
2006-05-05 16:07:03 +0000

    Add a bunch of methods
    svn commit r5393

--- Store/StoreSessionModule.php
+++ Store/StoreSessionModule.php
@@ -19,7 +19,74 @@ class StoreSessionModule extends SiteApplicationModule
 		session_cache_limiter('');
 		session_save_path('/so/phpsessions/'.$this->app->id);
 		session_name($this->app->id);
+	}
+
+    // }}}
+    // {{{ public function activate()
+
+	public function activate()
+	{
 		session_start();
+
+		if (!isset($_SESSION['account_id']))
+			$_SESSION['account_id'] = 0;
+	}
+
+    // }}}
+    // {{{ public function isLoggedIn()
+
+	/**
+	 * Check the user's logged-in status
+	 * @return bool True if user is logged in. 
+	 */
+	public function isLoggedIn()
+	{
+		if (isset($_SESSION['account_id']))
+			return ($_SESSION['account_id'] != 0);
+
+		return false;
+	}
+
+    // }}}
+    // {{{ public function isActive()
+
+	/**
+	 * Check if there is an active session
+	 * @return bool True if session is active. 
+	 */
+	public function isActive()
+	{
+		return isset($_SESSION);
+	}
+
+    // }}}
+    // {{{ public function getAccountID()
+
+	/**
+	 * Retrieve the current account ID
+	 * @return integer current account ID, or null if not logged in.
+	 */
+	public function getAccountID()
+	{
+		if (!$this->isLoggedIn())
+			return null;
+
+		return $_SESSION['account_id'];
+	}
+
+    // }}}
+    // {{{ public function getSessionID()
+
+	/**
+	 * Retrieve the current session ID
+	 * @return integer current session ID, or null if no active session.
+	 */
+	public function getSessionID()
+	{
+		if (!$this->isActive())
+			return null;
+
+		return session_id();
 	}
 
     // }}}
