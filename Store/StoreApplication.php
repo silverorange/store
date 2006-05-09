54c7c00db8afc186bedbde646971a58497a20e8f
Nathan Fredrickson <nathan@silverorange.com>
2006-05-09 16:20:35 +0000

    Session aware relocate() method.
    svn commit r5507

--- Store/StoreApplication.php
+++ Store/StoreApplication.php
@@ -106,8 +106,6 @@ abstract class StoreApplication extends SiteApplication
 	}
 
 	// }}}
-
-	// }}}
 	// {{{ public function replacePage()
 
 	/**
@@ -121,6 +119,34 @@ abstract class StoreApplication extends SiteApplication
 	{
 		$new_page = $this->instantiatePage($source);
 		$this->setPage($new_page);
+	}
+
+	// }}}
+	// {{{ public function relocate()
+
+	/**
+	 * Relocates to another URI
+	 *
+	 * Appends the session identifier (SID) to the URL if necessary.
+	 * Then call parent method Site::relocate to do the actual relocating.
+	 * This function does not return and in fact calls the PHP exit()
+	 * function just to be sure execution does not continue.
+	 *
+	 * @param string $uri the URI to relocate to.
+	 * @param boolean $append_sid whether to append the SID to the URI
+	 */
+	public function relocate($uri, $append_sid = true)
+	{
+		if ($append_sid && $this->session->isActive()) {
+			if (strpos($url, '?') === FALSE)
+				$uri.= '?';
+			else
+				$uri.= '&';
+
+			$uri.= sprintf('%s=%s', session_name(), session_id());
+		}
+
+		parent::relocate($uri);
 	}
 
 	// }}}
