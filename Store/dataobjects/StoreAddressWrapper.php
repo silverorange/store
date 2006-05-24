c111c4522ccf2e1ec4f7b84f0acfaa4d0756545d
Nick Burka <nick@silverorange.com>
2006-05-24 16:44:47 +0000

    Add a wrapper class for Store Addresses
    svn commit r6020

---
+++ Store/dataobjects/StoreAddressWrapper.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'SwatDB/SwatDBRecordsetWrapper.php';
+require_once 'Store/dataobjects/StoreCartEntry.php';
+
+/**
+ * A recordset wrapper class for StoreAddress objects
+ *
+ * This class contains cart functionality common to all sites. It is typically
+ * extended on a per-site basis.
+ *
+ * @package   Store
+ * @copyright 2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class StoreAddressWrapper extends SwatDBRecordsetWrapper
+{
+	// {{{ protected function init()
+
+	protected function init()
+	{
+		parent::init();
+		$this->row_wrapper_class = 'StoreAddress';
+	}
+
+	// }}}
+}
+
+?>
