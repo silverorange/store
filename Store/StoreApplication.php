812db36215af4564e7ad5847e26d046cde2d30ea
Nathan Fredrickson <nathan@silverorange.com>
2006-05-26 22:46:37 +0000

    Major refactoring of the site framework across all packages.  This is still a bit rough, but both the Veseys frontend and the admin are running on it.  While the Admin package was changed heavily to work with the new Site package, its interface did not change much so admin site code requires minimal changes to work.
    - Go upstream! Site package does more now.
    - Remove duplicated functionality form Store and Admin packages.
    - SiteLayout and subclasses are now responsible for the "site trim".
    - HtmlHeadEntry API moved to SiteLayout and now used by Admin too.
    - Single point of page sub-classing from StorePage to VeseysPage is gone allowing other base page subclasses in Store.
    - The *Internal() methods are gone from Store, init()/proccess()/build() are now sub-classed directly.
    
    
    
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@6169 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreApplication.php
+++ Store/StoreApplication.php
@@ -4,9 +4,6 @@ require_once 'Site/SiteApplication.php';
 require_once 'Site/SiteDatabaseModule.php';
 require_once 'Site/SiteServerConfigModule.php';
 require_once 'Store/StoreSessionModule.php';
-require_once 'Store/StoreCartModule.php';
-require_once 'Store/exceptions/StoreNotFoundException.php';
-require_once 'MDB2.php';
 
 /**
  * Web application class for a store
@@ -20,82 +17,14 @@ abstract class StoreApplication extends SiteApplication
 
 	public $db;
 
-	public $exception_page_source = 'exception';
-
 	// }}}
-	// {{{ public function init()
+	// {{{ protected function initModules()
 
-	/**
-	 * Initialize the application.
-	 */
-	public function init()
+	protected function initModules()
 	{
-		$this->initBaseHref(3);
-		$this->initModules();
-
+		parent::initModules();
 		// set up convenience references
 		$this->db = $this->database->getConnection();
-
-		try {
-			$this->initPage();
-		}
-		catch (StoreException $e) {
-			$this->replacePage($this->exception_page_source);
-			$this->page->setException($e);
-			$this->initPage();
-		}
-	}
-
-	// }}}
-	// {{{ public function run()
-
-	/**
-	 * Run the application.
-	 */
-	public function run()
-	{
-		try {
-			$this->getPage()->process();
-			$this->getPage()->build();
-		}
-		catch (StoreException $e) {
-			$this->replacePage($this->exception_page_source);
-			$this->page->setException($e);
-			$this->page->build();
-		}
-
-		$this->getPage()->layout->display();
-	}
-
-	// }}}
-	// {{{ public function resolvePage()
-
-	/**
-	 * Get the page object.
-	 * Uses the $_GET variables to decide which page subclass to instantiate.
-	 * @return SitePage A subclass of SitePage is returned.
-	 */
-	public function resolvePage()
-	{
-		$source = self::initVar('source');
-		$page = $this->instantiatePage($source);
-		return $page;
-	}
-
-	// }}}
-	// {{{ public function replacePage()
-
-	/**
-	 * Replace the page object
-	 *
-	 * This method can be used to load another page to replace the current 
-	 * page. For example, this is used to load a confirmation page when 
-	 * processing an admin index page.
-	 */
-	public function replacePage($source)
-	{
-		$new_page = $this->instantiatePage($source);
-		$this->setPage($new_page);
 	}
 
 	// }}}
