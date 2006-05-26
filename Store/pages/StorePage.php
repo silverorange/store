e6c8da1a8ac9fe2bfed00528100105e51ad8f585
Nathan Fredrickson <nathan@silverorange.com>
2006-05-26 22:46:37 +0000

    Major refactoring of the site framework across all packages.  This is still a bit rough, but both the Veseys frontend and the admin are running on it.  While the Admin package was changed heavily to work with the new Site package, its interface did not change much so admin site code requires minimal changes to work.
    - Go upstream! Site package does more now.
    - Remove duplicated functionality form Store and Admin packages.
    - SiteLayout and subclasses are now responsible for the "site trim".
    - HtmlHeadEntry API moved to SiteLayout and now used by Admin too.
    - Single point of page sub-classing from StorePage to VeseysPage is gone allowing other base page subclasses in Store.
    - The *Internal() methods are gone from Store, init()/proccess()/build() are now sub-classed directly.
    
    
    
    svn commit r6169

---
+++ Store/pages/StorePage.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('Site/pages/SitePage.php');
+
+/**
+ * @package   Store
+ * @copyright 2005 silverorange
+ */
+abstract class StorePage extends SitePage
+{
+	public $found = false;
+
+}
+
+?>
