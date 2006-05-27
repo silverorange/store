51a57d1efa40973d5a08d06f68dbc8ad2187172d
Nathan Fredrickson <nathan@silverorange.com>
2006-05-27 18:32:55 +0000

    Refactor payment method/type dataobjects as discussed with Mike & Nick.
    svn commit r6194

---
+++ Store/dataobjects/StorePaymentType.php
@@ -0,0 +1,83 @@
+<?php
+
+require_once 'SwatDB/SwatDBDataObject.php';
+
+/**
+ * A payment type data object
+ *
+ * @package   Store
+ * @copyright 2006 silverorange
+ */
+class StorePaymentType extends SwatDBDataObject
+{
+	// {{{ public properties
+
+	/**
+	 * 
+	 *
+	 * @var string 
+	 */
+	public $id;
+
+	/**
+	 * Non-visible string indentifier
+	 *
+	 * @var string
+	 */
+	public $shortname;
+
+	/**
+	 * Whether this payment type is available
+	 *
+	 * @var boolean
+	 */
+	public $enabled;
+
+	/**
+	 * User visible name for this type
+	 *
+	 * @var string
+	 */
+	public $title;
+
+	/**
+	 * User visible note for this type
+	 *
+	 * @var string
+	 */
+	public $note;
+
+	/**
+	 * Relative order of display
+	 *
+	 * @var integer
+	 */
+	public $displayorder;
+
+	/**
+	 * Whether this type is a credit card
+	 *
+	 * @var boolean
+	 */
+	public $credit_card;
+
+	/**
+	 * Additional charge applied when using this type
+	 *
+	 * @var double
+	 */
+	public $surcharge;
+
+	// }}}
+	// {{{ protected function init()
+
+	protected function init()
+	{
+		$this->table = 'PaymentType';
+		$this->id_field = 'integer:id';
+	}
+
+	// }}}
+}
+
+?>
