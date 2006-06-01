2dbe619f64c52d2163fb3ebb93d32843090cf099
Nathan Fredrickson <nathan@silverorange.com>
2006-06-01 21:24:55 +0000

    Move article dataobject into Store package. Going upstream!
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@6498 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Store/dataobjects/StoreArticle.php
@@ -0,0 +1,147 @@
+<?php
+
+require_once 'Store/dataobjects/StoreDataObject.php';
+require_once 'Store/StoreCategoryWrapper.php';
+
+/**
+ * @package   Store
+ * @copyright 2006 silverorange
+ */
+class StoreArticle extends StoreDataObject
+{
+	// {{{ public properties
+
+	/**
+	 * Unique identifier
+	 *
+	 * @var integer
+	 */
+	public $id;
+
+	/**
+	 * User visible title
+	 *
+	 * @var string
+	 */
+	public $title;
+
+	/**
+	 * User visible description
+	 *
+	 * @var string
+	 */
+	public $description;
+
+	/**
+	 * User visible content
+	 *
+	 * @var string
+	 */
+	public $bodytext;
+
+	/**
+	 * Create date
+	 *
+	 * @var Date
+	 */
+	public $createdate;
+
+	/**
+	 * Order of display
+	 *
+	 * @var integer
+	 */
+	public $displayorder;
+
+	/**
+	 * Whether article can be loaded on the front-end
+	 *
+	 * @var boolean
+	 */
+	public $enabled;
+
+	/**
+	 * Whether article is listed in sub-article lists
+	 *
+	 * @var boolean
+	 */
+	public $show;
+
+	/**
+	 * Weather article is included in search results
+	 *
+	 * @var boolean
+	 */
+	public $searchable;
+
+	/**
+	 * String identifer
+	 *
+	 * Unique among siblings and used in the URL.
+	 *
+	 * @var string
+	 */
+	public $shortname;
+
+	// }}}
+	// {{{ protected function init()
+
+	protected function init()
+	{
+		$this->registerInternalField('parent',
+			$this->class_map->resolveClass('StoreArticle'));
+
+		$this->registerDateField('createdate');
+
+		$this->table = 'Article';
+		$this->id_field = 'integer:id';
+	}
+
+	// }}}
+	// {{{ protected function loadPath()
+
+	protected function loadPath()
+	{
+		if ($this->hasInternalValue('path')) {
+			$path = $this->getInternalValue('path');
+
+			if ($path !== null)
+				return $path;
+		}
+
+		$sql = sprintf('select getArticlePath(%s)',
+			$this->db->quote($this->id, 'integer'));
+
+		$path = SwatDB::queryOne($this->db, $sql);
+
+		return $path;
+	}
+
+	// }}}
+	// {{{ protected function loadRelatedCategories()
+
+	/**
+	 * Loads related cateogries 
+	 *
+	 * Related categories are ordered by the category table's display order.
+	 *
+	 * @see Category::loadRelatedArticles()
+	 */
+	protected function loadRelatedCategories()
+	{
+		$sql = 'select Category.*, getCategoryPath(id) as path
+			from Category 
+				inner join ArticleCategoryBinding
+					on Category.id = ArticleCategoryBinding.category
+						and ArticleCategoryBinding.article = %s
+			order by Category.displayorder asc';
+
+		$sql = sprintf($sql, $this->db->quote($this->id, 'integer'));
+		$wrapper = $this->class_map->resolveClass('StoreCategoryWrapper');
+		return SwatDB::query($this->db, $sql, $wrapper);
+	}
+
+	// }}}
+}
+
+?>
