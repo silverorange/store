800915b51d7e0f3a3f4c9f8c68f594e0f22abd0a
Nathan Fredrickson <nathan@silverorange.com>
2006-06-01 21:24:55 +0000

    Move article dataobject into Store package. Going upstream!
    svn commit r6498

---
+++ Store/dataobjects/StoreArticleWrapper.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once 'Store/StoreRecordsetWrapper.php';
+require_once 'StoreArticle.php';
+
+/**
+ * A recordset wrapper class for StoreArticle objects
+ *
+ * @package   Store
+ * @copyright 2006 silverorange
+ * @see       StoreArticle
+ */
+class StoreArticleWrapper extends StoreRecordsetWrapper
+{
+	// {{{ public static function loadSetFromDB()
+
+	public static function loadSetFromDB($db, $id_set, $fields = '*')
+	{
+		$sql = 'select %s from Article where id in (%s)';
+		$sql = sprintf($sql, $fields, $id_set);
+		$wrapper = $this->class_map->resolveClass('StoreArticleWrapper');
+		return SwatDB::query($db, $sql, $wrapper);
+	}
+
+	// }}}
+	// {{{ public function getByShortname()
+
+	public function getByShortname($shortname)
+	{
+		foreach($this as $article)
+			if ($article->shortname === $shortname)
+				return $article;
+
+		return null;
+	}
+
+	// }}}
+	// {{{ protected function init()
+
+	protected function init()
+	{
+		parent::init();
+		$this->row_wrapper_class =
+			$this->class_map->resolveClass('StoreArticle');
+	}
+
+	// }}}
+}
+
+?>
