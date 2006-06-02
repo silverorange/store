1b61ef66bad32eea6773fd4f2853836c3e05890f
Nathan Fredrickson <nathan@silverorange.com>
2006-06-02 14:43:38 +0000

    make address wrapper work, still broken in other ways
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@6561 94490950-a86c-4493-9ccb-530f72933e47

--- Store/dataobjects/StoreArticleWrapper.php
+++ Store/dataobjects/StoreArticleWrapper.php
@@ -12,6 +12,16 @@ require_once 'StoreArticle.php';
  */
 class StoreArticleWrapper extends StoreRecordsetWrapper
 {
+	// {{{ protected function init()
+
+	protected function init()
+	{
+		parent::init();
+		$this->row_wrapper_class =
+			$this->class_map->resolveClass('StoreArticle');
+	}
+
+	// }}}
 	// {{{ public static function loadSetFromDB()
 
 	public static function loadSetFromDB($db, $id_set, $fields = '*')
@@ -32,16 +42,6 @@ class StoreArticleWrapper extends StoreRecordsetWrapper
 				return $article;
 
 		return null;
-	}
-
-	// }}}
-	// {{{ protected function init()
-
-	protected function init()
-	{
-		parent::init();
-		$this->row_wrapper_class =
-			$this->class_map->resolveClass('StoreArticle');
 	}
 
 	// }}}
