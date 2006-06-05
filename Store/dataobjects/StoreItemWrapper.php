9322c36b82cd55ea4971ef87d0d25deb796e1e6a
Michael Gauthier <mike@silverorange.com>
2006-06-05 19:16:23 +0000

    Added a default item wrapper.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@6730 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Store/dataobjects/StoreItemWrapper.php
@@ -0,0 +1,85 @@
+<?php
+
+require_once 'Store/dataobjects/StoreRecordsetWrapper.php';
+require_once 'Store/dataobjects/StoreItem.php';
+
+/**
+ * A recordset wrapper class for Item objects
+ *
+ * TODO: remove DB dependency on ItemGroupBinding table
+ *
+ * @package   Store
+ * @copyright 2005-2006 silverorange
+ */
+class StoreItemWrapper extends StoreRecordsetWrapper
+{
+	// {{{ public static function loadSetFromDB()
+
+	public static function loadSetFromDB($db, $id_set)
+	{
+		$sql = 'select Item.*, ItemGroup.title as item_group_title
+			from Item
+				left outer join ItemGroup on Item.item_group = ItemGroup.id
+			where Item.id in (%s)
+			order by ItemGroup.displayorder, Item.displayorder, Item.sku, 
+				Item.part_count';
+
+		$sql = sprintf($sql, $id_set);
+
+		$class_map = StoreDataObjectClassMap::instance();
+		return SwatDB::query($db, $sql,
+			$class_map->resolveClass('StoreItemWrapper'));
+	}
+
+	// }}}
+	// {{{ public static function loadSetFromDBWithRegion()
+
+	/**
+	 * Loads a set of items with region-specific fields filled in with a
+	 * specific region
+	 *
+	 * @param MDB2_Driver_Common $db
+	 * @param string $id_set
+	 * @param integer $region the region to use for region-specific fields.
+	 * @param boolean $limiting whether or not to load items that are not in
+	 *                           the given region. If true, these items are not
+	 *                           loaded. If false, the items are loaded without
+	 *                           region specific fields.
+	 */
+	public static function loadSetFromDBWithRegion($db, $id_set, $region,
+		$limiting = true)
+	{
+		$sql = 'select Item.*, ItemRegionBinding.price,
+			ItemRegionBinding.region, ItemGroup.title as item_group_title
+			from Item
+				%s ItemRegionBinding on ItemRegionBinding.item = Item.id and
+					ItemRegionBinding.region = %s
+				left outer join ItemGroup on Item.item_group = ItemGroup.id
+			where Item.id in (%s)
+			order by ItemGroup.displayorder, Item.displayorder, Item.sku, 
+				Item.part_count';
+
+		$sql = sprintf($sql,
+			$limiting ? 'inner join' : 'left outer join',
+			$db->quote($region, 'integer'),
+			$id_set);
+
+		$class_map = StoreDataObjectClassMap::instance();
+		return SwatDB::query($db, $sql,
+			$class_map->resolveClass('StoreItemWrapper'));
+	}
+
+	// }}}
+	// {{{ protected function init()
+
+	protected function init()
+	{
+		parent::init();
+		$this->row_wrapper_class = $this->class_map->resolveClass('StoreItem');
+		$this->index_field = 'id';
+	}
+
+	// }}}
+}
+
+?>
