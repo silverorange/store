a1fc28f600f07495c46f7a78478f87cfd98dc797
Nick Burka <nick@silverorange.com>
2006-06-05 23:47:35 +0000

    New Address View
    svn commit r6757

--- Store/StoreAddressView.php
+++ Store/StoreAddressView.php
@@ -1,6 +1,9 @@
 <?php
 
-require_once 'Store/StoreAdress.php';
+require_once 'Store/dataobjects/StoreAddress.php';
+require_once 'Swat/SwatConfirmationButton.php';
+require_once 'Swat/SwatHtmlTag.php';
+require_once 'Swat/SwatControl.php';
 
 /**
  * A viewer for an address object.
@@ -10,25 +13,66 @@ require_once 'Store/StoreAdress.php';
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 
-class StoreAddressView
+class StoreAddressView extends SwatControl
 {
 	private $address = null;
+	private $remove_button;
+	private $edit_address_link = 'account/address%s';
 
 	public function __construct(StoreAddress $address)
 	{
-		$this->setAddress($address);
-	}
-
-	public function display();
-
-	public function getAddress()
-	{
-		return $this->address
-	}
-
-	public function setAddress(StoreAddress $address)
-	{
 		$this->address = $address;
+		$this->remove_button =
+			new SwatConfirmationButton('address_remove_'.$address->id);
+	}
+
+	public function process()
+	{
+		$this->remove_button->process();
+	}
+
+	public function hasBeenClicked()
+	{
+		return $this->remove_button->hasBeenClicked();
+	}
+
+	public function getHtmlHeadEntries()
+	{
+		$out = new SwatHtmlHeadEntrySet($this->html_head_entries);
+		$out->addEntrySet($this->remove_button->getHtmlHeadEntries());
+
+		return $out;
+	}
+
+	public function display()
+	{
+		ob_start();
+		//$this->address->displayCondensedAsText();
+		//TEMP because the above is currently broken
+		echo 'Steven Garrity, Loserville,
+			IN, USA, 50567';
+		$address_text = ob_get_clean();
+
+		$div = new SwatHtmlTag('div');
+
+		$div->open();
+
+		//TEMP because the above is currently broken
+		//$this->address->displayCondensed();
+		echo 'Steven Garrity, Loserville, IN, USA, 50567';
+
+		$a = new SwatHtmlTag('a');
+		$a->href = sprintf($this->edit_address_link, $this->address->id);
+		$a->setContent('edit address');
+		$a->display();
+
+		$this->remove_button->title = 'Remove Address';
+		$this->remove_button->confirmation_message = sprintf(
+			"Are you sure you want to remove the following address?\n\n%s",
+			$address_text);
+		$this->remove_button->display();
+
+		$div->close();
 	}
 }
 
