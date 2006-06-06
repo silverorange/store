901e11cf2999b5923aca0820536b536cfb3355a7
Nathan Fredrickson <nathan@silverorange.com>
2006-06-06 05:12:48 +0000

    Add StoreXMLRPCServerFactory and a common base class StorePageFactory. Push upstream!
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@6795 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreArticlePageFactory.php
+++ Store/StoreArticlePageFactory.php
@@ -1,30 +1,16 @@
 <?php
 
-require_once 'SwatDB/SwatDB.php';
-require_once 'Store/exceptions/StoreNotFoundException.php';
-require_once 'Store/layouts/StoreLayout.php';
+require_once 'Store/StorePageFactory.php';
 
 /**
  * @package   Store
  * @copyright 2006 silverorange
  */
-abstract class StoreArticlePageFactory
+abstract class StoreArticlePageFactory extends StorePageFactory
 {
 	// {{{ protected properties
 
-	protected $page_class_path = '../include/pages';
 	protected $default_page_class = 'ArticlePage';
-
-	// }}}
-	// {{{ abstract public static function instance()
-
-	/**
-	 * Gets the singleton instance of the class-mapping object
-	 *
-	 * @return StoreDataObjectClassMap the singleton instance of the class-
-	 *                                  mapping object.
-	 */
-	abstract public static function instance();
 
 	// }}}
 	// {{{ public function resolvePage()
@@ -50,48 +36,6 @@ abstract class StoreArticlePageFactory
 		$params = array($app, $layout);
 		$page = $this->instantiatePage($this->default_page_class, $params);
 		return $page;
-	}
-
-	// }}}
-	// {{{ protected function instantiatePage()
-
-	public function instantiatePage($class, $params)
-	{
-		$class_file = sprintf('%s/%s.php', $this->page_class_path, $class);
-		require_once $class_file;
-
-		$page = call_user_func_array(
-			array(new ReflectionClass($class), 'newInstance'), $params);
-
-		return $page;
-	}
-
-	// }}}
-	// {{{ protected function getPageMap()
-
-	protected function getPageMap()
-	{
-		return array();
-	}
-
-	// }}}
-	// {{{ protected function resolveLayout()
-
-	protected function resolveLayout($app, $source)
-	{
-		return new StoreLayout($app);
-	}
-
-	// }}}
-	// {{{ private function __construct()
-
-	/**
-	 * Creates a StoreAritclePageFactory object
-	 *
-	 * The constructor is private as this class uses the singleton pattern.
-	 */
-	private function __construct()
-	{
 	}
 
 	// }}}
