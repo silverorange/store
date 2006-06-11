dbf96ab204dc521f4690039967e580068ddeaf1e
Nathan Fredrickson <nathan@silverorange.com>
2006-06-11 02:28:09 +0000

    While this method may return exactly -1, it is often conventional that comparison functions return: less than zero, zero, or greater than zero.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@6987 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreCreditCardExpiryEntry.php
+++ Store/StoreCreditCardExpiryEntry.php
@@ -30,7 +30,7 @@ class StoreCreditCardExpiryEntry extends SwatDateEntry
 
 	public function isValid()
 	{
-		if (Date::compare($this->value, $this->valid_range_start, true) == -1)
+		if (Date::compare($this->value, $this->valid_range_start, true) < 0)
 			return false;
 		else
 			return true;
