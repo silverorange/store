0dac51d55cafc8cad7b8a276a7321abd43d730cc
Nick Burka <nick@silverorange.com>
2006-06-11 00:49:37 +0000

    This message made sense when the logic was more complicated. Removed once it just became a message.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@6983 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreCreditCardExpiryEntry.php
+++ Store/StoreCreditCardExpiryEntry.php
@@ -33,27 +33,6 @@ class StoreCreditCardExpiryEntry extends SwatDateEntry
 	}
 
 	// }}}
-	// {{{ public function showEditMessage()
-
-	/*
-	 * Checks the validity of the current set date
-	 *
-	 * This method is useful when editing an expiry date to notify the
-	 * customer that they must update it.
-	 */
-	public function showEditMessage()
-	{
-		$content = sprintf('The expiry date that was entered (%s)
-			is in the past. Please enter an updated date.',
-			$this->value->format(SwatDate::DF_MY));
-
-		$message = new SwatMessage($content, SwatMessage::ERROR);
-		$this->addMessage($message);
-
-		$this->value = null;
-	}
-
-	// }}}
 }
 
 ?>
