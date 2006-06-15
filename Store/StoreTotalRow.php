9a607d81ad7a87953cfbd0a06fd94d0003097148
Nathan Fredrickson <nathan@silverorange.com>
2006-06-15 20:27:50 +0000

    Move TotalRow into StoreTotalRow and - Use SwatMoneyCellRenderer - Drop inclusion of non-existent CSS - Put CSS class on TR and name to match PHP class - Use new column count method on table view
    svn commit r7117

---
+++ Store/StoreTotalRow.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once 'Swat/SwatTableViewRow.php';
+require_once 'Swat/SwatHtmlTag.php';
+require_once 'Swat/SwatMoneyCellRenderer.php';
+
+/**
+ * Displays totals in a special row at the bottom of a table view.
+ *
+ * @package   Store
+ * @copyright 2006 silverorange
+ */
+class StoreTotalRow extends SwatTableViewRow
+{
+	public $title;
+	public $value;
+	public $offset = 0;
+
+	public function display()
+	{
+		if ($this->value === null)
+			$this->visible = false;
+
+		if (!$this->visible)
+			return;
+
+		$tr_tag = new SwatHtmlTag('tr');
+		$tr_tag->class = 'store-total-row';
+		$tr_tag->id = $this->id;
+
+		$column_count = $this->view->getVisibleColumnCount();
+		$th_tag = new SwatHtmlTag('th');
+		$th_tag->colspan = $column_count - 1 - $this->offset;
+		$th_tag->setContent($this->title.':');
+
+		$renderer = new SwatMoneyCellRenderer();
+		$renderer->value = $this->value;
+
+		$td_tag = new SwatHtmlTag('td', $renderer->getTdAttributes());
+
+		$tr_tag->open();
+		$th_tag->display();
+		$td_tag->open();
+		$renderer->render();
+		$td_tag->close();
+
+		if ($this->offset > 0) {
+			$td_tag = new SwatHtmlTag('td');
+			$td_tag->colspan = $this->offset;
+			$td_tag->setContent('&nbsp;');
+			$td_tag->display();
+		}
+
+		$tr_tag->close();
+	}
+}
+
+?>
