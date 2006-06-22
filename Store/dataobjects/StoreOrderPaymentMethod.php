350d9027e279e2befce173bfd55a63b0d56738f1
Michael Gauthier <mike@silverorange.com>
2006-06-22 21:24:39 +0000

    Added documentation.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@7442 94490950-a86c-4493-9ccb-530f72933e47

--- Store/dataobjects/StoreOrderPaymentMethod.php
+++ Store/dataobjects/StoreOrderPaymentMethod.php
@@ -3,8 +3,12 @@
 require_once 'Store/dataobjects/StorePaymentMethod.php';
 
 /**
+ * A payment method for an order for an e-commerce web application 
+ *
  * @package   Store
  * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @see       StorePaymentMethod
  */
 class StoreOrderPaymentMethod extends StorePaymentMethod
 {
