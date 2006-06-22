f826528b2a13c616a3208efa02ba7c22c87a3839
Michael Gauthier <mike@silverorange.com>
2006-06-22 21:03:31 +0000

    Added documentation.
    svn commit r7429

--- Store/dataobjects/StoreOrderAddress.php
+++ Store/dataobjects/StoreOrderAddress.php
@@ -3,9 +3,14 @@
 require_once 'Store/dataobjects/StoreAddress.php';
 
 /**
+ * An address belonging to an order for an e-commerce web application
+ *
+ * This could represent either a billing or a shipping address.
  *
  * @package   Store
- * @copyright 2005-2006 silverorane
+ * @copyright 2005-2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @see       StoreOrder::$billing_address, StoreOrder::$shipping_address
  */
 class StoreOrderAddress extends StoreAddress
 {
