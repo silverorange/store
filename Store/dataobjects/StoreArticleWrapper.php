bc4ef841d69200c07c24647353f5c23598bc3d34
Michael Gauthier <mike@silverorange.com>
2006-06-22 20:15:54 +0000

    - Added documentation - Refactored the getByShortname() method to have one return point. The   developer of Corel Draw told me I should do this.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@7410 94490950-a86c-4493-9ccb-530f72933e47

--- Store/dataobjects/StoreArticleWrapper.php
+++ Store/dataobjects/StoreArticleWrapper.php
@@ -8,19 +8,34 @@ require_once 'StoreArticle.php';
  *
  * @package   Store
  * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  * @see       StoreArticle
  */
 class StoreArticleWrapper extends StoreRecordsetWrapper
 {
 	// {{{ public function getByShortname()
 
+	/**
+	 * Gets a single article from this recordset by the article's shortname
+	 *
+	 * If two or more articles in the recordset have the same shortname, the
+	 * first one is returned.
+	 *
+	 * @param string $shortname the shortname of the article to get from this
+	 *                           recordset.
+	 */
 	public function getByShortname($shortname)
 	{
-		foreach($this as $article)
-			if ($article->shortname === $shortname)
-				return $article;
+		$returned_article = null;
 
-		return null;
+		foreach($this as $article) {
+			if ($article->shortname === $shortname) {
+				$returned_article = $article;
+				break;
+			}
+		}
+
+		return $returned_article;
 	}
 
 	// }}}
