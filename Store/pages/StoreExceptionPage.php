c1540edb020ca1f460f244e9fece068c15bb2bb4
Nick Burka <nick@silverorange.com>
2006-06-22 02:12:19 +0000

    Store Exception
    svn commit r7350

---
+++ Store/pages/StoreExceptionPage.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'Site/pages/SiteExceptionPage.php';
+
+/**
+ * @package   veseys2
+ * @copyright 2006 silverorange
+ */
+class StoreExceptionPage extends SiteExceptionPage
+{
+	// build phase
+	// {{{ protected function display()
+
+	protected function display($status)
+	{
+		printf('<p>%s</p>', $this->getSummary($status));
+
+		echo '<ul class="spaced">
+				<li>If you followed a link from our site or elsewhere, please <a href="about/contact">contact us</a> and let us know where you came from so we can do our best to fix it.</li>
+				<li>If you typed in the address, please double check the spelling.</li>
+				<li>If you are looking for a product or product information, try browsing the product listing to the left or using the search box on the top right.</li>
+			</ul>';
+
+		if ($this->exception !== null)
+			$this->exception->process(false);
+	}
+
+	// }}}
+}
