3d9a754f152399dc8ce37a3cc44bed42319cc641
Michael Gauthier <mike@silverorange.com>
2006-06-22 20:59:27 +0000

    Documentation updates.
    svn commit r7427

--- Store/dataobjects/StoreCountry.php
+++ Store/dataobjects/StoreCountry.php
@@ -7,20 +7,21 @@ require_once 'Store/dataobjects/StoreDataObject.php';
  *
  * @package   Store
  * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreCountry extends StoreDataObject
 {
 	// {{{ public properties
 
 	/**
-	 * 
+	 * Unique identifier of this country 
 	 *
 	 * @var string 
 	 */
 	public $id;
 
 	/**
-	 * 
+	 * User visible title of this country 
 	 *
 	 * @var string
 	 */
