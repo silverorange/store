f7a70d5e7ed6bdaf7a4824ae6f6bb42b85c27ed4
Michael Gauthier <mike@silverorange.com>
2006-06-22 20:37:31 +0000

    Added documentation.
    svn commit r7419

--- Store/dataobjects/StoreCategoryWrapper.php
+++ Store/dataobjects/StoreCategoryWrapper.php
@@ -4,9 +4,12 @@ require_once 'Store/dataobjects/StoreRecordsetWrapper.php';
 require_once 'Store/dataobjects/StoreCategory.php';
 
 /**
+ * A recordset wrapper fot StoreCategory objects
  *
  * @package   Store
  * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @see       StoreCategory
  */
 class StoreCategoryWrapper extends StoreRecordsetWrapper
 {
