669ad5ccdd217d48a5bff33f6cf2b94886d6c986
Michael Gauthier <mike@silverorange.com>
2006-06-23 18:52:28 +0000

    Relicense.
    svn commit r7480

--- Store/dataobjects/StoreRegionWrapper.php
+++ Store/dataobjects/StoreRegionWrapper.php
@@ -8,6 +8,7 @@ require_once 'Store/dataobjects/StoreRegion.php';
  *
  * @package   Store 
  * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreRegionWrapper extends StoreRecordsetWrapper
 {
