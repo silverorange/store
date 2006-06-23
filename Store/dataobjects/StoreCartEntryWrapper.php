9089e3e14e68654ef595fabfd763b4d27df36cbb
Michael Gauthier <mike@silverorange.com>
2006-06-23 15:56:40 +0000

    Removed useless comment.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@7466 94490950-a86c-4493-9ccb-530f72933e47

--- Store/dataobjects/StoreCartEntryWrapper.php
+++ Store/dataobjects/StoreCartEntryWrapper.php
@@ -6,9 +6,6 @@ require_once 'Store/dataobjects/StoreCartEntry.php';
 /**
  * A recordset wrapper class for StoreCartEntry objects
  *
- * This class contains cart functionality common to all sites. It is typically
- * extended on a per-site basis.
- *
  * @package   Store
  * @copyright 2005-2006 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
