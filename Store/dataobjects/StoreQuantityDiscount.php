56cb0f92b909f6297c31fd5694fb35b45c9237ef
Michael Gauthier <mike@silverorange.com>
2006-06-26 17:08:22 +0000

    Added and updated documentation (did this Friday).
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@7519 94490950-a86c-4493-9ccb-530f72933e47

--- Store/dataobjects/StoreQuantityDiscount.php
+++ Store/dataobjects/StoreQuantityDiscount.php
@@ -5,29 +5,37 @@ require_once 'Store/dataobjects/StoreDataObject.php';
 /**
  * Quantity discount object
  *
+ * Quantity discounts are a discount scheme whereby the unit price of an item
+ * changes when more items are purchased at the same time. For example:
+ *
+ * - 1  item  at $5.00 each
+ * - 5  items at $4.00 each
+ * - 10 items at $3.00 each
+ *
  * @package   Store 
  * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreQuantityDiscount extends SwatDBDataObject
 {
 	// {{{ public properties
 
 	/**
-	 * 
+	 * Unique identifier of this quantity discount 
 	 *
 	 * @var integer
 	 */
 	public $id;
 
 	/**
-	 * not null references items(id),
+	 * Identifier of the item this discount applies to
 	 *
 	 * @var integer
 	 */
 	public $item;
 
 	/**
-	 * not null default 0,
+	 * Quantity required for this discount to apply
 	 *
 	 * @var integer
 	 */
