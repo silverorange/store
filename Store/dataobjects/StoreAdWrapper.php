9c529528826ba96ab0bc1af5708db8ce8e58e829
Michael Gauthier <mike@silverorange.com>
2006-06-28 18:51:12 +0000

    A wrapper for ad objects.
    svn commit r7659

---
+++ Store/dataobjects/StoreAdWrapper.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'Store/dataobjects/StoreRecordsetWrapper.php';
+require_once 'Store/dataobjects/StoreAd.php';
+
+/**
+ * A recordset wrapper class for StoreAd objects
+ *
+ * @package   Store
+ * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @see       StoreAd
+ */
+class StoreAdWrapper extends StoreRecordsetWrapper
+{
+	// {{{ protected function init()
+
+	protected function init()
+	{
+		parent::init();
+
+		$this->row_wrapper_class =
+			$this->class_map->resolveClass('StoreAd');
+
+		$this->index_field = 'id';
+	}
+
+	// }}}
+}
+
+?>
