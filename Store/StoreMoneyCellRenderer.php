544f94ff09d2acc9206f7948d6f1843add0da715
Michael Gauthier <mike@silverorange.com>
2006-06-29 21:00:41 +0000

    Money cell renderer that shows n/a when no value is available.
    svn commit r7692

---
+++ Store/StoreMoneyCellRenderer.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'Swat/SwatMoneyCellRenderer.php';
+
+/** 
+ * Money cell renderer that displays n/a when no value is available
+ *
+ * @package   Store
+ * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class StoreMoneyCellRenderer extends SwatMoneyCellRenderer
+{
+	public function render()
+	{
+		if ($this->value === null) {
+			$span_tag = new SwatHtmlTag('span');
+			$span_tag->class = 'swat-none';
+			$span_tag->setContent('n/a');
+			$span_tag->display();
+		} else {
+			parent::render();
+		}
+	}
+}
+
+?>
