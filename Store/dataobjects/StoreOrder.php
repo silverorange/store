4ef9df064874f9bafbee4dda4ad4a671f440ec76
Nathan Fredrickson <nathan@silverorange.com>
2006-07-10 14:49:18 +0000

    Add a fullname property to StoreOrder
    svn commit r7854

--- Store/dataobjects/StoreOrder.php
+++ Store/dataobjects/StoreOrder.php
@@ -25,6 +25,13 @@ class StoreOrder extends StoreDataObject
 	public $id;
 
 	/**
+	 * The full name of this customer
+	 *
+	 * @var string
+	 */
+	public $fullname;
+
+	/**
 	 * Snapshot of the customer's email address
 	 *
 	 * @var string
