4721ad78287857622816ab45e8261f145dcf9cde
Michael Gauthier <mike@silverorange.com>
2006-07-31 20:44:09 +0000

    Methods to load applicable billing and shipping countries.
    svn commit r8872

--- Store/dataobjects/StoreRegionWrapper.php
+++ Store/dataobjects/StoreRegionWrapper.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'Store/dataobjects/StoreRecordsetWrapper.php';
+require_once 'Store/dataobjects/StoreCountryWrapper.php';
 require_once 'Store/dataobjects/StoreRegion.php';
 
 /**
@@ -19,6 +20,51 @@ class StoreRegionWrapper extends StoreRecordsetWrapper
 		parent::init();
 		$this->row_wrapper_class =
 			$this->class_map->resolveClass('StoreRegion');
+	}
+
+	// }}}
+	// {{{ protected function loadBillingCountries()
+
+	/** 
+	 * Gets countries that orders may be billed to in this region
+	 *
+	 * @return StoreCountryWrapper a recordset of StoreCountry objects that
+	 *                              orders may be billed to.
+	 */
+	protected function loadBillingCountries()
+	{
+		$this->checkDB();
+
+		$sql = 'select id, title from Country
+			inner join on BillingCountryRegionBinding where
+				Country.id = BillingCountryRegionBinding.country and
+					BillingCountryRegionBinding.region = %s';
+
+		$sql = sprintf($sql, $this->id);
+		return SwatDB::query($this->db, $sql, 'StoreCountryWrapper');
+	}
+
+	// }}}
+	// {{{ protected function loadShippingCountries()
+
+	/** 
+	 * Gets countries that orders may be shipped to in this region
+	 *
+	 * @return StoreCountryWrapper a recordset of StoreCountry objects that
+	 *                              orders may be shipped to.
+	 */
+	protected function loadShippingCountries()
+	{
+		$this->checkDB();
+
+		$sql = 'select id, title from Country
+			inner join on ShippingCountryRegionBinding where
+				Country.id = ShippingCountryRegionBinding.country and
+					ShippingCountryRegionBinding.region = %s';
+
+		$sql = sprintf($sql, $this->id);
+		return SwatDB::query($this->db, $sql, 'StoreCountryWrapper');
+
 	}
 
 	// }}}
