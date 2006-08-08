3f857adeb31cd4c4e8974c6c78f597c9bff0366b
Nathan Fredrickson <nathan@silverorange.com>
2006-08-08 22:04:53 +0000

    Straighten out confusion between AccountAddress ids and OrderAddress ids. Same for PaymentMethod.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@9310 94490950-a86c-4493-9ccb-530f72933e47

--- Store/dataobjects/StoreOrderAddress.php
+++ Store/dataobjects/StoreOrderAddress.php
@@ -14,12 +14,52 @@ require_once 'Store/dataobjects/StoreAddress.php';
  */
 class StoreOrderAddress extends StoreAddress
 {
+	// {{{ protected properties
+
+	/**
+	 * Id of the account address this order address was created from
+	 *
+	 * @var integer
+	 */
+	protected $account_address_id = null;
+
+	// }}}
+	// {{{ public function getAccountAddressId()
+
+	public function getAccountAddressId()
+	{
+		return $this->account_address_id;
+	}
+
+	// }}}
+	// {{{ public function copyFrom()
+
+	public function copyFrom(StoreAddress $address)
+	{
+		parent::copyFrom($address);
+
+		if ($address instanceof StoreAccountAddress)
+			$this->account_address_id = $address->id;
+	}
+
+	// }}}
 	// {{{ protected function init()
 
 	protected function init()
 	{
 		parent::init();
 		$this->table = 'OrderAddress';
+	}
+
+	// }}}
+	// {{{ protected function getSerializablePrivateProperties()
+
+	protected function getSerializablePrivateProperties()
+	{
+		$properties = parent::getSerializablePrivateProperties();
+		$properties[] = 'account_address_id';
+
+		return $properties;
 	}
 
 	// }}}
--- Store/dataobjects/StoreOrderPaymentMethod.php
+++ Store/dataobjects/StoreOrderPaymentMethod.php
@@ -12,12 +12,52 @@ require_once 'Store/dataobjects/StorePaymentMethod.php';
  */
 class StoreOrderPaymentMethod extends StorePaymentMethod
 {
+	// {{{ protected properties
+
+	/**
+	 * Id of the account payment method this order payment method was created from
+	 *
+	 * @var integer
+	 */
+	protected $account_payment_method_id = null;
+
+	// }}}
+	// {{{ public function getAccountPaymentMethodId()
+
+	public function getAccountPaymentMethodId()
+	{
+		return $this->account_payment_method_id;
+	}
+
+	// }}}
+	// {{{ public function copyFrom()
+
+	public function copyFrom(StorePaymentMethod $method)
+	{
+		parent::copyFrom($method);
+
+		if ($method instanceof StoreAccountPaymentMethod)
+			$this->account_payment_method_id = $method->id;
+	}
+
+	// }}}
 	// {{{ protected function init()
 
 	protected function init()
 	{
 		parent::init();
 		$this->table = 'OrderPaymentMethod';
+	}
+
+	// }}}
+	// {{{ protected function getSerializablePrivateProperties()
+
+	protected function getSerializablePrivateProperties()
+	{
+		$properties = parent::getSerializablePrivateProperties();
+		$properties[] = 'account_payment_method_id';
+
+		return $properties;
 	}
 
 	// }}}
