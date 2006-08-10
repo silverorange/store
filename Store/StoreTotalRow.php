a233d4e5975b8b232d95cd6494339a877d41ef54
Steven Garrity <steven@silverorange.com>
2006-08-10 17:59:29 +0000

    Right-align <th> cells in StoreTotalRow
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@9453 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreTotalRow.php
+++ Store/StoreTotalRow.php
@@ -30,6 +30,7 @@ class StoreTotalRow extends SwatTableViewRow
 	{
 		parent::__construct();
 		$this->money_cell_renderer = new SwatMoneyCellRenderer();
+		$this->addStyleSheet('packages/store/styles/store-total-row.css');
 	}
 
 	// }}}
