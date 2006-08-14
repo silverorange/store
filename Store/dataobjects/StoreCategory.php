5c99d8822a6079b345145c457a411f4c55a53be0
Michael Gauthier <mike@silverorange.com>
2006-08-14 18:55:57 +0000

    use visible product count cache
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@9563 94490950-a86c-4493-9ccb-530f72933e47

--- Store/dataobjects/StoreCategory.php
+++ Store/dataobjects/StoreCategory.php
@@ -165,7 +165,7 @@ class StoreCategory extends StoreDataObject
 			$product_count = $this->getInternalValue('product_count');
 		} else {
 			$sql = 'select product_count
-				from CategoryVisibleProductCountByRegionView
+				from CategoryVisibleProductCountByRegion
 				where region = %s and category = %s';
 
 			$sql = sprintf($sql,
