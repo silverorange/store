bda30d2ae92f9c0413a2dffb32c1f8fb3b62d046
Nathan Fredrickson <nathan@silverorange.com>
2006-08-15 21:31:22 +0000

    Site does this now, and it does it better.
    svn commit r9672

--- Store/StoreApplication.php
+++ Store/StoreApplication.php
@@ -24,34 +24,6 @@ abstract class StoreApplication extends SiteApplication
 	public $db;
 
 	// }}}
-	// {{{ public function relocate()
-
-	/**
-	 * Relocates to another URI
-	 *
-	 * Appends the session identifier (SID) to the URL if necessary.
-	 * Then call parent method Site::relocate to do the actual relocating.
-	 * This function does not return and in fact calls the PHP exit()
-	 * function just to be sure execution does not continue.
-	 *
-	 * @param string $uri the URI to relocate to.
-	 * @param boolean $append_sid whether to append the SID to the URI
-	 */
-	public function relocate($uri, $secure = null, $append_sid = true)
-	{
-		if ($append_sid && $this->session->isActive()) {
-			if (strpos($uri, '?') === FALSE)
-				$uri.= '?';
-			else
-				$uri.= '&';
-
-			$uri.= sprintf('%s=%s', session_name(), session_id());
-		}
-
-		parent::relocate($uri, $secure);
-	}
-
-	// }}}
 	// {{{ protected function getDefaultModuleList()
 
 	protected function getDefaultModuleList()
