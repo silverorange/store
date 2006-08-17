7dea81c77469717192da7423f40dfe4914f86d43
Michael Gauthier <mike@silverorange.com>
2006-08-17 21:39:33 +0000

    New class for store quantity entry boxes.
    svn commit r9822

---
+++ Store/StoreQuantityEntry.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once 'Swat/SwatIntegerEntry.php';
+
+/**
+ * An integer entry widget especially taillored to quantity entry for an
+ * e-commerce web application
+ *
+ * @package   Store
+ * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class StoreQuantityEntry extends SwatIntegerEntry
+{
+	// {{{ public function __construct()
+
+	public function __construct($id = null)
+	{
+		parent::__construct($null);
+
+		$this->minimum_value = 0;
+		$this->maxlength = 8;
+		$this->size = 3;
+		$this->show_thousands_separator = false;
+	}
+
+	// }}}
+	// {{{ protected function getCSSClassNames()
+	/**
+	 * Gets the array of CSS classes that are applied to this entry widget
+	 *
+	 * @return array the array of CSS classes that are applied to this entry
+	 *                widget.
+	 */
+	protected function getCSSClassNames()
+	{
+		$classes = array('swat-entry', 'store-quantity-entry');
+		$classes = array_merge($classes, $this->classes);
+		return $classes;
+	}
+
+	// }}}
+}
+
+?>
