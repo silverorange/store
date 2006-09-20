500b3ddcc9bb1159f0fdb5d1bf62cd2867034293
Nathan Fredrickson <nathan@silverorange.com>
2006-09-20 21:50:31 +0000

    Create a new StoreLocaleApplication that upstreams alot of veseys Application.
    svn commit r11168

---
+++ Store/StoreLocaleApplication.php
@@ -0,0 +1,104 @@
+<?php
+
+require_once 'Store/StoreApplication.php';
+require_once 'Store/dataobjects/StoreRegionWrapper.php';
+
+/**
+ *
+ *
+ * @package   Store
+ * @copyright 2006 silverorange
+ */
+abstract class StoreLocaleApplication extends StoreApplication
+{
+	// {{{ protected properties
+
+	protected $locale;
+	protected $region;
+
+	// }}}
+	// {{{ public function getBaseHref()
+
+	public function getBaseHref($secure = null, $locale = null)
+	{
+		if ($locale === null)
+			$locale = $this->locale;
+
+		if ($locale === null)
+			return parent::getBaseHref($secure);
+
+		$language = substr($locale, 0, 2);
+		$country = strtolower(substr($locale, 3, 2));
+		return parent::getBaseHref($secure).$country.'/'.$language.'/';
+	}
+
+	// }}}
+	// {{{ public function getLocale()
+
+	public function getLocale()
+	{
+		return $this->locale;
+	}
+
+	// }}}
+	// {{{ public function getRegion()
+
+	public function getRegion()
+	{
+		return $this->region;
+	}
+
+	// }}}
+	// {{{ protected function loadPage()
+
+	protected function loadPage()
+	{
+		$this->parseLocale(self::initVar('locale'));
+
+		parent::loadPage();
+	}
+
+	// }}}
+	// {{{ protected function getBaseHrefRelativeUri()
+
+	protected function getBaseHrefRelativeUri($secure = null)
+	{
+		$uri = parent::getBaseHrefRelativeUri($secure);
+
+		// trim locale
+		if (preg_match('|^[a-z][a-z]/[a-z][a-z]/|', $uri) === 1)
+			$uri = substr($uri, 6);
+
+		return $uri;
+	}
+
+	// }}}
+	// {{{ private function parseLocale()
+
+	private function parseLocale($locale)
+	{
+		$this->locale = null;
+		$this->region = null;
+
+		if (!ereg('([a-z][a-z])/([a-z][a-z])', $locale, $regs))
+			return;
+
+		$this->locale = $regs[2].'_'.strtoupper($regs[1]);
+
+		$sql = 'select id, title from Region where id in
+			(select region from Locale where id = %s)';
+
+		$sql = sprintf($sql, $this->db->quote($this->locale, 'text'));
+		$regions = SwatDB::query($this->db, $sql, 'StoreRegionWrapper');
+		$this->region = $regions->getFirst();
+
+		if ($this->region === null)
+			$this->locale = null;
+		else
+			setlocale(LC_ALL, $this->locale);
+	}
+
+	// }}}
+}
+
+?>
