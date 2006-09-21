0d27e407a98674baa3a9b4dd6c18e8dee1569248
Nathan Fredrickson <nathan@silverorange.com>
2006-09-21 02:32:04 +0000

    Add/remove a few fields that were missed.
    svn commit r11203

--- sql/views/ItemView.sql
+++ sql/views/ItemView.sql
@@ -1,12 +1,3 @@
 create or replace view ItemView as
-	select id, sku, product, displayorder, description, unit, 
-			minimum_multiple, free_shipping, pst, gst, export_unit, 
-			special_shipping, status, item_group, minimum_quantity, 
-			limited_stock_quantity, part_count, part_unit
+	select id, sku, product, displayorder, description, status, item_group
 		from Item
-	union
-	select item as id, ItemAlias.sku, product, displayorder, description, unit, 
-			minimum_multiple, free_shipping, pst, gst, export_unit, 
-			special_shipping, status, item_group, minimum_quantity, 
-			limited_stock_quantity, part_count, part_unit
-		from ItemAlias inner join Item on ItemAlias.item = Item.id;
