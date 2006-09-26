9e95b3feaa79387d9f6d61ca7b55ca53831f7f90
Nathan Fredrickson <nathan@silverorange.com>
2006-09-26 01:33:31 +0000

    Add category image dataobject to Store package.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@11453 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Store/dataobjects/StoreCategoryImage.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'Store/dataobjects/StoreImage.php';
+
+/**
+ * An image data object for categories
+ *
+ * @package Store
+ * @copyright silverorange 2006
+ */
+abstract class StoreCategoryImage extends StoreImage
+{
+}
+
+?>
---
+++ Store/dataobjects/StoreCategoryImageWrapper.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'Store/dataobjects/StoreRecordsetWrapper.php';
+require_once 'Store/dataobjects/StoreCategoryImage.php';
+
+/**
+ * A recordset wrapper class for StoreCategoryImage objects
+ *
+ * @package   Store
+ * @copyright 2006 silverorange
+ * @see       StoreCategoryImage
+ */
+class StoreCategoryImageWrapper extends StoreRecordsetWrapper
+{
+	// {{{ protected function init()
+
+	protected function init()
+	{
+		parent::init();
+		$this->row_wrapper_class =
+			$this->class_map->resolveClass('StoreCategoryImage');
+
+		$this->index_field = 'id';
+	}
+
+	// }}}
+}
+
+?>
