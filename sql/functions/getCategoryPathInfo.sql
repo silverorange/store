d32bac9a88fb728d3a964a41df2ee971fc6ba01f
Nathan Fredrickson <nathan@silverorange.com>
2006-09-27 19:48:34 +0000

    remove twigs from Store package
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@11563 94490950-a86c-4493-9ccb-530f72933e47

--- sql/functions/getCategoryPathInfo.sql
+++ sql/functions/getCategoryPathInfo.sql
@@ -3,29 +3,25 @@
  *
  * @param_parent INTEGER: the id of the category.
  *
- * @returned_row type_category_path_info: a row containing id, parent, shortname, title, and twig
+ * @returned_row type_category_path_info: a row containing id, parent, shortname, title
  *
  * Returns a set of returned_rows ordered from leaf to root category.
  * Checking if the parent categories are enabled is left up to sp_category_find.
  * If the category is not found, returns an empty recordset
  */
-CREATE TYPE type_category_path_info AS (id INTEGER, parent INTEGER, shortname VARCHAR(255), title VARCHAR(255), twig BOOLEAN);
+CREATE TYPE type_category_path_info AS (id INTEGER, parent INTEGER, shortname VARCHAR(255), title VARCHAR(255));
 
-CREATE OR REPLACE FUNCTION getCategoryPathInfo(INTEGER, INTEGER) RETURNS SETOF type_category_path_info AS $$
+CREATE OR REPLACE FUNCTION getCategoryPathInfo(INTEGER) RETURNS SETOF type_category_path_info AS $$
 	DECLARE
 		param_id ALIAS FOR $1;
-		param_twigcount ALIAS FOR $2;
 		local_id INTEGER;
 		local_count INTEGER;
-		local_twig BOOLEAN;
 		returned_row type_category_path_info%ROWTYPE;
 	BEGIN
 		local_id := param_id;
 
 		WHILE local_id is not null LOOP
 			BEGIN
-				local_twig = FALSE;
-
 				SELECT INTO local_count count(id) FROM Category WHERE parent = local_id;
 
 				IF local_count != 0 THEN
@@ -34,15 +30,11 @@ CREATE OR REPLACE FUNCTION getCategoryPathInfo(INTEGER, INTEGER) RETURNS SETOF t
 
 					IF local_count = 0 THEN
 						SELECT INTO local_count count(product) FROM CategoryProductBinding WHERE category IN (SELECT id FROM Category WHERE parent = local_id);
-
-						IF local_count < param_twigcount THEN
-							local_twig = TRUE;
-						END IF;
 					END IF;
 				END IF;
 
 				-- Get the results
-				SELECT INTO returned_row id, parent, shortname, title, local_twig
+				SELECT INTO returned_row id, parent, shortname, title
 				FROM Category
 				WHERE id = local_id;
 
