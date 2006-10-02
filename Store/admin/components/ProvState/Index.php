6a8ddeda3382136cb0e097d5810aea52fe9451d6
Isaac Grant <isaac@silverorange.com>
2006-10-02 16:20:27 +0000

    moving ProvState up to Store
    svn commit r11709

---
+++ Store/admin/components/ProvState/Index.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once 'Admin/pages/AdminIndex.php';
+require_once 'Admin/AdminTableStore.php';
+require_once 'SwatDB/SwatDB.php';
+
+/**
+ * Index page for ProvStates
+ *
+ * @package   Store
+ * @copyright 2005-2006 silverorange
+ */
+class StoreProvStateIndex extends AdminIndex
+{
+	// init phase
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		parent::initInternal();
+
+		$this->ui->loadFromXML(dirname(__FILE__).'/index.xml');
+	}
+
+	// }}}
+
+	// process phase
+	// {{{ protected function processActions()
+
+	protected function processActions(SwatTableView $view, SwatActions $actions)
+	{
+		switch ($actions->selected->id) {
+		case 'delete':
+			$this->app->replacePage('ProvState/Delete');
+			$this->app->getPage()->setItems($view->checked_items);
+			break;
+		}
+	}
+
+	// }}}
+
+	// build phase
+	// {{{ protected function getTableStore()
+
+	protected function getTableStore($view)
+	{
+		$sql = 'select ProvState.id, ProvState.title,
+					Country.title as country_title, ProvState.abbreviation,
+					ProvState.location
+				from ProvState
+					inner join Country on Country.id = ProvState.country
+				order by %s';
+
+		$sql = sprintf($sql, $this->getOrderByClause($view,
+			'Country.title, ProvState.title'));
+
+		$store = SwatDB::query($this->app->db, $sql, 'AdminTableStore');
+
+		return $store;
+	}
+
+	// }}}
+}
+
+?>
