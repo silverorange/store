c113f4aaf0769e4a21a4e53221dada6ca25c2651
Isaac Grant <isaac@silverorange.com>
2006-10-02 17:04:20 +0000

    Locale admin component to Store
    svn commit r11716

---
+++ Store/admin/components/Locale/Edit.php
@@ -0,0 +1,85 @@
+<?php
+
+require_once 'Admin/pages/AdminDBEdit.php';
+require_once 'SwatDB/SwatDB.php';
+require_once 'Swat/SwatMessage.php';
+
+/**
+ * Edit page for Locale
+ *
+ * @package   Store
+ * @copyright 2005-2006 silverorange
+ */
+class StoreLocaleEdit extends AdminDBEdit
+{
+	// {{{ protected properties
+
+	protected $fields;
+
+	// }}}
+
+	// init phase
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		parent::initInternal();
+
+		$this->ui->loadFromXML(dirname(__FILE__).'/edit.xml');
+
+		$this->fields = array('text:id', 'integer:region');
+		
+		$id_flydown = $this->ui->getWidget('region');
+		$id_flydown->show_blank = false;
+		$id_flydown->addOptionsByArray(SwatDB::getOptionArray($this->app->db, 
+			'Region', 'title', 'id', 'title'));
+	}
+
+	// }}}
+
+	// process phase
+	// {{{ protected function validate()
+
+	protected function validate()
+	{
+		$localeid = $this->ui->getWidget('id');
+
+		if (!ereg('^[a-z][a-z]_[A-Z][A-Z]$', $localeid->value)) {
+			$localeid->addMessage(new SwatMessage(
+				Store::_('Invalid locale identifier.'), SwatMessage::ERROR));
+		}
+	}
+
+	// }}}
+	// {{{ protected function saveDBData()
+
+	protected function saveDBData()
+	{
+		$values = $this->ui->getValues(array('id', 'region'));
+
+		SwatDB::insertRow($this->app->db, 'Locale', $this->fields,
+			$values);
+
+		$msg = new SwatMessage(
+			sprintf(Store::_('“%s” has been saved.'), $values['id']));
+
+		$this->app->messages->add($msg);
+	}
+
+	// }}}
+
+	// build phase
+	// {{{ protected function loadDBData()
+
+	protected function loadDBData()
+	{
+		$msg = new SwatMessage('Locales can not be edited.',
+			SwatMessage::WARNING);
+
+		$this->app->messages->add($msg);
+		$this->app->relocate('Locale');
+	}
+
+	// }}}
+}
+?>
---
+++ Store/admin/components/Locale/Index.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once 'Admin/pages/AdminIndex.php';
+require_once 'Admin/AdminTableStore.php';
+require_once 'SwatDB/SwatDB.php';
+
+/**
+ * Index page for Locales
+ *
+ * @package   Store
+ * @copyright 2005-2006 silverorange
+ */
+class StoreLocaleIndex extends AdminIndex
+{
+	// init phase
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		parent::initInternal();
+
+		$this->ui->loadFromXML(dirname(__FILE__).'/index.xml');
+	}
+
+	// }}}
+
+	// process phase
+	// {{{ protected function processActions()
+	
+	protected function processActions(SwatTableView $view, SwatActions $actions)
+	{
+		switch ($actions->selected->id) {
+		case 'delete':
+			$this->app->replacePage('Locale/Delete');
+			$this->app->getPage()->setItems($view->checked_items);
+			break;
+		}
+	}
+
+	// }}}
+
+	// build phase
+	// {{{ protected function getTableStore()
+
+	protected function getTableStore($view)
+	{
+		$sql = 'select Locale.id, Region.title as region_title
+				from Locale
+				inner join Region on Locale.region = Region.id
+				order by %s';
+
+		$sql = sprintf($sql, $this->getOrderByClause($view, 'id'));
+
+		$store = SwatDB::query($this->app->db, $sql, 'AdminTableStore');
+
+		return $store;
+	}
+
+	// }}}
+}	
+
+?>
