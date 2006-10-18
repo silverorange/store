d0d37d15187f9f482bbf02c375d065bb27666b05
Nick Burka <nick@silverorange.com>
2006-10-18 21:01:01 +0000

    Nathan and I have deceided to move Email List stuff to site-specific code for now until we sort out better what is in common. Right now, uppermost and veseys are quite different.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@12143 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/EmailListSubscriber.sql
+++
@@ -1,8 +0,0 @@
-create table EmailListSubscriber (
-	id serial,
-	email varchar(255),
-	locale char(5) not null references Locale(id),
-	primary key (id)
-);
-
-CREATE INDEX EmailListSubscriber_index ON EmailListSubscriber(email);
