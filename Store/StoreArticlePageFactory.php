c821fb89d1fce3f997d2d31d842e36787d918bee
Michael Gauthier <mike@silverorange.com>
2006-11-13 04:20:17 +0000

    - add and update api documentation - make constructor protected to preserve singleton pattern - escape delimiters in page map patterns
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@12968 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreArticlePageFactory.php
+++ Store/StoreArticlePageFactory.php
@@ -3,40 +3,48 @@
 require_once 'Site/SitePageFactory.php';
 
 /**
+ * Resolves and creates article pages in a store web application
+ *
  * @package   Store
  * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 abstract class StoreArticlePageFactory extends SitePageFactory
 {
 	// {{{ protected properties
 
-	protected $default_page_class = 'StoreArticlePage';
-
-	// }}}
-	// {{{ public function __construct()
-
 	/**
-	 * Creates a new StoreUI object
+	 * The name of the default class to use when instantiating resolved pages
+	 *
+	 * This must be either {@link StoreArticlePage} or a subclass of
+	 * StoreArticlePage.
+	 *
+	 * @var string
 	 */
-	public function __construct()
-	{
-		parent::__construct();
-
-		$this->class_map['Store'] = 'Store/pages';
-	}
+	protected $default_page_class = 'StoreArticlePage';
 
 	// }}}
 	// {{{ public function resolvePage()
 
-	public function resolvePage($app, $source)
+	/**
+	 * Resolves a page object from a source string
+	 *
+	 * @param SiteWebApplication $app the web application for which the page is
+	 *                                 being resolved.
+	 * @param string $source the source string for which to get the page.
+	 *
+	 * @return StoreArticlePage the page for the given source string.
+	 */
+	public function resolvePage(SiteWebApplication $app, $source)
 	{
 		$layout = $this->resolveLayout($app, $source);
 
 		foreach ($this->getPageMap() as $pattern => $class) {
 			$regs = array();
+			$pattern = str_replace('@', '\@', $pattern); // escape delimiters
 			$regexp = '@'.$pattern.'@u';
 			if (preg_match($regexp, $source, $regs) === 1) {
-				array_shift($regs); //discard full match
+				array_shift($regs); // discard full match string
 				$article_path = array_shift($regs);
 				array_unshift($regs, $layout);
 				array_unshift($regs, $app);
@@ -46,9 +54,24 @@ abstract class StoreArticlePageFactory extends SitePageFactory
 			}
 		}
 
+		// not found in page map so instantiate default page
 		$params = array($app, $layout);
 		$page = $this->instantiatePage($this->default_page_class, $params);
 		return $page;
+	}
+
+	// }}}
+	// {{{ protected function __construct()
+
+	/**
+	 * Creates a StoreArticlePageFactory
+	 */
+	protected function __construct()
+	{
+		parent::__construct();
+
+		// set location to load Store page classes from
+		$this->class_map['Store'] = 'Store/pages';
 	}
 
 	// }}}
