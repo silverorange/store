3c72195279b2ea63937e71e3155fb85dd7a9d874
Michael Gauthier <mike@silverorange.com>
2006-11-20 20:44:04 +0000

    Rewrite JavaScript so it uses inheritance.
    This is the first JavaScript class to use YUI class extension. And there was much rejoicing.
    
    svn commit r13145

--- www/javascript/store-checkout-billing-address.js
+++ www/javascript/store-checkout-billing-address.js
@@ -1,28 +1,22 @@
-function StoreCheckoutBillingAddress(id)
+function StoreCheckoutAddress(id)
 {
 	this.id = id;
 	this.sensitive = null;
-	this.container = document.getElementById('billing_address_container');
-	this.list = document.getElementsByName('billing_address_list');
-	this.list_new = document.getElementById('billing_address_list_new');
-
-	this.provstate = document.getElementById('billing_address_provstate');
 
 	// set up event handlers
 	for (var i = 0; i < this.list.length; i++)
 		YAHOO.util.Event.addListener(this.list[i], 'click',
-			StoreCheckoutBillingAddress.clickHandler, this);
+			StoreCheckoutAddress.clickHandler, this);
 
 	if (this.provstate) {
 		YAHOO.util.Event.addListener(this.provstate, 'change',
-			StoreCheckoutBillingAddress.provstateChangeHandler, this);
+			StoreCheckoutAddress.provstateChangeHandler, this);
 
 		this.provstate_other_sensitive =
 			(this.provstate.value === 's:5:"other";');
 
 		if (!this.provstate_other_sensitive)
-			StoreCheckoutPage_desensitizeFields([
-				'billing_address_provstate_other']);
+			StoreCheckoutPage_desensitizeFields([this.provstate_other_id]);
 	}
 
 	// initialize state
@@ -32,7 +26,40 @@ function StoreCheckoutBillingAddress(id)
 		this.desensitize();
 }
 
-StoreCheckoutBillingAddress.clickHandler = function(event, address)
+StoreCheckoutAddress.prototype.sensitize = function()
+{
+	if (this.container)
+		YAHOO.util.Dom.removeClass(this.container, 'swat-insensitive');
+
+	var fields = [];
+	for (var i = 0; i < this.fields.length; i++)
+		fields.push(this.fields[i]);
+
+	if (this.provstate_other_sensitive)
+		fields.push(this.provstate_other_id);
+
+	StoreCheckoutPage_sensitizeFields(fields);
+
+	this.sensitive = true;
+}
+
+StoreCheckoutAddress.prototype.desensitize = function()
+{
+	if (this.container)
+		YAHOO.util.Dom.addClass(this.container, 'swat-insensitive');
+
+	var fields = [];
+	for (var i = 0; i < this.fields.length; i++)
+		fields.push(this.fields[i]);
+	
+	fields.push(this.provstate_other_id);
+
+	StoreCheckoutPage_desensitizeFields(fields);
+
+	this.sensitive = false;
+}
+
+StoreCheckoutAddress.clickHandler = function(event, address)
 {
 	if (address.list_new.checked)
 		address.sensitize();
@@ -40,25 +67,27 @@ StoreCheckoutBillingAddress.clickHandler = function(event, address)
 		address.desensitize();
 }
 
-StoreCheckoutBillingAddress.provstateChangeHandler = function(event, address)
+StoreCheckoutAddress.provstateChangeHandler = function(event, address)
 {
 	var provstate = YAHOO.util.Event.getTarget(event);
 	address.provstate_other_sensitive = (provstate.value === 's:5:"other";');
 
 	if (address.provstate_other_sensitive)
-		StoreCheckoutPage_sensitizeFields([
-			'billing_address_provstate_other']);
+		StoreCheckoutPage_sensitizeFields([address.provstate_other_id]);
 	else
-		StoreCheckoutPage_desensitizeFields([
-			'billing_address_provstate_other']);
+		StoreCheckoutPage_desensitizeFields([address.provstate_other_id]);
 }
 
-StoreCheckoutBillingAddress.prototype.sensitize = function()
+function StoreCheckoutBillingAddress(id)
 {
-	if (this.container)
-		YAHOO.util.Dom.removeClass(this.container, 'swat-insensitive');
+	this.container = document.getElementById('billing_address_container');
+	this.list = document.getElementsByName('billing_address_list');
+	this.list_new = document.getElementById('billing_address_list_new');
 
-	var fields = [
+	this.provstate = document.getElementById('billing_address_provstate');
+	this.provstate_other_id = 'billing_address_provstate_other';
+
+	this.fields = [
 		'billing_address_fullname',
 		'billing_address_line1',
 		'billing_address_line2',
@@ -67,30 +96,7 @@ StoreCheckoutBillingAddress.prototype.sensitize = function()
 		'billing_address_postalcode',
 		'billing_address_country'];
 
-	if (this.provstate_other_sensitive)
-		fields.push('billing_address_provstate_other');
-
-	StoreCheckoutPage_sensitizeFields(fields);
-
-	this.sensitive = true;
+	StoreCheckoutBillingAddress.superclass.constructor.call(this, id);
 }
 
-StoreCheckoutBillingAddress.prototype.desensitize = function()
-{
-	if (this.container)
-		YAHOO.util.Dom.addClass(this.container, 'swat-insensitive');
-
-	var fields = [
-		'billing_address_fullname',
-		'billing_address_line1',
-		'billing_address_line2',
-		'billing_address_city',
-		'billing_address_provstate',
-		'billing_address_provstate_other',
-		'billing_address_postalcode',
-		'billing_address_country'];
-
-	StoreCheckoutPage_desensitizeFields(fields);
-
-	this.sensitive = false;
-}
+YAHOO.extend(StoreCheckoutBillingAddress, StoreCheckoutAddress);
