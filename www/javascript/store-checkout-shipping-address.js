cd89d35cf2faf0172b5f913f06e7bc502823320f
Michael Gauthier <mike@silverorange.com>
2006-11-20 20:55:22 +0000

    Make shipping address extend address base class. This makes shipping adderess work for #256 while removing dozens of lines of code.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@13148 94490950-a86c-4493-9ccb-530f72933e47

--- www/javascript/store-checkout-shipping-address.js
+++ www/javascript/store-checkout-shipping-address.js
@@ -1,65 +1,22 @@
 function StoreCheckoutShippingAddress(id)
 {
-	this.id = id;
-	this.sensitive = null;
 	this.container = document.getElementById('shipping_address_container');
 	this.list = document.getElementsByName('shipping_address_list');
 	this.list_new = document.getElementById('shipping_address_list_new');
 
-	// set up event handlers
-	for (var i = 0; i < this.list.length; i++)
-		YAHOO.util.Event.addListener(this.list[i], 'click',
-			StoreCheckoutShippingAddress.clickHandler, this);
+	this.provstate = document.getElementById('shipping_address_provstate');
+	this.provstate_other_id = 'shipping_address_provstate_other';
 
-	// initialize state
-	if (this.list_new.checked)
-		this.sensitize();
-	else
-		this.desensitize();
-}
-
-StoreCheckoutShippingAddress.clickHandler = function(event, address)
-{
-	if (address.list_new.checked)
-		address.sensitize();
-	else if (address.sensitive || address.sensitive == null)
-		address.desensitize();
-}
-
-StoreCheckoutShippingAddress.prototype.sensitize = function()
-{
-	if (this.container)
-		YAHOO.util.Dom.removeClass(this.container, 'swat-insensitive');
-	
-	StoreCheckoutPage_sensitizeFields([
+	this.fields = [
 		'shipping_address_fullname',
 		'shipping_address_line1',
 		'shipping_address_line2',
 		'shipping_address_city',
 		'shipping_address_provstate',
-		'shipping_address_provstate_other',
 		'shipping_address_postalcode',
-		'shipping_address_country'
-	]);
+		'shipping_address_country'];
 
-	this.sensitive = true;
+	StoreCheckoutShippingAddress.superclass.constructor.call(this, id);
 }
 
-StoreCheckoutShippingAddress.prototype.desensitize = function()
-{
-	if (this.container)
-		YAHOO.util.Dom.addClass(this.container, 'swat-insensitive');
-
-	StoreCheckoutPage_desensitizeFields([
-		'shipping_address_fullname',
-		'shipping_address_line1',
-		'shipping_address_line2',
-		'shipping_address_city',
-		'shipping_address_provstate',
-		'shipping_address_provstate_other',
-		'shipping_address_postalcode',
-		'shipping_address_country'
-	]);
-
-	this.sensitive = false;
-}
+YAHOO.extend(StoreCheckoutShippingAddress, StoreCheckoutAddress);
