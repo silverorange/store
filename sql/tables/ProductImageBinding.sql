e463157f231da5cbe4359b9eef0deb2c363a18d1
Nick Burka <nick@silverorange.com>
2006-11-28 20:47:01 +0000

    New displayorder field
    svn commit r13445

--- sql/tables/ProductImageBinding.sql
+++ sql/tables/ProductImageBinding.sql
@@ -1,5 +1,6 @@
 create table ProductImageBinding (
 	product integer not null references Product(id),
 	image integer not null references Image(id),
+	displayorder integer not null default 0,
 	primary key (product, image)
 );
