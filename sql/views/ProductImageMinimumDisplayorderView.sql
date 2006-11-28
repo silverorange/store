ec3e1f2021d2e6e02388b27e2ff2a8f3d0937dd6
Nick Burka <nick@silverorange.com>
2006-11-28 20:46:12 +0000

    New views to automatically figure out which image is the primary image based on displayorder
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@13444 94490950-a86c-4493-9ccb-530f72933e47

---
+++ sql/views/ProductImageMinimumDisplayorderView.sql
@@ -0,0 +1,7 @@
+-- as long as the item is enabled in one region, its considered enabled by this view
+-- this is soley for the use of ProductItemCountByStatusView
+create or replace view ProductImageMinimumDisplayorderView as
+
+select min(displayorder) as displayorder, product 
+	from ProductImageBinding
+	group by ProductImageBinding.product
