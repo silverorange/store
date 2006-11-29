a4963c469939e2eefb0f9d85f41c5d93531cfbc2
Nathan Fredrickson <nathan@silverorange.com>
2006-11-29 16:00:46 +0000

    Refactor primary_image - Remove primary_image from Product table. - Add a loader method to StoreProduct for primary_image.  The primary_image is now handled very much like primary_category and path and can be loaded in the initial query or individually on-demand later. - Update category, product, and search pages to efficiently load the primary image.  No additional queries are run on these pages, just an extra join. - Remove image loading from cart module since images are not displayed on the cart.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@13453 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreCartModule.php
+++ Store/StoreCartModule.php
@@ -371,8 +371,7 @@ class StoreCartModule extends SiteApplicationModule
 			$this->app->db, $quoted_item_ids, $this->app->getRegion()->id,
 			false);
 
-		$product_sql = 'select id, shortname, title, primary_category, catalog,
-				primary_image
+		$product_sql = 'select id, shortname, title, primary_category, catalog
 			from Product left outer join ProductPrimaryCategoryView
 			on product = id where id in (%s)';
 
