d369b31600c54c4eed564ce5b79256cffd1ad554
Michael Gauthier <mike@silverorange.com>
2006-12-06 23:34:15 +0000

    Button row using confirmation buttons
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@13699 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Store/StoreTableViewConfirmationButtonRow.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once 'Swat/SwatConfirmationButton.php';
+require_once 'Store/StoreTableViewButtonRow.php';
+
+/**
+ * A table view row with an embedded confirmation button
+ *
+ * @package   Store
+ * @copyright 2006 silverorange
+ */
+class StoreTableViewConfirmationButtonRow extends StoreTableViewButtonRow
+{
+	// {{{ public properties
+
+	/**
+	 * Confirmation message of the button
+	 *
+	 * @var string
+	 */
+	public $confirmation_message;
+
+	// }}}
+	// {{{ protected function displayButton()
+
+	protected function displayButton()
+	{
+		// properties may have been modified since the widgets were created
+		$this->button->title = $this->title;
+		$this->button->tab_index = $this->tab_index;
+		$this->button->confirmation_message = $this->confirmation_message;
+		$this->button->display();
+	}
+
+	// }}}
+	// {{{ protected function createEmbeddedWidgets()
+
+	protected function createEmbeddedWidgets()
+	{
+		if (!$this->widgets_created) {
+			$this->button = new SwatConfirmationButton($this->id.'_button');
+			$this->button->parent = $this;
+			$this->widgets_created = true;
+		}
+	}
+
+	// }}}
+}
