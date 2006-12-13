5d36fd7a32a0bf6f6a726c324c8f4794c16779fd
Nathan Fredrickson <nathan@silverorange.com>
2006-12-13 15:22:50 +0000

    Add missing ZIP code range for VA.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@13856 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StorePostalCodeEntry.php
+++ Store/StorePostalCodeEntry.php
@@ -430,7 +430,7 @@ class StorePostalCodeEntry extends SwatEntry
 			$ranges = array('247' => '269');
 			break;
 		case 'VA': // Virginia (partially overlaps with DC)
-			$ranges = array('220' => '246', '200' => '200');
+			$ranges = array('220' => '246', '200' => '200', '201' => '201');
 			break;
 		case 'SC': // South Carolina
 			$ranges = array('29' => '29');
