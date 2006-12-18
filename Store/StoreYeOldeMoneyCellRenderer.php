c9ff5fb7791edc61d9839ac6ddd0f4748f4ee260
Michael Gauthier <mike@silverorange.com>
2006-12-18 15:57:11 +0000

    New cell renderer for rendering ye'olde style monies.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@13941 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Store/StoreYeOldeMoneyCellRenderer.php
@@ -0,0 +1,70 @@
+<?php
+
+/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+require_once 'Swat/SwatMoneyCellRenderer.php';
+
+/**
+ * A currency cell renderer
+ *
+ * @package   Swat
+ * @copyright 2005-2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class StoreYeOldeMoneyCellRenderer extends SwatMoneyCellRenderer
+{
+	// {{{ public function render()
+
+	/**
+	 * Renders the contents of this cell
+	 *
+	 * @see SwatCellRenderer::render()
+	 */
+	public function render()
+	{
+		if (!$this->visible)
+			return;
+
+		$money = SwatString::minimizeEntities(
+			SwatString::moneyFormat(
+				$this->value, $this->locale, $this->display_currency));
+
+		if ($locale !== null) {
+			$old_locale = setlocale(LC_ALL, 0);
+			if (setlocale(LC_ALL, $locale) === false) {
+				throw new SwatException(sprintf('Locale %s passed to the '.
+					'moneyFormat() method is not valid for this operating '.
+					'system.', $locale));
+			}
+		}
+
+		$lc = localeconv();
+		$decimal_point = $lc['mon_decimal_point'];
+
+		// convert decimal character to UTF-8
+		$character_set = nl_langinfo(CODESET);
+		if ($character_set !== 'UTF-8') {
+			$decimal_point = iconv($character_set, 'UTF-8', $decimal_point);
+			if ($decimal_point === false)
+				throw new SwatException(sprintf('Could not convert %s output '.
+					'to UTF-8', $character_set));
+		}
+
+		if ($locale !== null)
+			setlocale(LC_ALL, $old_locale);
+
+		$search = sprintf('/%s([0-9][0-9])/u',
+			preg_quote($decimal_point, '/'));
+
+		$replace = sprintf('<sup>%s\1</sup>',
+			$decimal_point);
+
+		$money = preg_replace($search, $replace, $money);
+
+		echo $money;
+	}
+
+	// }}}
+}
+
+?>
