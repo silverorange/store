c8ab554a6bc4e29d37d63e7de81fc11f7eee9ee4
Michael Gauthier <mike@silverorange.com>
2006-12-20 18:35:43 +0000

    new payment method fields for supporting debit cards.
    svn commit r14018

--- sql/tables/OrderPaymentMethod.sql
+++ sql/tables/OrderPaymentMethod.sql
@@ -5,5 +5,7 @@ create table OrderPaymentMethod (
 	credit_card_last4 varchar(6),
 	credit_card_number text,
 	credit_card_expiry date,
+	card_inception date,
+	card_issue_number varchar(4),
 	primary key (id)
 );
