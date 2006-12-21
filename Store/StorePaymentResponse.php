428f00138e7f61e0ee074f923299a78123270432
Michael Gauthier <mike@silverorange.com>
2006-12-21 19:22:46 +0000

    docs and folding
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@14062 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StorePaymentResponse.php
+++ Store/StorePaymentResponse.php
@@ -1,11 +1,65 @@
 <?php
 
+/**
+ * Base class for online financial transaction responses
+ *
+ * StorePaymentResponse objects are returned as the result of a
+ * {@link StorePaymentRequest::process()} method call.
+ *
+ * @package   Store
+ * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @see       StorePaymentRequest
+ */
 abstract class StorePaymentResponse
 {
+	// {{{ abstract public function __construct()
+
+	/**
+	 * Builds a payment response
+	 *
+	 * @param mixed $response_data the data to build the response object from.
+	 */
 	abstract public function __construct($response_data);
+
+	// }}}
+	// {{{ abstract public function getField()
+
+	/**
+	 * Gets a protocol-specific response field
+	 *
+	 * @param string $name the name of the field to get.
+	 *
+	 * @return mixed the value of the field.
+	 */
 	abstract public function getField($name);
+
+	// }}}
+	// {{{ abstract public function hasField()
+
+	/**
+	 * Gets whether or not protocol-specific response field exists
+	 *
+	 * @param string $name the name of the field to check.
+	 *
+	 * @return boolean true if the field <i>name</i> exists and false if the
+	 *                       field does not exist.
+	 */
 	abstract public function hasField($name);
+
+	// }}}
+	// {{{ abstract protected function __toString()
+
+	/**
+	 * Gets a string representation of this payment response
+	 *
+	 * This is primarily useful for debugging and/or logging.
+	 *
+	 * @return string a string representation of this payment response.
+	 */
 	abstract protected function __toString();
+
+	// }}}
 }
 
 ?>
