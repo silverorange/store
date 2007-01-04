51d642c693a457d0454867ba293fc9a011b2421f
Michael Gauthier <mike@silverorange.com>
2007-01-04 21:49:16 +0000

    Exception for unimplemented methods.
    svn commit r14188

---
+++ Store/exceptions/StoreUnimplementedException.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'Store/exceptions/StoreException.php';
+
+/**
+ * Exception that is thrown when an unimplemented method is called
+ *
+ * @package   Store
+ * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class StoreUnimplementedException extends StoreException
+{
+}
+
+?>
