9d8af28b9204f638d5b6afb8984257116450d05f
Michael Gauthier <mike@silverorange.com>
2007-01-05 05:25:55 +0000

    Work in progress high-level API for payment processing.
    svn commit r14196

---
+++ Store/StorePaymentProvider.php
@@ -0,0 +1,113 @@
+<?php
+
+abstract class StorePaymentProvider
+{
+	/**
+	 * Creates a new payment provider instance
+	 *
+	 * This is the main mechanism for starting an online payment transaction.
+	 *
+	 * @param string $driver the payment provider driver to use.
+	 * @param array $parameters an array of additional key-value parameters to
+	 *                           pass to the payment provider driver.
+	 *
+	 * @return StorePaymentProvider a payment provider instance using the
+	 *                               specified driver.
+	 *
+	 * @throws StoreException if the driver specified by <i>$driver</i> could
+	 *                         not be loaded.
+	 */
+	public static function factory($driver, array $parameters = array())
+	{
+		static $loaded_drivers = array();
+
+		if (array_key_exists($driver, $loaded_drivers)) {
+			$class_name = $loaded_drivers[$driver];
+		} else {
+			$sanitized_driver = basename($driver);
+			include_once 'Store/Store'.$sanitized_driver.'PaymentProvider.php';
+			$class_name = 'Store'.$sanitized_driver.'PaymentProvider';
+
+			if (!class_exists($class_name)) {
+				throw new Exception(sprintf('No payment provider available '.
+					'for driver %s', $driver));
+			}
+
+			$loaded_drivers[$sanitized_driver] = $class_name;
+		}
+
+		$reflector = new ReflectionClass($class_name);
+		return $reflector->newInstance($parameters);
+	}
+
+	/**
+	 * Creates a new payment provider
+	 *
+	 * @param array $paramaters an array of key-value pairs containing driver-
+	 *                           specific constructor properties. See
+	 *                           individual driver documentation for valid
+	 *                           parameters.
+	 */
+	abstract public function __construct(array $paramaters);
+
+	public function pay(StoreOrder $order, $card_number,
+		$card_verification_value = null)
+	{
+		require_once 'Store/exceptions/StoreUnimplementedException.php';
+		throw new StoreUnimplementedException(sprintf(
+			'%s does not implement the %s() method.',
+			get_class($this), __FUNCTION__));
+	}
+
+	public function release(StoreOrder $order)
+	{
+		require_once 'Store/exceptions/StoreUnimplementedException.php';
+		throw new StoreUnimplementedException(sprintf(
+			'%s does not implement the %s() method.',
+			get_class($this), __FUNCTION__));
+	}
+
+	public function hold(StoreOrder $order, $card_number,
+		$card_verification_value = null)
+	{
+		require_once 'Store/exceptions/StoreUnimplementedException.php';
+		throw new StoreUnimplementedException(sprintf(
+			'%s does not implement the %s() method.',
+			get_class($this), __FUNCTION__));
+	}
+
+	public function authorize(StoreOrder $order, $card_number,
+		$card_verification_value = null)
+	{
+		require_once 'Store/exceptions/StoreUnimplementedException.php';
+		throw new StoreUnimplementedException(sprintf(
+			'%s does not implement the %s() method.',
+			get_class($this), __FUNCTION__));
+	}
+
+	public function refund(StoreOrder $order, $amount = null)
+	{
+		require_once 'Store/exceptions/StoreUnimplementedException.php';
+		throw new StoreUnimplementedException(sprintf(
+			'%s does not implement the %s() method.',
+			get_class($this), __FUNCTION__));
+	}
+
+	public function authorizedPay(StoreOrder $order)
+	{
+		require_once 'Store/exceptions/StoreUnimplementedException.php';
+		throw new StoreUnimplementedException(sprintf(
+			'%s does not implement the %s() method.',
+			get_class($this), __FUNCTION__));
+	}
+
+	public function void(StoreOrder $order)
+	{
+		require_once 'Store/exceptions/StoreUnimplementedException.php';
+		throw new StoreUnimplementedException(sprintf(
+			'%s does not implement the %s() method.',
+			get_class($this), __FUNCTION__));
+	}
+}
+
+?>
