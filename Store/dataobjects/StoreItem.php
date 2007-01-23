9bf72ff484c4e3c615151b054de55ed672d94116
Michael Gauthier <mike@silverorange.com>
2007-01-23 20:12:40 +0000

    Add missing requires
    svn commit r14455

--- Store/dataobjects/StoreItem.php
+++ Store/dataobjects/StoreItem.php
@@ -4,6 +4,7 @@ require_once 'Store/dataobjects/StoreDataObject.php';
 require_once 'Store/dataobjects/StoreQuantityDiscountWrapper.php';
 require_once 'Store/dataobjects/StoreItemRegionBindingWrapper.php';
 require_once 'Store/dataobjects/StoreItemGroup.php';
+require_once 'Store/dataobjects/StoreRegion.php';
 
 /**
  * An item for an e-commerce web application
