5c4b7b677b94b3db791f29b42d6f0f7a47013562
Michael Gauthier <mike@silverorange.com>
2007-02-02 15:37:27 +0000

    Add loadFromShortname() method to StorePaymentType
    svn commit r14655

--- Store/dataobjects/StorePaymentType.php
+++ Store/dataobjects/StorePaymentType.php
@@ -98,6 +98,35 @@ class StorePaymentType extends StoreDataObject
 	public $surcharge;
 
 	// }}}
+	// {{{ public function loadFromShortname()
+
+	/**
+	 * Loads a payment type by its shortname
+	 *
+	 * @param string $shortname the shortname of the payment tpye to load.
+	 */
+	public function loadFromShortname($shortname)
+	{
+		$row = null;
+
+		if ($this->table !== null) {
+			$sql = sprintf('select * from %s where shortname = %s',
+				$this->table,
+				$this->db->quote($shortname, 'text'));
+
+			$rs = SwatDB::query($this->db, $sql, null);
+			$row = $rs->fetchRow(MDB2_FETCHMODE_ASSOC);
+		}
+
+		if ($row === null)
+			return false;
+
+		$this->initFromRow($row);
+		$this->generatePropertyHashes();
+		return true;
+	}
+
+	// }}}
 	// {{{ public function isAvailableInRegion()
 
 	/**
