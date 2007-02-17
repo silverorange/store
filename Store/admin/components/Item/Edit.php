84df6b3d6deee5b96890e63d9ca1109c40a08771
Michael Gauthier <mike@silverorange.com>
2007-02-17 01:41:39 +0000

    add TODO
    svn commit r15114

--- Store/admin/components/Item/Edit.php
+++ Store/admin/components/Item/Edit.php
@@ -48,6 +48,7 @@ class StoreItemEdit extends AdminDBEdit
 			'packages/store/admin/javascript/store-item-edit-page.js',
 			Store::PACKAGE_ID));
 
+		// TODO: move this style to Store
 		$this->layout->addHtmlHeadEntry(new SwatStyleSheetHtmlHeadEntry(
 			'styles/item-edit-page.css', Store::PACKAGE_ID));
 
