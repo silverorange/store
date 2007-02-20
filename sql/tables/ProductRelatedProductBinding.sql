a49e160f450ca4ce2d748d802fd200e9b5ab28f3
Nathan Fredrickson <nathan@silverorange.com>
2007-02-20 21:15:11 +0000

    more missing on delete cascades
    svn commit r15232

--- sql/tables/ProductRelatedProductBinding.sql
+++ sql/tables/ProductRelatedProductBinding.sql
@@ -1,6 +1,6 @@
 create table ProductRelatedProductBinding (
-	source_product integer not null references Product(id),
-	related_product integer not null references Product(id),
+	source_product integer not null references Product(id) on delete cascade,
+	related_product integer not null references Product(id) on delete cascade,
 	displayorder integer not null default 0,
 	primary key(source_product, related_product)
 );
