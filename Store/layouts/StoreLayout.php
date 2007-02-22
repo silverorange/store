bb77a7ca5fc67eab7f3d6805f0e41f790ae5aa7b
Michael Gauthier <mike@silverorange.com>
2007-02-22 19:11:31 +0000

    Coding conventions. This must be old.
    svn commit r15289

--- Store/layouts/StoreLayout.php
+++ Store/layouts/StoreLayout.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once('Site/layouts/SiteLayout.php');
+require_once 'Site/layouts/SiteLayout.php';
 
 /**
  * @package   Store
- * @copyright 2006 silverorange
+ * @copyright 2006-2007 silverorange
  */
 abstract class StoreLayout extends SiteLayout
 {
