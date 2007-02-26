b8d8c643487328a5ed1ad57fb06f3680a0eb9886
Nathan Fredrickson <nathan@silverorange.com>
2007-02-26 18:37:15 +0000

    clean up
    svn commit r15381

--- sql/functions/getArticlePathInfo.sql
+++ sql/functions/getArticlePathInfo.sql
@@ -5,8 +5,8 @@
  *
  * @returned_row type_article_path_info: a row containing id, parent, shortname, title
  *
- * Returns a set of returned_rows ordered from leaf to root.
- * If the article is not found, returns an empty recordset
+ * Returns a set of type_article_path_info. The set is ordered from the leaf to the root.
+ * If the article is not found, an empty record set is returned.
  */
 CREATE TYPE type_article_path_info AS (id INTEGER, parent INTEGER, shortname VARCHAR(255), title VARCHAR(255));
 
