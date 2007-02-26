37ddade0fbd29a732833400dc2841afc900dfd34
Nathan Fredrickson <nathan@silverorange.com>
2007-02-26 18:35:22 +0000

    cleanup
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@15379 94490950-a86c-4493-9ccb-530f72933e47

--- sql/functions/getCategoryPathInfo.sql
+++ sql/functions/getCategoryPathInfo.sql
@@ -6,6 +6,7 @@
  * @returned_row type_category_path_info: a row containing id, parent, shortname, and title.
  *
  * Returns a set of type_category_path_info. The set is ordered from the leaf category to the root category.
+ * This function does not check if the parent categories are enabled.
  * If the category is not found, an empty record set is returned.
  */
 CREATE TYPE type_category_path_info AS (id INTEGER, parent INTEGER, shortname VARCHAR(255), title VARCHAR(255));
@@ -19,17 +20,16 @@ CREATE OR REPLACE FUNCTION getCategoryPathInfo(INTEGER) RETURNS SETOF type_categ
 		local_id := param_id;
 		WHILE local_id is not null LOOP
 			BEGIN
-				-- Get the results
 				SELECT INTO returned_row id, parent, shortname, title
 				FROM Category
 				WHERE id = local_id;
 
-				-- Return the navbar results
+				-- return the row
 				IF FOUND THEN
 					RETURN NEXT returned_row;
 				END IF;
 
-				-- Get next parent id.
+				-- move up the tree
 				local_id := returned_row.parent;
 			END;
 		END LOOP;
