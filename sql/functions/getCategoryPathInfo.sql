aac7e7d2e3a7d51138f149ebd4a3ba626ea464c6
Nathan Fredrickson <nathan@silverorange.com>
2007-02-26 18:24:32 +0000

    Remove veseysism from Store.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@15374 94490950-a86c-4493-9ccb-530f72933e47

--- sql/functions/getCategoryPathInfo.sql
+++ sql/functions/getCategoryPathInfo.sql
@@ -6,7 +6,7 @@
  * @returned_row type_category_path_info: a row containing id, parent, shortname, title
  *
  * Returns a set of returned_rows ordered from leaf to root category.
- * Checking if the parent categories are enabled is left up to sp_category_find.
+ * Checking if the parent categories are enabled is left up to findCategory.
  * If the category is not found, returns an empty recordset
  */
 CREATE TYPE type_category_path_info AS (id INTEGER, parent INTEGER, shortname VARCHAR(255), title VARCHAR(255));
@@ -15,24 +15,12 @@ CREATE OR REPLACE FUNCTION getCategoryPathInfo(INTEGER) RETURNS SETOF type_categ
 	DECLARE
 		param_id ALIAS FOR $1;
 		local_id INTEGER;
-		local_count INTEGER;
 		returned_row type_category_path_info%ROWTYPE;
 	BEGIN
 		local_id := param_id;
 
 		WHILE local_id is not null LOOP
 			BEGIN
-				SELECT INTO local_count count(id) FROM Category WHERE parent = local_id;
-
-				IF local_count != 0 THEN
-
-					SELECT INTO local_count count(id) FROM Category WHERE parent IN (SELECT id FROM Category WHERE parent = local_id);
-
-					IF local_count = 0 THEN
-						SELECT INTO local_count count(product) FROM CategoryProductBinding WHERE category IN (SELECT id FROM Category WHERE parent = local_id);
-					END IF;
-				END IF;
-
 				-- Get the results
 				SELECT INTO returned_row id, parent, shortname, title
 				FROM Category
@@ -45,7 +33,6 @@ CREATE OR REPLACE FUNCTION getCategoryPathInfo(INTEGER) RETURNS SETOF type_categ
 
 				-- Get next parent id.
 				local_id := returned_row.parent;
-
 			END;
 		END LOOP;
 
