141510e3ed5ceb4335a8de36cc32b13647e9b408
Michael Gauthier <mike@silverorange.com>
2007-02-28 22:24:34 +0000

    New table for storing card payment transactions
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@15497 94490950-a86c-4493-9ccb-530f72933e47

---
+++ sql/tables/PaymentTransaction.sql
@@ -0,0 +1,11 @@
+create table PaymentTransaction (
+	id serial,
+	transaction_id varchar(255),
+	security_key varchar(255),
+	createdate timestamp,
+	address_status integer,
+	postal_code_status integer,
+	card_verification_value_status integer,
+	ordernum integer not null references Orders(id),
+	primary key (id)
+);
