4a7c79b433beb4b96988e9dccea27c91c582389a
Michael Gauthier <mike@silverorange.com>
2007-03-06 14:52:04 +0000

    rename transaction_type as request_type
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@15624 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/PaymentTransaction.sql
+++ sql/tables/PaymentTransaction.sql
@@ -7,7 +7,7 @@ create table PaymentTransaction (
 	address_status integer,
 	postal_code_status integer,
 	card_verification_value_status integer,
-	transaction_type integer not null,
+	request_type integer not null,
 	ordernum integer not null references Orders(id),
 	primary key (id)
 );
