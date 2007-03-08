427cbf9187d304c739a51e62c82f02a824c1407d
Michael Gauthier <mike@silverorange.com>
2007-03-08 19:38:34 +0000

    Split majority of StoreItemStatus code into an abstract base class and remove the unuseful compare() and isEqual() methods (these methods were useful before item::getStatus() existed).
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@15718 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Store/StoreStatus.php
@@ -0,0 +1,79 @@
+<?php
+
+require_once 'Swat/SwatObject.php';
+
+/**
+ * Abstract base class for an object status
+ *
+ * This class forms the base of StoreItemStatus and StoreOrderStatus.
+ *
+ * Making statuses objects instead of simple constants has a few advantages
+ * that are not immediately obvious:
+ *
+ * First, it is easier to define a base set of statuses in the Store package
+ * and have site code provide additional statuses. Alternatives to this
+ * technique are having no statuses defined in Store, resulting in unecessarily
+ * abstract objects, and having a mix of constant namespaces
+ * (StoreOrder::STATUS_AUTHORIZED, MyOrder::STATUS_READYTOSHIP).
+ *
+ * A second advantage is that information about an object's statuses can easily
+ * be retrieved from the object itself. For example, it is easy to get the
+ * textual description of an order status:
+ *
+ * <code>
+ * echo $order->getStatus()->title;
+ * </code>
+ *
+ * @package   Store
+ * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+abstract class StoreOrderStatus extends SwatObject
+{
+	// {{{ public properties
+
+	/**
+	 * The 'value' of this status
+	 *
+	 * This is often what is saved in the database for an object.
+	 *
+	 * @var integer
+	 */
+	public $id;
+
+	/**
+	 * User visible title of this status
+	 *
+	 * @var string
+	 */
+	public $title;
+
+	/**
+	 * Mnemonic shortname used primarily when assigning statuses to objects
+	 * or when writing conditional code
+	 *
+	 * @var string
+	 */
+	public $shortname;
+
+	// }}}
+	// {{{ public function __construct()
+
+	/**
+	 * Creates a new status object
+	 *
+	 * @param integer $id the valud of this status.
+	 * @param string $shortname the mnemonic shortname of this status.
+	 * @param string $title the user visible title of this status.
+	 */
+	public function __construct($id, $shortname, $title)
+	{
+		$this->id = (integer)$id;
+		$this->shortname = (string)$shortname;
+		$this->title = (string)$title;
+	}
+
+	// }}}
+}
+
+?>
