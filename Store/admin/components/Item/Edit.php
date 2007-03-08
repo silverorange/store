05732689843f46dc5a720e0b90d8326db3adf5e4
Michael Gauthier <mike@silverorange.com>
2007-03-08 21:49:07 +0000

    require required file
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@15732 94490950-a86c-4493-9ccb-530f72933e47

--- Store/admin/components/Item/Edit.php
+++ Store/admin/components/Item/Edit.php
@@ -7,6 +7,7 @@ require_once 'SwatDB/SwatDB.php';
 require_once 'Swat/SwatMessage.php';
 require_once 'Swat/SwatYUI.php';
 require_once 'Store/StoreClassMap.php';
+require_once 'Store/StoreItemStatusList.php';
 require_once 'Store/dataobjects/StoreItem.php';
 require_once 'Store/dataobjects/StoreRegionWrapper.php';
 
