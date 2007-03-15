bb90d2f7695041dfe7d21c4090555142abdf0aaf
Michael Gauthier <mike@silverorange.com>
2007-03-15 18:34:38 +0000

    require correct file
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@15914 94490950-a86c-4493-9ccb-530f72933e47

--- Store/pages/StoreCheckoutThankYouPage.php
+++ Store/pages/StoreCheckoutThankYouPage.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'Store/pages/StoreCheckoutUIPage.php';
+require_once 'Store/pages/StoreCheckoutFinalPage.php';
 
 /**
  * Page displayed when an order is processed successfully on the checkout
