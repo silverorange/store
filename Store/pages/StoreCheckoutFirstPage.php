92d4b3ef6a6d70601a16a3427b29e8003248867e
Nathan Fredrickson <nathan@silverorange.com>
2007-03-15 17:22:03 +0000

    Add new abstract class StoreCheckoutAggregateStepPage as an intermediate page class.
    	StoreCheckoutStepPage - a base class for a step page in the checkout
    	---> StoreCheckoutAggregateStepPAge - a base class for a step page composed of embedded pages
    		---> StoreCheckoutFirstPage - a generic implementation of the first page of a one page checkout
    
    
    svn commit r15907

--- Store/pages/StoreCheckoutFirstPage.php
+++ Store/pages/StoreCheckoutFirstPage.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'Store/pages/StoreCheckoutStepPage.php';
+require_once 'Store/pages/StoreCheckoutAggregateStepPage.php';
 require_once 'Store/pages/StoreCheckoutBasicInfoPage.php';
 require_once 'Store/pages/StoreCheckoutBillingAddressPage.php';
 require_once 'Store/pages/StoreCheckoutShippingAddressPage.php';
@@ -12,17 +12,14 @@ require_once 'Store/pages/StoreCheckoutPaymentMethodPage.php';
  * @package   Store
  * @copyright 2006 silverorange
  */
-class StoreCheckoutFirstPage extends StoreCheckoutStepPage
+class StoreCheckoutFirstPage extends StoreCheckoutAggregateStepPage
 {
 	// {{{ public function __construct()
 
 	public function __construct(SiteApplication $app, SiteLayout $layout)
 	{
-		parent::__construct($app, $layout);
 		$this->ui_xml = dirname(__FILE__).'/checkout-first.xml';
-
-		foreach ($this->instantiateEmbeddedEditPages() as $page)
-			$this->registerEmbeddedEditPage($page);
+		parent::__construct($app, $layout);
 	}
 
 	// }}}
@@ -43,20 +40,6 @@ class StoreCheckoutFirstPage extends StoreCheckoutStepPage
 	// }}}
 
 	// init phase
-	// {{{ protected function loadUI()
-
-	protected function loadUI()
-	{
-		parent::loadUI();
-
-		$pages = $this->getEmbeddedEditPages();
-		foreach ($pages as $page) {
-			$container = $this->getContainer($page);
-			$this->ui->loadFromXML($page->getXml(), $container);
-		}
-	}
-
-	// }}}
 	// {{{ protected function initInternal()
 
 	protected function initInternal()
@@ -74,54 +57,6 @@ class StoreCheckoutFirstPage extends StoreCheckoutStepPage
 	protected function getProgressDependencies()
 	{
 		return array('checkout');
-	}
-
-	// }}}
-	// {{{ private function getContainer()
-
-	private function getContainer($page)
-	{
-		$class = get_class($page);
-
-		$matches = array();
-		if (preg_match('/Checkout(.*)Page$/', $class, $matches) === 1) {
-			$container_id = strtolower(
-				preg_replace('/([A-Z])/u', '_\\1', $matches[1])).'_container';
-
-			$container_id = substr($container_id, 1);
-		} else {
-			throw new StoreException(
-				"Unable to guess container for page {$class}");
-		}
-
-		$container = $this->ui->getWidget($container_id);
-		return $container;
-	}
-
-	// }}}
-
-	// process phase
-	// {{{ public function process()
-
-	public function process()
-	{
-		parent::process();
-
-		$form = $this->ui->getWidget('form');
-		if ($form->isProcessed()) {
-			if ($form->hasMessage()) {
-				$message = new SwatMessage(Store::_('There is a problem with '.
-					'the information submitted.'), SwatMessage::ERROR);
-
-				$message->secondary_content = Store::_('Please address the '.
-					'fields highlighted below and re-submit the form.');
-
-				$this->ui->getWidget('message_display')->add($message);
-			} else {
-				$this->updateProgress();
-				$this->app->relocate('checkout/confirmation');
-			}
-		}
 	}
 
 	// }}}
