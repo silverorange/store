49d727c97fc8d09bd134ac207ca1369cd033264b
Michael Gauthier <mike@silverorange.com>
2007-03-16 00:53:24 +0000

    These lines got commented out and comitted by accident.
    svn commit r15937

--- Store/pages/StoreCheckoutFinalPage.php
+++ Store/pages/StoreCheckoutFinalPage.php
@@ -17,7 +17,7 @@ abstract class StoreCheckoutFinalPage extends StoreCheckoutUIPage
 	public function init()
 	{
 		parent::init();
-//		$this->resetProgress();
+		$this->resetProgress();
 	}
 
 	// }}}
