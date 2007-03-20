c8158617ee25b5818453543f73993004d95aaf5d
Michael Gauthier <mike@silverorange.com>
2007-03-20 14:17:09 +0000

    Add LGPL license
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@16057 94490950-a86c-4493-9ccb-530f72933e47

--- Store/pages/StoreAccountEditPage.php
+++ Store/pages/StoreAccountEditPage.php
@@ -8,6 +8,7 @@ require_once 'Store/StoreUI.php';
 /**
  * @package   Store
  * @copyright 2006-2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreAccountEditPage extends StoreAccountPage
 {
