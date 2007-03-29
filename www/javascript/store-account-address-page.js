fac4bc340781532fe28aee0c0a8126abd7100c40
Michael Gauthier <mike@silverorange.com>
2007-03-29 18:30:09 +0000

    Tidy up JavaScript
    svn commit r16351

--- www/javascript/store-account-address-page.js
+++ www/javascript/store-account-address-page.js
@@ -1,30 +1,20 @@
 function StoreAccountAddressPage(id, provstate_other_index)
 {
-	this.provstate = document.getElementById('provstate');
-	this.provstate_other =
-		document.getElementById('provstate_other');
-
-	this.provstate_other_index = provstate_other_index;
-
 	this.id = id;
+
+	this.provstate = document.getElementById('provstate');
+	this.provstate_other = document.getElementById('provstate_other');
+	this.provstate_other_index = provstate_other_index;
 
 	if (this.provstate && this.provstate_other) {
 		YAHOO.util.Event.addListener(this.provstate, 'change',
 			this.provstateChangeHandler, this, true);
 
-		this.provstate_other_sensitive =
-			(this.provstate.selectedIndex == this.provstate_other_index);
-
-		if (!this.provstate_other_sensitive && this.provstate_other) {
-			this.provstate_other.disabled = true;
-			YAHOO.util.Dom.addClass(this.provstate_other,
-				'swat-insensitive');
-		}
+		this.updateFields();
 	}
 }
 
-StoreAccountAddressPage.prototype.provstateChangeHandler = function(event,
-	address)
+StoreAccountAddressPage.prototype.updateFields = function()
 {
 	this.provstate_other_sensitive =
 		(this.provstate.selectedIndex == this.provstate_other_index);
@@ -36,4 +26,10 @@ StoreAccountAddressPage.prototype.provstateChangeHandler = function(event,
 		this.provstate_other.disabled = true;
 		YAHOO.util.Dom.addClass(this.provstate_other, 'swat-insensitive');
 	}
+}
+
+StoreAccountAddressPage.prototype.provstateChangeHandler = function(event,
+	address)
+{
+	this.updateFields();
 }
