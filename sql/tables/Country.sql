43237977eb94ce18e7fc86856e86028cab7d3121
Nick Burka <nick@silverorange.com>
2007-04-02 22:32:50 +0000

    Forgot to commit the Country.show field earlier
    svn commit r16520

--- sql/tables/Country.sql
+++ sql/tables/Country.sql
@@ -2,6 +2,7 @@ create table Country (
 	-- this is the ISO-3611 two-letter country code for this country
 	id char(2),
 	title varchar(255),
+	show boolean not null default true,
 	primary key(id)
 );
 
