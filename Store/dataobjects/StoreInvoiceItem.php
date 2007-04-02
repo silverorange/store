82f2d3df6824e77a2056c690e02da8ea0e9a9fc1
Nathan Fredrickson <nathan@silverorange.com>
2007-04-02 17:52:02 +0000

    Dataobject and wrapper for invoice items.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@16491 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Store/dataobjects/StoreInvoiceItem.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once 'Store/dataobjects/StoreDataObject.php';
+
+/**
+ * An item in an invoice
+ *
+ * @package   Store
+ * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class StoreInvoiceItem extends StoreDataObject
+{
+	// {{{ public properties
+
+	/**
+	 * Unique identifier
+	 *
+	 * @var integer
+	 */
+	public $id;
+
+	/**
+	 * Merchant's stocking keeping unit (SKU)
+	 *
+	 * @var string
+	 */
+	public $sku;
+
+	/**
+	 * Quantity
+	 *
+	 * @var integer
+	 */
+	public $quantity;
+
+	/**
+	 * Price
+	 *
+	 * @var float
+	 */
+	public $price;
+
+	/**
+	 * Description
+	 *
+	 * @var string
+	 */
+	public $description;
+
+	// }}}
+	// {{{ protected function init()
+
+	protected function init()
+	{
+		$this->registerInternalProperty('invoice',
+			$this->class_map->resolveClass('StoreInvoice'));
+
+		$this->table = 'InvoiceItem';
+		$this->id_field = 'integer:id';
+	}
+
+	// }}}
+}
+
+?>
