f0f2e51c7f516436bf64bb429064409afb7610b9
Nathan Fredrickson <nathan@silverorange.com>
2007-04-10 03:54:59 +0000

    Rewrite edit pages to use dataobjects.
    svn commit r16723

--- Store/admin/components/Invoice/ItemEdit.php
+++ Store/admin/components/Invoice/ItemEdit.php
@@ -40,57 +40,56 @@ class StoreInvoiceItemEdit extends AdminDBEdit
 		parent::initInternal();
 
 		$this->ui->loadFromXML($this->ui_xml);
+		$this->initInvoice();
+		$this->initInvoiceItem();
+		$this->ui->getWidget('price')->locale = $this->invoice->locale->id;
 	}
 
 	// }}}
-	// {{{ protected function getInvoiceItem()
+	// {{{ protected function initInvoiceItem()
 
-	protected function getInvoiceItem()
+	protected function initInvoiceItem()
 	{
-		if ($this->invoice_item === null) {
-			$class_map = StoreClassMap::instance();
-			$class_name = $class_map->resolveClass('StoreInvoiceItem');
-			$this->invoice_item = new $class_name();
-			$this->invoice_item->setDatabase($this->app->db);
+		$class_map = StoreClassMap::instance();
+		$class = $class_map->resolveClass('StoreInvoiceItem');
+		$invoice_item = new $class();
+		$invoice_item->setDatabase($this->app->db);
 
-			if ($this->id === null) {
-				$this->invoice_item->invoice = $this->getInvoice();
-			} else {
-				if (!$this->invoice_item->load($this->id)) {
-					throw new AdminNotFoundException(sprintf(
-						Store::_('Invoice item with id ‘%s’ not found.'),
-						$this->id));
-				}
+		if ($this->id === null) {
+			$invoice_item->invoice = $this->invoice;
+		} else {
+			if (!$invoice_item->load($this->id)) {
+				throw new AdminNotFoundException(sprintf(
+					Store::_('Invoice item with id ‘%s’ not found.'),
+					$this->id));
 			}
 		}
 
-		return $this->invoice_item;
+		$this->invoice_item = $invoice_item;
 	}
 
 	// }}}
-	// {{{ protected function getInvoice()
+	// {{{ protected function initInvoice()
 
-	protected function getInvoice() 
+	protected function initInvoice() 
 	{
-		if ($this->invoice === null) {
-			if ($this->id === null) 
-				$invoice_id = $this->app->initVar('invoice');
-			else
-				$invoice_id = SwatDB::queryOne($this->app->db, sprintf(
-					'select invoice from InvoiceItem where id = %s',
-					$this->app->db->quote($this->id, 'integer')));
+		if ($this->id === null) 
+			$invoice_id = $this->app->initVar('invoice');
+		else
+			$invoice_id = SwatDB::queryOne($this->app->db, sprintf(
+				'select invoice from InvoiceItem where id = %s',
+				$this->app->db->quote($this->id, 'integer')));
 
-			$class_map = StoreClassMap::instance();
-			$invoice_class = $class_map->resolveClass('StoreInvoice');
-			$this->invoice = new $invoice_class();
-			$this->invoice->setDatabase($this->app->db);
-			if (!$this->invoice->load($invoice_id))
-				throw new AdminNotFoundException(sprintf(
-					Store::_('Invoice with id ‘%d’ not found.'),
-					$this->id));
-		}
+		$class_map = StoreClassMap::instance();
+		$class = $class_map->resolveClass('StoreInvoice');
+		$invoice = new $class();
+		$invoice->setDatabase($this->app->db);
+		if (!$invoice->load($invoice_id))
+			throw new AdminNotFoundException(sprintf(
+				Store::_('Invoice with id ‘%s’ not found.'),
+				$this->id));
 
-		return $this->invoice;
+		$this->invoice = $invoice;
 	}
 
 	// }}}
@@ -100,16 +99,17 @@ class StoreInvoiceItemEdit extends AdminDBEdit
 
 	protected function saveDBData()
 	{
-		$invoice_item = $this->getInvoiceItem();
-		$invoice_item->sku = $this->ui->getWidget('sku')->value;
-		$invoice_item->description = $this->ui->getWidget('description')->value;
-		$invoice_item->quantity = $this->ui->getWidget('quantity')->value;
-		$invoice_item->price = $this->ui->getWidget('price')->value;
-		$invoice_item->save();
+		$this->invoice_item->sku = $this->ui->getWidget('sku')->value;
+		$this->invoice_item->quantity = $this->ui->getWidget('quantity')->value;
+		$this->invoice_item->price = $this->ui->getWidget('price')->value;
+		$this->invoice_item->description =
+			$this->ui->getWidget('description')->value;
+
+		$this->invoice_item->save();
 
 		$message = new SwatMessage(sprintf(
 			Store::_('“%s” has been saved.'),
-			$invoice_item->getDetailedDescription()));
+			$this->invoice_item->getDetailedDescription()));
 
 		$this->app->messages->add($message);
 	}
@@ -124,7 +124,7 @@ class StoreInvoiceItemEdit extends AdminDBEdit
 		parent::buildInternal();
 		$frame = $this->ui->getWidget('edit_frame');
 		$frame->subtitle = sprintf(Store::_('Invoice %s'),
-			$this->getInvoice()->id);
+			$this->invoice->id);
 	}
 
 	// }}}
@@ -134,7 +134,7 @@ class StoreInvoiceItemEdit extends AdminDBEdit
 	{
 		parent::buildForm();
 		$form = $this->ui->getWidget('edit_form');
-		$form->addHiddenField('invoice', $this->getInvoice()->id);
+		$form->addHiddenField('invoice', $this->invoice->id);
 	}
 
 	// }}}
@@ -144,20 +144,18 @@ class StoreInvoiceItemEdit extends AdminDBEdit
 	{
 		parent::buildNavBar();
 
-		$invoice = $this->getInvoice();
-
 		$this->navbar->popEntry();
 
 		$this->navbar->replaceEntryByPosition(1,
 			new SwatNavBarEntry(Store::_('Customer Accounts'), 'Account'));
 
 		$this->navbar->addEntry(new SwatNavBarEntry(
-			$invoice->account->fullname,
-			sprintf('Account/Details?id=%s', $invoice->account->id)));
+			$this->invoice->account->fullname,
+			sprintf('Account/Details?id=%s', $this->invoice->account->id)));
 
 		$this->navbar->addEntry(new SwatNavBarEntry(
-			sprintf(Store::_('Invoice %s'), $invoice->id),
-			sprintf('Invoice/Details?id=%s', $invoice->id)));
+			sprintf(Store::_('Invoice %s'), $this->invoice->id),
+			sprintf('Invoice/Details?id=%s', $this->invoice->id)));
 
 		$title = ($this->id === null) ?
 			Store::_('Add Invoice Item') : Store::_('Edit Invoice Item');
@@ -170,12 +168,7 @@ class StoreInvoiceItemEdit extends AdminDBEdit
 
 	protected function loadDBData()
 	{
-		$invoice_item = $this->getInvoiceItem();
-
-		$this->ui->getWidget('sku')->value = $invoice_item->sku;
-		$this->ui->getWidget('description')->value = $invoice_item->description;
-		$this->ui->getWidget('quantity')->value = $invoice_item->quantity;
-		$this->ui->getWidget('price')->value = $invoice_item->price;
+		$this->ui->setValues(get_object_vars($this->invoice_item));
 	}
 
 	// }}}
