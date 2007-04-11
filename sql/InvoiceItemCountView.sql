9f8062a77dfd9ac33fb7e126a660ad8d0b4fb1f9
Michael Gauthier <mike@silverorange.com>
2007-04-11 05:20:34 +0000

    This was committed in the wrong location.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@16762 94490950-a86c-4493-9ccb-530f72933e47

--- sql/InvoiceItemCountView.sql
+++
@@ -1,4 +0,0 @@
-create view InvoiceItemCountView as
-	select Invoice.id, count(InvoiceItem.id) as item_count from InvoiceItem
-		inner join Invoice on InvoiceItem.invoice = Invoice.id
-	group by Invoice.id;
