3925219c133b23a0159bbf6e165e14b8324fd939
Nathan Fredrickson <nathan@silverorange.com>
2007-04-19 19:37:06 +0000

    prepare for release of 1.8.0
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@17004 94490950-a86c-4493-9ccb-530f72933e47

--- package.php
+++ package.php
@@ -2,7 +2,7 @@
 
 require_once 'PEAR/PackageFileManager2.php';
 
-$version = '1.7.4';
+$version = '1.8.0';
 $notes = <<<EOT
 see ChangeLog
 EOT;
@@ -53,11 +53,11 @@ $package->addReplacement('Store/Store.php', 'pear-config', '@DATA-DIR@', 'data_d
 
 $package->setPhpDep('5.1.5');
 $package->setPearinstallerDep('1.4.0');
-$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.2.12');
+$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.2.16');
 $package->addPackageDepWithChannel('required', 'Site', 'pear.silverorange.com', '1.0.21');
 $package->addPackageDepWithChannel('required', 'Admin', 'pear.silverorange.com', '1.1.4');
-$package->addPackageDepWithChannel('required', 'XML_RPCAjax', 'pear.silverorange.com', '1.0.2');
-$package->addPackageDepWithChannel('required', 'Yui', 'pear.silverorange.com', '1.0.1');
+$package->addPackageDepWithChannel('required', 'XML_RPCAjax', 'pear.silverorange.com', '1.0.5');
+$package->addPackageDepWithChannel('required', 'Yui', 'pear.silverorange.com', '1.0.4');
 $package->addPackageDepWithChannel('required', 'Crypt_GPG', 'pear.silverorange.com', '0.3.3');
 $package->addPackageDepWithChannel('required', 'Text_Password', 'pear.php.net', '1.1.0');
 $package->addPackageDepWithChannel('required', 'Image_Transform', 'pear.silverorange.com', '0.9.0so2');
