83dc0c8dd8eb925beae95fab5eb708653ff12782
Michael Gauthier <mike@silverorange.com>
2007-05-30 19:13:40 +0000

    Documentation and coding conventions fixes.
    svn commit r17812

--- Store/StoreUI.php
+++ Store/StoreUI.php
@@ -3,15 +3,16 @@
 require_once 'Swat/SwatUI.php';
 
 /**
- * UI manager for stores
+ * UI manager for the store package
  *
  * Subclass of {@link SwatUI} for use with the Store package.  This can be used
- * as a central place to add {@link SwatUI::$classmap class maps} and 
+ * as a central place to add {@link SwatUI::$class_map class maps} and 
  * {@link SwatUI::registerHandler() UI handlers} that are specific to the Store
  * package.
  *
- * @package Store
- * @copyright silverorange 2006
+ * @package   Store
+ * @copyright 2006-2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreUI extends SwatUI
 {
