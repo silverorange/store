silverorange/store

commit 633959b530b776aa8d86914014d87fe27e8f2bbf

Part of ticket #1044, pages using old methods of adding options are now fixed and work with the new SwatCheckboEntryList
git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@18119 94490950-a86c-4493-9ccb-530f72933e47

Store/admin/components/Article/Edit.php
@@ -182,8 +182,9 @@ class StoreArticleEdit extends AdminDBEdit
 		$form->addHiddenField('parent', $this->parent);
 
 		$regions = $this->ui->getWidget('regions');
-		$regions->options = SwatDB::getOptionArray($this->app->db,
+		$regions_options = SwatDB::getOptionArray($this->app->db,
 			'Region', 'text:title', 'integer:id');
+		$regions->addOptionsByArray($regions_options);
 
 		// default region visibility
 		if ($this->id === null)

Store/admin/components/PaymentType/Edit.php
@@ -32,8 +32,9 @@ class StorePaymentTypeEdit extends AdminDBEdit
 		$this->fields = array('title', 'shortname');
 
 		$region_list = $this->ui->getWidget('regions');
-		$region_list->options = SwatDB::getOptionArray($this->app->db,
+		$region_list_options = SwatDB::getOptionArray($this->app->db,
 			'Region', 'title', 'id', 'title');
+		$region_list->addOptionsByArray($region_list_options);
 
 		if ($this->id === null)
 			$this->ui->getWidget('shortname_field')->visible = false;

Store/admin/components/Region/Edit.php
@@ -36,12 +36,12 @@ class StoreRegionEdit extends AdminDBEdit
 		$region_billing_country_list =
 			$this->ui->getWidget('region_billing_country');
 
-		$region_billing_country_list->options = $countries;
+		$region_billing_country_list->addOptionsByArray($countries);
 
 		$region_shipping_country_list = 
 			$this->ui->getWidget('region_shipping_country');
 
-		$region_shipping_country_list->options = $countries;
+		$region_shipping_country_list->addOptionsByArray($countries);
 
 		$this->fields = array('title');
 	}

Store/admin/components/Article/Search.php
@@ -41,8 +41,8 @@ class StoreArticleSearch extends AdminSearch
 		$regions = SwatDB::query($this->app->db, $regions_sql);
 		$search_regions = $this->ui->getWidget('search_regions');
 		foreach ($regions as $region) {
-			$search_regions->options[$region->id] = $region->title;
-			$search_regions->values[] = $region->id;
+			$search_regions->addOption($region->id, $region->title);
+			$search_regions->values[] = $region->id;			
 		}
 
 		$this->ui->getWidget('article_region_action')->db = $this->app->db;

commit 4339803f37f43068611e8388b4cde77c0d9a3d26

fix defaulting of regions - you can't flip an array of objects, which is what ->options is - noticed this with a real exception on the live site here: http://stage2.silverorange.com/exceptions/veseys2/2007-07-04/error-11d687ffc8d90ccea32b4746f8c1a660.html - this was accidently broken in r18119
svn commit r18266

Store/admin/components/Article/Edit.php
@@ -189,7 +189,7 @@ class StoreArticleEdit extends AdminDBEdit
 
 		// default region visibility
 		if ($this->id === null)
-			$regions->values = array_flip($regions->options);
+			$regions->values = array_flip($regions_options);
 	}
 
 	// }}}