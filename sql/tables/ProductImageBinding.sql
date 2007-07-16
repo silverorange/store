550b054d5d36a6d79019759546d51a4e58f1fdb5
Nathan Fredrickson <nathan@silverorange.com>
2007-07-16 14:47:52 +0000

    typo
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@18451 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/ProductImageBinding.sql
+++ sql/tables/ProductImageBinding.sql
@@ -1,6 +1,6 @@
 create table ProductImageBinding (
 	product integer not null references Product(id) on delete cascade,
-	image integer not null references Image(id) on delete casacde,
+	image integer not null references Image(id) on delete cascade,
 	displayorder integer not null default 0,
 	primary key (product, image)
 );
