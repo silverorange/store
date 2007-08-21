f4b07fd495f90d9dd62d764e7cc37612806784f6
Nick Burka <nick@silverorange.com>
2007-08-21 20:49:20 +0000

    A simplified Article component derived from site. All remaining code here is concerned with Region.
    svn commit r19568

--- Store/admin/components/Article/Edit.php
+++ Store/admin/components/Article/Edit.php
@@ -1,9 +1,6 @@
 <?php
 
-require_once 'Admin/pages/AdminDBEdit.php';
-require_once 'Admin/exceptions/AdminNotFoundException.php';
-require_once 'SwatDB/SwatDB.php';
-require_once 'Swat/SwatDate.php';
+require_once 'Site/admin/components/Article/Edit.php';
 
 /**
  * Edit page for Articles
@@ -12,16 +9,14 @@ require_once 'Swat/SwatDate.php';
  * @copyright 2005-2007 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class StoreArticleEdit extends AdminDBEdit
+class StoreArticleEdit extends SiteArticleEdit
 {
 	// {{{ protected properties 
 
-	protected $fields;
-
-	// }}}
-	// {{{ private properties 
-
-	private $parent;
+	/**
+	 * @var string
+	 */
+	protected $ui_xml = 'Store/admin/components/Article/edit.xml';
 
 	// }}}
 
@@ -33,108 +28,18 @@ class StoreArticleEdit extends AdminDBEdit
 		parent::initInternal();
 
 		$this->ui->mapClassPrefixToPath('Store', 'Store');
-		$this->ui->loadFromXML(dirname(__FILE__).'/edit.xml');
-
-		$this->fields = array(
-			'title',
-			'shortname',
-			'description',
-			'bodytext',
-			'boolean:show',
-			'boolean:searchable',
-			'date:modified_date',
-		);
-
-		$this->parent = SiteApplication::initVar('parent');
-
-		if ($this->id === null) {
-			$this->ui->getWidget('shortname_field')->visible = false;
-		} else {
-			$sql = sprintf('select parent from Article where id = %s',
-				$this->app->db->quote($this->id, 'integer'));
-
-			$this->parent = SwatDB::queryOne($this->app->db, $sql);
-		}
 	}
 
 	// }}}
 
 	// process phase
-	// {{{ protected function validate()
-
-	protected function validate()
-	{
-		$shortname = $this->ui->getWidget('shortname')->value;
-
-		if ($this->id === null && $shortname === null) {
-			$shortname = $this->generateShortname(
-				$this->ui->getWidget('title')->value);
-			$this->ui->getWidget('shortname')->value = $shortname;
-
-		} elseif (!$this->validateShortname($shortname)) {
-			$message = new SwatMessage(
-				Store::_('Shortname already exists and must be unique.'), 
-				SwatMessage::ERROR);
-
-			$this->ui->getWidget('shortname')->addMessage($message);
-		}
-	}
-
-	// }}}
-	// {{{ protected function validateShortname()
-
-	protected function validateShortname($shortname)
-	{
-		$sql = 'select shortname from Article
-			where shortname = %s and parent %s %s and id %s %s';
-
-		$sql = sprintf($sql,
-			$this->app->db->quote($shortname, 'text'),
-			SwatDB::equalityOperator($this->parent, false),
-			$this->app->db->quote($this->parent, 'integer'),
-			SwatDB::equalityOperator($this->id, true),
-			$this->app->db->quote($this->id, 'integer'));
-
-		$query = SwatDB::query($this->app->db, $sql);
-
-		return (count($query) == 0);
-	}
-
-	// }}}
 	// {{{ protected function saveDBData()
 
 	protected function saveDBData()
 	{
-		$values = $this->ui->getValues(array('title', 'shortname', 'bodytext',
-			'description', 'show', 'searchable'));
-
-		$now = new SwatDate();
-		$now->toUTC();
-		$values['modified_date'] = $now->getDate();
-
-		if ($this->id === null) {
-			$this->fields[] = 'date:createdate';
-			$values['createdate'] = $now->getDate();
-			
-			$this->fields[] = 'integer:parent';
-			$values['parent'] = 
-				$this->ui->getWidget('edit_form')->getHiddenField('parent');
-			
-			$this->id = SwatDB::insertRow($this->app->db, 'Article',
-				$this->fields, $values, 'integer:id');
-		} else {
-			SwatDB::updateRow($this->app->db, 'Article', $this->fields, 
-				$values, 'integer:id', $this->id);
-		}
+		parent::saveDBData();
 
 		$this->saveRegions();
-
-		$this->addToSearchQueue();
-
-		$message = new SwatMessage(
-			sprintf(Store::_('“%s” has been saved.'), $values['title']));
-
-		$this->app->messages->add($message);
 	}
 
 	// }}}
@@ -150,26 +55,6 @@ class StoreArticleEdit extends AdminDBEdit
 	}
 
 	// }}}
-	// {{{ protected function addToSearchQueue()
-
-	protected function addToSearchQueue()
-	{
-		$sql = sprintf('delete from NateGoSearchQueue
-			where document_id = %s and document_type = %s',
-			$this->app->db->quote($this->id, 'integer'),
-			$this->app->db->quote(1, 'integer'));
-
-		SwatDB::exec($this->app->db, $sql);
-
-		$sql = sprintf('insert into NateGoSearchQueue
-			(document_id, document_type) values (%s, %s)',
-			$this->app->db->quote($this->id, 'integer'),
-			$this->app->db->quote(1, 'integer'));
-
-		SwatDB::exec($this->app->db, $sql);
-	}
-
-	// }}}
 
 	// build phase
 	// {{{ protected function buildInternal()
@@ -177,9 +62,6 @@ class StoreArticleEdit extends AdminDBEdit
 	protected function buildInternal()
 	{
 		parent::buildInternal();
-
-		$form = $this->ui->getWidget('edit_form');
-		$form->addHiddenField('parent', $this->parent);
 
 		$regions = $this->ui->getWidget('regions');
 		$regions_options = SwatDB::getOptionArray($this->app->db,
@@ -197,15 +79,7 @@ class StoreArticleEdit extends AdminDBEdit
 
 	protected function loadDBData()
 	{
-		$row = SwatDB::queryRowFromTable($this->app->db, 'Article',
-			$this->fields, 'integer:id', $this->id);
-
-		if ($row === null)
-			throw new AdminNotFoundException(
-				sprintf(Store::_('Article with id ‘%s’ not found.'),
-					$this->id));
-
-		$this->ui->setValues(get_object_vars($row));
+		parent::loadDBData();
 
 		$this->loadRegions();
 	}
@@ -225,31 +99,6 @@ class StoreArticleEdit extends AdminDBEdit
 
 		foreach ($bindings as $binding)
 			$regions_list->values[] = $binding->region;
-	}
-
-	// }}}
-	// {{{ protected function buildNavBar()
-
-	protected function buildNavBar()
-	{
-		if ($this->id !== null) {
-			$navbar_rs = SwatDB::executeStoredProc($this->app->db,
-				'getArticleNavBar', array($this->id));
-
-			foreach ($navbar_rs as $elem)
-				$this->navbar->addEntry(new SwatNavBarEntry($elem->title,
-					'Article/Index?id='.$elem->id));
-
-		} elseif ($this->parent !== null) {
-			$navbar_rs = SwatDB::executeStoredProc($this->app->db,
-				'getArticleNavBar', array($this->parent));
-
-			foreach ($navbar_rs as $elem)
-				$this->navbar->addEntry(new SwatNavBarEntry($elem->title,
-					'Article/Index?id='.$elem->id));
-		}
-
-		parent::buildNavBar();
 	}
 
 	// }}}
--- Store/admin/components/Article/Search.php
+++ Store/admin/components/Article/Search.php
@@ -1,13 +1,9 @@
 <?php
 
-require_once 'Admin/pages/AdminSearch.php';
-require_once 'Admin/AdminSearchClause.php';
-require_once 'SwatDB/SwatDB.php';
-require_once 'NateGoSearch/NateGoSearchQuery.php';
+require_once 'Site/admin/components/Article/Search.php';
 
 require_once 'include/StoreArticleActionsProcessor.php';
 require_once 'include/StoreArticleRegionAction.php';
-require_once 'include/StoreArticleVisibilityCellRenderer.php';
 
 /**
  * Search page for Articles
@@ -16,13 +12,14 @@ require_once 'include/StoreArticleVisibilityCellRenderer.php';
  * @copyright 2005-2007 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class StoreArticleSearch extends AdminSearch
+class StoreArticleSearch extends SiteArticleSearch
 {
 	// {{{ protected properties
 
-	protected $where_clause;
-	protected $join_clause;
-	protected $order_by_clause;
+	/**
+	 * @var string
+	 */
+	protected $ui_xml = 'Store/admin/components/Article/search.xml';
 
 	// }}}
 
@@ -34,7 +31,6 @@ class StoreArticleSearch extends AdminSearch
 		parent::initInternal();
 
 		$this->ui->mapClassPrefixToPath('Store', 'Store');
-		$this->ui->loadFromXML(dirname(__FILE__).'/search.xml');
 
 		$regions_sql = 'select id, title from Region';
 		$regions = SwatDB::query($this->app->db, $regions_sql);
@@ -45,29 +41,11 @@ class StoreArticleSearch extends AdminSearch
 		}
 
 		$this->ui->getWidget('article_region_action')->db = $this->app->db;
-
-		$this->navbar->createEntry(Store::_('Search'));
 	}
 
 	// }}}
 
 	// process phase
-	// {{{ protected function processInternal()
-
-	protected function processInternal()
-	{
-		parent::processInternal();
-
-		$pager = $this->ui->getWidget('pager');
-		$pager->process();
-
-		if ($pager->getCurrentPage() > 0) {
-			$disclosure = $this->ui->getWidget('search_disclosure');
-			$disclosure->open = false;
-		}
-	}
-
-	// }}}
 	// {{{ protected function processActions()
 
 	protected function processActions(SwatTableView $view, SwatActions $actions)
@@ -79,43 +57,12 @@ class StoreArticleSearch extends AdminSearch
 	// }}}
 
 	// build phase
-	// {{{ protected function buildInternal()
-
-	protected function buildInternal() 
-	{
-		parent::buildInternal();
-
-		$this->ui->getWidget('visibility')->addOptionsByArray(
-			StoreArticleActionsProcessor::getActions());
-	}
-
-	// }}}
 	// {{{ protected function getWhereClause()
 
 	protected function getWhereClause()
 	{
 		if ($this->where_clause === null) {
-			$where = '1 = 1';
-
-			// keywords are included in the where clause if fulltext searching
-			// is turned off
-			if ($this->getArticleSearchType() === null) {
-				$where.= ' and (';
-
-				$clause = new AdminSearchClause('title');
-				$clause->table = 'Article';
-				$clause->value = $this->ui->getWidget('search_keywords')->value;
-				$clause->operator = AdminSearchClause::OP_CONTAINS;
-				$where.= $clause->getClause($this->app->db, '');
-
-				$clause = new AdminSearchClause('bodytext');
-				$clause->table = 'Article';
-				$clause->value = $this->ui->getWidget('search_keywords')->value;
-				$clause->operator = AdminSearchClause::OP_CONTAINS;
-				$where.= $clause->getClause($this->app->db, 'or');
-
-				$where.= ') ';
-			}
+			$where = parent::getWhereClause();
 
 			$search_regions = $this->ui->getWidget('search_regions');
 			foreach ($search_regions->options as $value => $title) {
@@ -132,115 +79,10 @@ class StoreArticleSearch extends AdminSearch
 				}
 			}
 
-			$clause = new AdminSearchClause('boolean:show');
-			$clause->value = 
-				$this->ui->getWidget('search_show')->value;
-
-			$where.= $clause->getClause($this->app->db);
-
-			$clause = new AdminSearchClause('boolean:searchable');
-			$clause->value = 
-				$this->ui->getWidget('search_searchable')->value;
-
-			$where.= $clause->getClause($this->app->db);
-
 			$this->where = $where;
 		}
 
 		return $this->where;
-	}
-
-	// }}}
-	// {{{ protected function getTableModel()
-
-	protected function getTableModel(SwatView $view)
-	{
-		$this->searchArticles();
-
-		$sql = sprintf('select count(id) from Article %s where %s',
-			$this->join_clause,
-			$this->getWhereClause());
-
-		$pager = $this->ui->getWidget('pager');
-		$pager->total_records = SwatDB::queryOne($this->app->db, $sql);
-
-		$sql = 'select Article.id,
-					Article.title, 
-					Article.show,
-					Article.searchable
-				from Article
-				%s
-				where %s
-				order by %s';
-
-		$sql = sprintf($sql,
-			$this->join_clause,
-			$this->getWhereClause(),
-			$this->getOrderByClause($view, $this->order_by_clause));
-
-		$this->app->db->setLimit($pager->page_size, $pager->current_record);
-		$rs = SwatDB::query($this->app->db, $sql);
-
-		$this->ui->getWidget('results_frame')->visible = true;
-		$view = $this->ui->getWidget('index_view');
-		$view->getColumn('visibility')->getRendererByPosition()->db =
-			$this->app->db;
-
-		if (count($rs) > 0)
-			$this->ui->getWidget('results_message')->content =
-				$pager->getResultsMessage(Store::_('result'), 
-					Store::_('results'));
-
-		return $rs;
-	}
-
-	// }}}
-	// {{{ protected function searchArticles()
-
-	protected function searchArticles()
-	{
-		$keywords = $this->ui->getWidget('search_keywords')->value;
-		if (strlen(trim($keywords)) > 0 &&
-			$this->getArticleSearchType() !== null) {
-
-			$query = new NateGoSearchQuery($this->app->db);
-			$query->addDocumentType($this->getArticleSearchType());
-			$query->addBlockedWords(
-				NateGoSearchQuery::getDefaultBlockedWords());
-
-			$result = $query->query($keywords);
-
-			$this->join_clause = sprintf(
-				'inner join %1$s on
-					%1$s.document_id = Article.id and
-					%1$s.unique_id = %2$s and %1$s.document_type = %3$s',
-				$result->getResultTable(),
-				$this->app->db->quote($result->getUniqueId(), 'text'),
-				$this->app->db->quote($this->getArticleSearchType(),
-					'integer'));
-
-			$this->order_by_clause =
-				sprintf('%1$s.displayorder1, %1$s.displayorder2, Article.title',
-					$result->getResultTable());
-		} else {
-			$this->join_clause = '';
-			$this->order_by_clause = 'Article.title';
-		}
-	}
-
-	// }}}
-	// {{{ protected function getArticleSearchType()
-
-	/**
-	 * Gets the search type for articles for this web-application
-	 *
-	 * @return integer the search type for articles for this web-application or
-	 *                  null if fulltext searching is not implemented for the
-	 *                  current application.
-	 */
-	protected function getArticleSearchType()
-	{
-		return null;
 	}
 
 	// }}}
