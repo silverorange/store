d8d8339c3d15f5f13edf7c216ca9db92421552ef
Nick Burka <nick@silverorange.com>
2007-08-21 20:49:20 +0000

    A simplified Article component derived from site. All remaining code here is concerned with Region.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@19568 94490950-a86c-4493-9ccb-530f72933e47

--- Store/admin/components/Article/Index.php
+++ Store/admin/components/Article/Index.php
@@ -1,13 +1,9 @@
 <?php
 
-require_once 'Admin/pages/AdminIndex.php';
-require_once 'Admin/exceptions/AdminNotFoundException.php';
-require_once 'SwatDB/SwatDB.php';
-require_once 'Swat/SwatString.php';
+require_once 'Site/admin/components/Article/Index.php';
 
 require_once 'include/StoreArticleActionsProcessor.php';
 require_once 'include/StoreArticleRegionAction.php';
-require_once 'include/StoreArticleVisibilityCellRenderer.php';
 
 /**
  * Index page for Articles
@@ -16,12 +12,14 @@ require_once 'include/StoreArticleVisibilityCellRenderer.php';
  * @copyright 2005-2007 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class StoreArticleIndex extends AdminIndex 
+class StoreArticleIndex extends SiteArticleIndex 
 {
-	// {{{ private properties
+	// {{{ protected properties
 
-	private $id = null;
-	private $parent = null;
+	/**
+	 * @var string
+	 */
+	protected $ui_xml = 'Store/admin/components/Article/index.xml';
 
 	// }}}
 
@@ -30,10 +28,9 @@ class StoreArticleIndex extends AdminIndex
 
 	protected function initInternal()
 	{
+		parent::initInternal();
+
 		$this->ui->mapClassPrefixToPath('Store', 'Store');
-		$this->ui->loadFromXML(dirname(__FILE__).'/index.xml');
-		
-		$this->id = SiteApplication::initVar('id');
 		
 		$view = $this->ui->getWidget('index_view');
 		$this->ui->getWidget('article_region_action')->db = $this->app->db;
@@ -53,137 +50,11 @@ class StoreArticleIndex extends AdminIndex
 	// }}}
 
 	// build phase
-	// {{{ protected function getTableModel()
-
-	protected function getTableModel(SwatView $view)
-	{
-		$sql = 'select Article.id,
-					Article.title, 
-					Article.show,
-					Article.searchable,
-					ArticleChildCountView.child_count
-				from Article
-					left outer join ArticleChildCountView on
-						ArticleChildCountView.article = Article.id
-				where Article.parent %s %s
-				order by %s';
-
-		$sql = sprintf($sql,
-			SwatDB::equalityOperator($this->id),
-			$this->app->db->quote($this->id, 'integer'),
-			$this->getOrderByClause($view, 
-				'Article.displayorder, Article.title', 'Article'));
-		
-		$rs = SwatDB::query($this->app->db, $sql);
-
-		$view = $this->ui->getWidget('index_view');
-		$view->getColumn('visibility')->getFirstRenderer()->db =
-			$this->app->db;
-
-		if (count($rs) < 2)
-			$this->ui->getWidget('articles_order')->sensitive = false;
-
-		return $rs;
-	}
-
-	// }}}
 	// {{{ protected function buildInternal()
 
 	protected function buildInternal() 
 	{
 		parent::buildInternal();
-
-		$articles_frame = $this->ui->getWidget('articles_frame');
-
-		if ($this->id != 0) {
-			// show the detail frame
-			$details_frame = $this->ui->getWidget('details_frame');
-			$details_frame->visible = true;
-			
-			// move the articles frame inside of the detail frame
-			$articles_frame->parent->remove($articles_frame);
-			$details_frame->add($articles_frame);
-			$articles_frame->title = Store::_('Sub-Articles');
-			$this->ui->getWidget('articles_new')->title =
-				Store::_('New Sub-Article');
-
-			$this->buildDetails();
-		}
-
-		$tool_value = ($this->id === null) ? '' : '?parent='.$this->id;	
-		$this->ui->getWidget('articles_toolbar')->setToolLinkValues(
-			$tool_value);
-
-		$this->ui->getWidget('visibility')->addOptionsByArray(
-			StoreArticleActionsProcessor::getActions());
-	}
-
-	// }}}
-	// {{{ protected function buildDetails()
-
-	protected function buildDetails() 
-	{
-		$details_block = $this->ui->getWidget('details_block');
-		$details_view = $this->ui->getWidget('details_view');
-		$details_frame = $this->ui->getWidget('details_frame');
-
-		// set default time zone
-		$createdate_field = $details_view->getField('createdate');
-		$createdate_renderer = $createdate_field->getFirstRenderer();
-		$createdate_renderer->display_time_zone =
-			$this->app->default_time_zone;
-
-		$modified_date_field = $details_view->getField('modified_date');
-		$modified_date_renderer =
-			$modified_date_field->getRendererByPosition();
-
-		$modified_date_renderer->display_time_zone =
-			$this->app->default_time_zone;
-
-		$details_view->getField('visibility')->getFirstRenderer()->db =
-			$this->app->db;
-
-		$fields = array('id', 'title', 'shortname', 'description', 'bodytext',
-			'show', 'parent', 'createdate', 'modified_date', 'searchable');
-
-		$row = SwatDB::queryRowFromTable($this->app->db, 'Article', $fields, 
-			'id' , $this->id);
-
-		if ($row === null)
-			throw new AdminNotFoundException(
-				sprintf(Store::_('Article with id ‘%s’ not found.'),
-					$this->id));
-
-		if ($row->bodytext !== null)
-			$row->bodytext = SwatString::condense(SwatString::toXHTML(
-				$row->bodytext));
-
-		if ($row->description !== null)
-			$row->description = SwatString::condense(SwatString::toXHTML(
-				$row->description));
-
-		$details_frame->title = Store::_('Article');
-		$details_frame->subtitle = $row->title;
-		$details_view->data = &$row;
-
-		// set link id
-		$this->ui->getWidget('details_toolbar')->setToolLinkValues($this->id);
-
-		// build navbar
-		$this->navbar->popEntry();
-		$this->navbar->addEntry(new SwatNavBarEntry(Store::_('Articles'),
-			'Article'));
-
-		if ($row->parent != null) {
-			$navbar_rs = SwatDB::executeStoredProc($this->app->db, 
-				'getArticleNavBar', array($row->parent));
-
-			foreach ($navbar_rs as $elem)
-				$this->navbar->addEntry(new SwatNavBarEntry($elem->title,
-					'Article/Index?id='.$elem->id));
-		}
-
-		$this->navbar->addEntry(new SwatNavBarEntry($row->title));
 	}
 
 	// }}}
--- Store/admin/components/Article/include/StoreArticleActionsProcessor.php
+++ Store/admin/components/Article/include/StoreArticleActionsProcessor.php
@@ -1,7 +1,6 @@
 <?php
 
-require_once 'Admin/pages/AdminPage.php';
-require_once 'SwatDB/SwatDB.php';
+require_once 'Site/admin/components/Article/include/SiteArticleActionsProcessor.php';
 
 /**
  * Processes actions on articles
@@ -13,31 +12,8 @@ require_once 'SwatDB/SwatDB.php';
  * @copyright 2005-2007 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class StoreArticleActionsProcessor
+class StoreArticleActionsProcessor extends SiteArticleActionsProcessor
 {
-	// {{{ private properties
-
-	/**
-	 * A reference to the page that is using this action processor
-	 *
-	 * @var AdminPage
-	 */
-	private $page;
-
-	// }}}
-	// {{{ public function __construct()
-
-	/**
-	 * Creates a new article action processor
-	 *
-	 * @param AdminPage $page the page that is using this action processor
-	 */
-	public function __construct(AdminPage $page)
-	{
-		$this->page = $page;
-	}
-
-	// }}}
 	// {{{ public function process()
 
 	/**
@@ -48,15 +24,12 @@ class StoreArticleActionsProcessor
 	 */
 	public function process($view, $actions)
 	{
+		parent::process($view, $actions);
+
 		$num = count($view->checked_items);
 		$message = null;
 
 		switch ($actions->selected->id) {
-		case 'delete':
-			$this->page->app->replacePage('Article/Delete');
-			$this->page->app->getPage()->setItems($view->checked_items);
-			break;
-
 		case 'accessibility_action':
 			$processor =
 				$actions->selected->widget->getChild('article_region_action');
@@ -70,95 +43,10 @@ class StoreArticleActionsProcessor
 				SwatString::numberFormat($num)));
 
 			break;
-
-		case 'visibility_action' :
-			$visibility = $actions->selected->widget->getChild('visibility');
-			switch ($visibility->value) {
-			case 'show_in_menu':
-				SwatDB::updateColumn($this->page->app->db, 'Article',
-					'boolean:show', true, 'id', $view->checked_items);
-
-				$message = new SwatMessage(sprintf(Store::ngettext(
-					'One article has been shown in the menu.',
-					'%d articles have been shown in the menu.', $num),
-					SwatString::numberFormat($num)));
-				
-				break;
-
-			case 'hide_from_menu':
-				SwatDB::updateColumn($this->page->app->db, 'Article',
-					'boolean:show', false, 'id', $view->checked_items);
-
-				$message = new SwatMessage(sprintf(Store::ngettext(
-					'One article has been hidden from the menu.',
-					'%d articles have been hidden from the menu.', $num),
-					SwatString::numberFormat($num)));
-
-				break;
-
-			case 'show_in_search':
-				SwatDB::updateColumn($this->page->app->db, 'Article',
-					'boolean:searchable', true, 'id', $view->checked_items);
-
-				$message = new SwatMessage(sprintf(Store::ngettext(
-					'One article has been made searchable.',
-					'%d articles have been made searchable.', $num),
-					SwatString::numberFormat($num)));
-				
-				break;
-
-			case 'hide_from_search':
-				SwatDB::updateColumn($this->page->app->db, 'Article',
-					'boolean:searchable', false, 'id', $view->checked_items);
-
-				$message = new SwatMessage(sprintf(Store::ngettext(
-					'One article has been hidden from the search results.',
-					'%d articles have been hidden from the search results.',
-					$num), SwatString::numberFormat($num)));
-
-				break;
-
-			case 'enable':
-				SwatDB::updateColumn($this->page->app->db, 'Article',
-					'boolean:enabled', true, 'id', $view->checked_items);
-
-				$message = new SwatMessage(sprintf(Store::ngettext(
-					'One article has been enabled.',
-					'%d articles have been enabled.', $num),
-					SwatString::numberFormat($num)));
-
-				break;
-
-			case 'disable':
-				SwatDB::updateColumn($this->page->app->db, 'Article',
-					'boolean:enabled', false, 'id', $view->checked_items);
-
-				$message = new SwatMessage(sprintf(Store::ngettext(
-					'One article has been disabled.',
-					'%d articles have been disabled.', $num),
-					SwatString::numberFormat($num)));
-
-				break;
-
-			break;
-			}
 		}
 
 		if ($message !== null)
 			$this->page->app->messages->add($message);
-	}
-
-	// }}}
-	// {{{ public static function getActions()
-
-	public static function getActions()
-	{
-		return array(
-			'show_in_menu' => Store::_('show in menu'),
-			'hide_from_menu' => Store::_('hide from menu'),
-			'show_in_search' => Store::_('show in search'),
-			'hide_from_search' => Store::_('hide from search'),
-		);
 	}
 
 	// }}}
