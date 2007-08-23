000bc5e4d39980cabb888c00c39036a774fc91ff
Nick Burka <nick@silverorange.com>
2007-08-23 17:58:30 +0000

    Newsletter isn't a Store package table, so this trigger is not needed.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@19602 94490950-a86c-4493-9ccb-530f72933e47

--- sql/triggers/AccountDeleteTrigger.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION deleteAccount () RETURNS trigger AS $$
-    BEGIN
-		delete from NewsletterSubscriber where email = OLD.email;
-        RETURN OLD;
-    END;
-$$ LANGUAGE 'plpgsql';
-
-CREATE TRIGGER AccountDeleteTrigger BEFORE DELETE ON Account
-    FOR EACH ROW EXECUTE PROCEDURE deleteAccount();
