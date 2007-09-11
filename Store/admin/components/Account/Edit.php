db5ac62c6dd1557f7570351efbe3dac641d091b5
Nick Burka <nick@silverorange.com>
2007-09-11 17:17:55 +0000

    Store Account component now inherets from Site
    svn commit r20103

--- Store/admin/components/Account/Edit.php
+++ Store/admin/components/Account/Edit.php
@@ -1,10 +1,6 @@
 <?php
 
-require_once 'Admin/pages/AdminDBEdit.php';
-require_once 'Admin/exceptions/AdminNotFoundException.php';
-require_once 'SwatDB/SwatDB.php';
-require_once 'Swat/SwatMessage.php';
-require_once 'Swat/SwatDate.php';
+require_once 'Site/admin/components/Account/Edit.php';
 
 /**
  * Edit page for Accounts
@@ -13,11 +9,9 @@ require_once 'Swat/SwatDate.php';
  * @copyright 2006-2007 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class StoreAccountEdit extends AdminDBEdit
+class StoreAccountEdit extends SiteAccountEdit
 {
 	// {{{ protected properties
-
-	protected $fields;
 
 	/**
 	 * @var string
@@ -31,119 +25,25 @@ class StoreAccountEdit extends AdminDBEdit
 
 	protected function initInternal()
 	{
+		$this->ui->mapClassPrefixToPath('Store', 'Store');
+
 		parent::initInternal();
 
-		$this->ui->mapClassPrefixToPath('Store', 'Store');
-		$this->ui->loadFromXML($this->ui_xml);
-
-		$this->fields = array('fullname', 'email', 'phone');
+		$this->fields[] = 'phone';
 	}
 
 	// }}}
 
 	// process phase
-	// {{{ protected function validate()
-
-	protected function validate()
-	{
-		$email = $this->ui->getWidget('email');
-		if ($email->hasMessage())
-			return;
-
-		$query = SwatDB::query($this->app->db, sprintf('select email
-			from Account where lower(email) = lower(%s) and id %s %s',
-			$this->app->db->quote($email->value, 'text'),
-			SwatDB::equalityOperator($this->id, true),
-			$this->app->db->quote($this->id, 'integer')));
-
-		if (count($query) > 0) {
-			$message = new SwatMessage(Store::_(
-				'An account already exists with this email address.'),
-				SwatMessage::ERROR);
-
-			$email->addMessage($message);
-		}
-	}
-
-	// }}}
-	// {{{ protected function saveDBData()
-
-	protected function saveDBData()
-	{
-		$values = $this->getUIValues();
-
-		if ($this->id === null) {
-			$now = new SwatDate();
-			$now->toUTC();
-			$this->fields[] = 'date:createdate';
-			$values['createdate'] = $now->getDate();
-
-			$this->id = SwatDB::insertRow($this->app->db, 'Account',
-				$this->fields, $values, 'id');
-
-			$this->new_account = true;
-		} else {
-			SwatDB::updateRow($this->app->db, 'Account', $this->fields,
-				$values, 'id', $this->id);
-		}
-
-		$message = new SwatMessage(sprintf(
-			Store::_('Account “%s” has been saved.'), $values['fullname']));
-
-		$this->app->messages->add($message);
-	}
-
-	// }}}
 	// {{{ protected function getUIValues()
 
 	protected function getUIValues()
 	{
-		return $this->ui->getValues(array('fullname', 'email', 'phone'));
-	}
+		$values = parent::getUIValues();
+		$values = array_merge($values,
+			$this->ui->getValues(array('phone')));
 
-	// }}}
-	// {{{ protected function relocate()
-
-	protected function relocate()
-	{
-		$this->app->relocate('Account/Details?id='.$this->id);
-	}
-
-	// }}}
-
-	// build phase
-	// {{{ protected function loadDBData()
-
-	protected function loadDBData()
-	{
-		$row = SwatDB::queryRowFromTable($this->app->db, 'Account',
-			$this->fields, 'id', $this->id);
-
-		if ($row === null)
-			throw new AdminNotFoundException(
-				sprintf(Store::_("Account with id ‘%s’ not found."),
-				$this->id));
-
-		$this->ui->setValues(get_object_vars($row));
-	}
-
-	// }}}
-	// {{{ private function buildNavBar()
-
-	protected function buildNavBar()
-	{
-		if ($this->id === null) {
-			$this->navbar->addEntry(new SwatNavBarEntry(Store::_('New Account')));
-			$this->title = Store::_('New Account');
-		} else {
-			$account_fullname = SwatDB::queryOneFromTable($this->app->db,
-				'Account', 'text:fullname', 'id', $this->id);
-	
-			$this->navbar->addEntry(new SwatNavBarEntry($account_fullname,
-				sprintf('Account/Details?id=%s', $this->id)));
-			$this->navbar->addEntry(new SwatNavBarEntry(Store::_('Edit')));
-			$this->title = $account_fullname;
-		}
+		return $values;
 	}
 
 	// }}}
--- Store/admin/components/Account/Index.php
+++ Store/admin/components/Account/Index.php
@@ -1,8 +1,6 @@
 <?php
 
-require_once 'Admin/pages/AdminSearch.php';
-require_once 'Admin/AdminSearchClause.php';
-require_once 'SwatDB/SwatDB.php';
+require_once 'Site/admin/components/Account/Index.php';
 
 /**
  * Index page for Accounts
@@ -11,7 +9,7 @@ require_once 'SwatDB/SwatDB.php';
  * @copyright 2006-2007 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class StoreAccountIndex extends AdminSearch
+class StoreAccountIndex extends SiteAccountIndex
 {
 	// {{{ protected properties
 
@@ -27,91 +25,7 @@ class StoreAccountIndex extends AdminSearch
 
 	// }}}
 
-	// init phase
-	// {{{ protected function initInternal()
-
-	protected function initInternal()
-	{
-		parent::initInternal();
-
-		$this->ui->mapClassPrefixToPath('Store', 'Store');
-		$this->ui->loadFromXML($this->search_xml);
-		$this->ui->loadFromXML($this->ui_xml);
-	}
-
-	// }}}
-
-	// process phase
-	// {{{ protected function processInternal()
-
-	protected function processInternal()
-	{
-		parent::processInternal();
-
-		$pager = $this->ui->getWidget('pager');
-		$pager->total_records = SwatDB::queryOne($this->app->db,
-			sprintf('select count(id) from Account where %s',
-				$this->getWhereClause()));
-
-		$pager->process();
-	}
-
-	// }}}
-
 	// build phase
-	// {{{ protected function getWhereClause()
-
-	protected function getWhereClause()
-	{
-		/** 
-		 * The only way an account fullname can be null is if we've cleared the
-		 * data from it with the privacy scripts - we don't ever want to display
-		 * these accounts in the search results
-		 */
-		$where = 'Account.fullname is not null';
-
-		// fullname
-		$clause = new AdminSearchClause('fullname');
-		$clause->table = 'Account';
-		$clause->value = $this->ui->getWidget('search_fullname')->value;
-		$clause->operator = AdminSearchClause::OP_CONTAINS;
-		$where.= $clause->getClause($this->app->db);
-
-		// email
-		$emailClause = new AdminSearchClause('email');
-		$emailClause->table = 'Account';
-		$emailClause->value = $this->ui->getWidget('search_email')->value;
-		$emailClause->operator = AdminSearchClause::OP_CONTAINS;
-		$where.= $emailClause->getClause($this->app->db);
-
-		return $where;
-	}
-
-	// }}}
-	// {{{ protected function getTableModel()
-
-	protected function getTableModel(SwatView $view)
-	{
-		$pager = $this->ui->getWidget('pager');
-
-		$sql = $this->getSQL();
-		$sql = sprintf($sql,
-			$this->getWhereClause(),
-			$this->getOrderByClause($view,
-				'fullname, email'));
-
-		$this->app->db->setLimit($pager->page_size, $pager->current_record);
-
-		$rs = SwatDB::query($this->app->db, $sql);
-
-		if (count($rs) > 0)
-			$this->ui->getWidget('results_message')->content =
-				$pager->getResultsMessage('result', 'results');
-
-		return $rs;
-	}
-
-	// }}}
 	// {{{ protected function getSQL()
 
 	protected function getSQL()
