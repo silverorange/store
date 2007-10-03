20b1dd45009a83ff2d15df14b1f4daf3f850403c
Charles Waddell <charles@silverorange.com>
2007-10-03 17:39:20 +0000

    Part of ticket #1321, the ProvState admin component now uses dataobjects.
    svn commit r20885

--- Store/admin/components/ProvState/Edit.php
+++ Store/admin/components/ProvState/Edit.php
@@ -3,6 +3,7 @@
 require_once 'Admin/pages/AdminDBEdit.php';
 require_once 'Admin/exceptions/AdminNotFoundException.php';
 require_once 'SwatDB/SwatDB.php';
+require_once 'SwatDB/SwatDBClassMap.php';
 require_once 'Swat/SwatMessage.php';
 
 /**
@@ -17,6 +18,7 @@ class StoreProvStateEdit extends AdminDBEdit
 	// {{{ protected properties
 
 	protected $fields;
+	protected $prov_state;
 
 	/**
 	 * @var string
@@ -31,16 +33,35 @@ class StoreProvStateEdit extends AdminDBEdit
 	protected function initInternal()
 	{
 		parent::initInternal();
+		$this->initProvState();
 
 		$this->ui->mapClassPrefixToPath('Store', 'Store');
 		$this->ui->loadFromXML($this->ui_xml);
 
 		$this->fields = array('title', 'abbreviation', 'country');
-		
+
 		$country_flydown = $this->ui->getWidget('country');
 		$country_flydown->show_blank = false;
 		$country_flydown->addOptionsByArray(SwatDB::getOptionArray(
 			$this->app->db, 'Country', 'text:title', 'integer:id', 'title'));
+	}
+
+	// }}}
+	// {{{ protected function initProvState()
+
+	protected function initProvState()
+	{
+		$class_name = SwatDBClassMap::get('StoreProvState');
+		$this->prov_state = new $class_name();
+		$this->prov_state->setDatabase($this->app->db);
+
+		if (!$this->id !== null) {
+			if (!$this->prov_state->load($this->id)) {
+				throw new AdminNotFoundException(
+					sprintf(Admin::_('Province/State with an id "%s"'.
+						' not found'), $this->id));
+			}
+		}
 	}
 
 	// }}}
@@ -51,13 +72,10 @@ class StoreProvStateEdit extends AdminDBEdit
 	protected function saveDBData()
 	{
 		$values = $this->getUIValues();
-
-		if ($this->id === null)
-			SwatDB::insertRow($this->app->db, 'ProvState', $this->fields,
-				$values);
-		else
-			SwatDB::updateRow($this->app->db, 'ProvState', $this->fields, 
-				$values, 'id', $this->id);
+		$this->prov_state->title        = $values['title'];
+		$this->prov_state->abbreviation = $values['abbreviation'];
+		$this->prov_state->country      = $values['country'];
+		$this->prov_state->save();
 
 		$message = new SwatMessage(
 			sprintf(Store::_('“%s” has been saved.'), $values['title']));
@@ -80,15 +98,7 @@ class StoreProvStateEdit extends AdminDBEdit
 
 	protected function loadDBData()
 	{
-		$row = SwatDB::queryRowFromTable($this->app->db, 'ProvState', 
-			$this->fields, 'id', $this->id);
-
-		if ($row === null)
-			throw new AdminNotFoundException(
-				sprintf(Store::_('Province/State with id ‘%s’ not found.'), 
-					$this->id));
-
-		$this->ui->setValues(get_object_vars($row));
+		$this->ui->setValues(get_object_vars($this->prov_state));
 	}
 
 	// }}}
