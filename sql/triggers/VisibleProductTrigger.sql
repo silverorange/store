b4cbdf354587ad61d0e187a31c598f91409b4943
Michael Gauthier <mike@silverorange.com>
2007-10-05 16:04:50 +0000

    Backport Veseys visible product trigger to Store. This version is newer and uses the caching mechanism.
    svn commit r21086

--- sql/triggers/VisibleProductTrigger.sql
+++ sql/triggers/VisibleProductTrigger.sql
@@ -28,26 +28,47 @@
  *   - enabled field
  *
  * - ItemRegionBinding insert
+ * - ItemRegionBinding update
  * - ItemRegionBinding delete
  */
 CREATE OR REPLACE FUNCTION updateVisibleProduct () RETURNS INTEGER AS $$
+	DECLARE
+		locale_row record;
     BEGIN
-		truncate VisibleProductCache;
-		insert into VisibleProductCache (product, region)
-			select product, region from VisibleProductView;
+		-- for all rows in view
+		for local_row in select * from VisibleProductView where product is not null loop
 
+			-- check if row in view exists in cache
+			perform product from VisibleProductCache
+			where product = locale_row.product and region = local_row.region;
+
+			if not FOUND then
+				-- doesn't exist, add the row
+				insert into VisibleProductCache (product, region)
+				values (local_row.product, local_row.region);
+			end if;
+
+		end loop;
+
+		-- delete all rows in cache that are not in the view
+		delete from VisibleProductCache
+		where array[coalesce(product, 0), region] not in
+			(select array[coalesce(product, 0), region] from VisibleProductView);
+
+		-- set cache as clean
+		update CacheFlag set dirty = false where shortname = 'VisibleProduct';
         RETURN NULL;
     END;
 $$ LANGUAGE 'plpgsql';
 
 CREATE OR REPLACE FUNCTION runUpdateVisibleProduct () RETURNS trigger AS $$
     BEGIN
-		perform updateVisibleProduct();
+		update CacheFlag set dirty = true where shortname = 'VisibleProduct';
         RETURN NULL;
     END;
 $$ LANGUAGE 'plpgsql';
 
-CREATE TRIGGER VisibleProductTrigger AFTER INSERT OR DELETE OR UPDATE ON CatalogRegionBinding 
+CREATE TRIGGER VisibleProductTrigger AFTER INSERT OR DELETE OR UPDATE ON CatalogRegionBinding
     FOR EACH STATEMENT EXECUTE PROCEDURE runUpdateVisibleProduct();
 
 CREATE TRIGGER VisibleProductTrigger AFTER INSERT OR DELETE OR UPDATE ON CategoryProductBinding
