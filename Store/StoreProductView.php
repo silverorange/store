72a520ffa4ce3daec416c9e3032a00c9dbec0247
Michael Gauthier <mike@silverorange.com>
2007-11-05 16:45:28 +0000

    Add abstract product view widget and actual product icon view widget.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@22516 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Store/StoreProductView.php
@@ -0,0 +1,72 @@
+<?php
+
+require_once 'Swat/SwatControl.php';
+require_once 'Swat/SwatHtmlTag.php';
+require_once 'Store/dataobjects/StoreProductWrapper.php';
+
+/**
+ * Control to display a recordset of products
+ *
+ * @package   Store
+ * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+abstract class StoreProductView extends SwatControl
+{
+	// {{{ protected properties
+
+	/**
+	 * The products displayed by this product view
+	 *
+	 * @var StoreProductWrapper
+	 */
+	protected $products;
+
+	/**
+	 * The category path of products displayed in this product view
+	 *
+	 * @var string
+	 */
+	protected $path;
+
+	// }}}
+	// {{{ abstract public function display()
+
+	/**
+	 * Displays this product view
+	 */
+	abstract public function display();
+
+	// }}}
+	// {{{ public function setProducts()
+
+	/**
+	 * Sets the products of this product view
+	 *
+	 * @param StoreProductWrapper $products the products of this product view.
+	 */
+	public function setProducts(StoreProductWrapper $products)
+	{
+		$this->products = $products;
+	}
+
+	// }}}
+	// {{{ public function setPath()
+
+	/**
+	 * Sets the category path of this product view
+	 *
+	 * This category path is prepended to the shortname of every product
+	 * displayed by this view and then used in links to products.
+	 *
+	 * @param string $path the category path of products in this product view.
+	 */
+	public function setPath($path)
+	{
+		$this->path = (string)$path;
+	}
+
+	// }}}
+}
+
+?>
