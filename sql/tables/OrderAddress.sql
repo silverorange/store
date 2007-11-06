75608f8469dbdd8582066ace6838a081b22ae7f7
Nick Burka <nick@silverorange.com>
2007-11-06 19:16:19 +0000

    Add phone and company fields to Store package order address
    svn commit r22674

--- sql/tables/OrderAddress.sql
+++ sql/tables/OrderAddress.sql
@@ -1,6 +1,7 @@
 create table OrderAddress (
 	id serial,
 	fullname varchar(255) not null,
+	company varchar(255),
 	line1 varchar(255) not null,
 	line2 varchar(255),
 	city varchar(255) not null,
@@ -8,6 +9,7 @@ create table OrderAddress (
 	provstate_other varchar(255),
 	country char(2) not null references Country(id),
 	postal_code varchar(50) not null,
+	phone varchar(100),
 	primary key (id)
 );
 
