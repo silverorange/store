b4fd8b1c1ce026ef1134195786f4fb32fda052e4
Nathan Fredrickson <nathan@silverorange.com>
2007-11-06 18:27:16 +0000

    Remove default data
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@22659 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/Region.sql
+++ sql/tables/Region.sql
@@ -4,8 +4,5 @@ create table Region (
 	primary key(id)
 );
 
-INSERT INTO Region (id, title) VALUES (1, 'Canada');
-INSERT INTO Region (id, title) VALUES (2, 'US');
-
 SELECT setval('Region_id_seq', max(id)) FROM Region;
 
