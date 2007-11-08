ead168e24e7512780e05fee953313e074e50e8c5
Michael Gauthier <mike@silverorange.com>
2007-11-08 19:06:59 +0000

    Remove feature from trunk that is not ready for this branch of Store.
    svn commit r22839

--- Store/pages/StoreAccountDetailsPage.php
+++ Store/pages/StoreAccountDetailsPage.php
@@ -396,7 +396,6 @@ class StoreAccountDetailsPage extends StoreAccountPage
 	protected function getAccountDetailsStore(StoreAccount $account)
 	{
 		$ds = new SwatDetailsStore($account);
-		$ds->fullname = $account->getFullName();
 		return $ds;
 	}
 
