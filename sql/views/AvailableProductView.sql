9a2c0e8acc1ddbde14a6a43773f781e23c5880ad
Michael Gauthier <mike@silverorange.com>
2007-12-06 20:10:17 +0000

    docs
    svn commit r24004

--- sql/views/AvailableProductView.sql
+++ sql/views/AvailableProductView.sql
@@ -1,3 +1,4 @@
+-- products are available if they have at least one available item
 create or replace view AvailableProductView as
 	select product, region from AvailableItemView
 		inner join Item on AvailableItemview.item = Item.id
