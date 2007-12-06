ad8a8a41801ad1bf4912497283dfe60ec04f548a
Nathan Fredrickson <nathan@silverorange.com>
2007-12-06 15:48:02 +0000

    This is better, extend the account table in the Site package.
    svn commit r23958

--- sql/tables/Account.sql
+++ sql/tables/Account.sql
@@ -1,25 +1,9 @@
--- renamed from customers
-create table Account (
-	id serial,
-	fullname varchar(255),
-	company varchar(255),
-	email varchar(255),
-	phone varchar(100),
-	password varchar(255),
-	password_salt varchar(50),
-	password_tag varchar(255),
-	createdate timestamp,
-	last_login timestamp,
-	default_billing_address integer,
-	default_shipping_address integer,
-	primary key (id)
-);
+alter table Account add company varchar(255);
+alter table Account add phone varchar(100);
 
-alter table Account
-	add constraint Accountfk foreign key (default_billing_address)
-	references AccountAddress(id) match full on delete set null;
+alter table Account add default_billing_address integer
+	references AccountAddress(id) on delete set null;
 
-alter table Account
-	add constraint Accountfk foreign key (default_shipping_address)
-	references AccountAddress(id) match full on delete set null;
+alter table Account add default_shipping_address integer
+	references AccountAddress(id) on delete set null;
 
