2c7912cb3a7db46fbbe14d2d8d3ee0e4eb646d2d
Isaac Grant <isaac@silverorange.com>
2007-12-07 13:55:42 +0000

    fix #1714 - aka de-punk-ify (somehow i forgot to commit these earlier)
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@24063 94490950-a86c-4493-9ccb-530f72933e47

--- sql/views/CategoryProductCountByCatalogView.sql
+++ sql/views/CategoryProductCountByCatalogView.sql
@@ -1,9 +1,9 @@
 -- used in the admin to get category product count
 -- don't use on front end as no visibility rules are respected
 create or replace view CategoryProductCountByCatalogView as
-	select CategoryDescendents.category, Product.catalog,
+	select CategoryDescendants.category, Product.catalog,
 		count(distinct Product.id) as product_count
 	from Product 
 		inner join CategoryProductBinding on Product.id = CategoryProductBinding.product
-		inner join getCategoryDescendents(NULL) as CategoryDescendents on CategoryDescendents.descendent = CategoryProductBinding.category
-	group by CategoryDescendents.category, Product.catalog;
+		inner join getCategoryDescendants(NULL) as CategoryDescendants on CategoryDescendants.descendant = CategoryProductBinding.category
+	group by CategoryDescendants.category, Product.catalog;
