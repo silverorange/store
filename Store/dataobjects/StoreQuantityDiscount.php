fe9844daa4c689614a376213a370de3cb66e11ce
Nathan Fredrickson <nathan@silverorange.com>
2007-12-11 02:45:44 +0000

    simplify
    svn commit r24265

--- Store/dataobjects/StoreQuantityDiscount.php
+++ Store/dataobjects/StoreQuantityDiscount.php
@@ -150,7 +150,7 @@ class StoreQuantityDiscount extends SwatDBDataObject
 
 		$sale = $this->item->sale_discount;
 		if ($sale !== null)
-			$price -= round($price * $sale->discount_percentage, 2);
+			$price = round($price * (1 - $sale->discount_percentage), 2);
 
 		return $price;
 	}
