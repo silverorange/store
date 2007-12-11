4e6da4e8053f52f08850f46568635d89b3d9f57c
Nick Burka <nick@silverorange.com>
2007-12-11 02:03:42 +0000

    Alow specifying whether or not to search by the original prices instead of discounted price.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@24257 94490950-a86c-4493-9ccb-530f72933e47

--- sql/functions/getProductPriceRange.sql
+++ sql/functions/getProductPriceRange.sql
@@ -9,21 +9,33 @@
 CREATE TYPE type_product_price_range AS (product INTEGER, region INTEGER,
 	min_price DECIMAL(9, 2), max_price DECIMAL(9, 2), num_items INTEGER);
 
-CREATE OR REPLACE FUNCTION getProductPriceRange(INTEGER) RETURNS SETOF type_product_price_range AS $$
+CREATE OR REPLACE FUNCTION getProductPriceRange(INTEGER, BOOLEAN) RETURNS SETOF type_product_price_range AS $$
 	DECLARE
 		param_region ALIAS FOR $1;
+		param_original_price ALIAS FOR $2;
 		returned_row type_product_price_range%ROWTYPE;
 	BEGIN
 	FOR returned_row IN
 		SELECT Product.id AS product, param_region AS region,
-				min(price - (price * coalesce(SaleDiscount.discount_percentage, 0))) AS min_price,
-				max(price - (price * coalesce(SaleDiscount.discount_percentage, 0))) AS max_price,
+
+			case when param_original_price = true then
+				min(price)
+				else min(price - (price * coalesce(SaleDiscount.discount_percentage, 0)))
+			end	AS min_price,
+
+			case when param_original_price = true then
+				max(price)
+				else max(price - (price * coalesce(SaleDiscount.discount_percentage, 0)))
+			end	AS max_price,
+
 			count(Item.id) AS num_items
+
 			FROM ItemRegionBinding
 			inner join Item ON ItemRegionBinding.item = Item.id AND ItemRegionBinding.region = param_region
 			inner join Product ON Item.product = Product.id
 			left outer join SaleDiscount on SaleDiscount.id = Item.sale_discount
 		GROUP BY product.id
+
 	LOOP
 		RETURN NEXT returned_row;
 	END LOOP;
