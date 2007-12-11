fb379c357e8082fb837e5249e2369771a543877c
Michael Gauthier <mike@silverorange.com>
2007-12-11 20:20:44 +0000

    Ticket #1721: Fix Layout of Stand-Alone Checkout Address Pages.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@24337 94490950-a86c-4493-9ccb-530f72933e47

--- Store/pages/StoreCheckoutBillingAddressPage.php
+++ Store/pages/StoreCheckoutBillingAddressPage.php
@@ -188,6 +188,21 @@ class StoreCheckoutBillingAddressPage extends StoreCheckoutEditPage
 	}
 
 	// }}}
+	// {{{ protected function buildInternal()
+
+	protected function buildInternal()
+	{
+		parent::buildInternal();
+
+		/*
+		 * Set page to two-column layout when page is stand-alone even when
+		 * there is no address list. The narrower layout of the form fields
+		 * looks better even withour a select list on the left.
+		 */
+		$this->ui->getWidget('form')->classes[] = 'checkout-no-column';
+	}
+
+	// }}}
 	// {{{ protected function loadDataFromSession()
 
 	protected function loadDataFromSession()
