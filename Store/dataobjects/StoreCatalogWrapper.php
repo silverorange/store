a66fb087b4ecfc93769733d16d578d1ef5c42881
Nick Burka <nick@silverorange.com>
2007-12-11 23:31:58 +0000

    Add CatalogWrapper
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@24355 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Store/dataobjects/StoreCatalogWrapper.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'SwatDB/SwatDBRecordsetWrapper.php';
+require_once 'Store/dataobjects/StoreCatalog.php';
+
+/**
+ * A recordset wrapper class for StoreCatalog objects
+ *
+ * @package   Store
+ * @copyright 2006-2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @see       StoreCatalog
+ */
+class StoreCatalogWrapper extends SwatDBRecordsetWrapper
+{
+	// {{{ protected function init()
+
+	protected function init()
+	{
+		parent::init();
+		$this->row_wrapper_class = SwatDBClassMap::get('StoreCatalog');
+	}
+
+	// }}}
+}
+
+?>
