2d73cc38eecdc1a1e42ef8c040108eca69215c5f
Keith Burgoyne <keith@silverorange.com>
2007-12-17 15:11:58 +0000

    prepare for release of 1.11.2
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@24600 94490950-a86c-4493-9ccb-530f72933e47

--- package.php
+++ package.php
@@ -2,7 +2,7 @@
 
 require_once 'PEAR/PackageFileManager2.php';
 
-$version = '1.11.1';
+$version = '1.11.2';
 $notes = <<<EOT
 see ChangeLog
 EOT;
@@ -53,8 +53,8 @@ $package->addReplacement('Store/Store.php', 'pear-config', '@DATA-DIR@', 'data_d
 
 $package->setPhpDep('5.1.5');
 $package->setPearinstallerDep('1.4.0');
-$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.3.0');
-$package->addPackageDepWithChannel('required', 'Site', 'pear.silverorange.com', '1.1.12');
+$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.3.1');
+$package->addPackageDepWithChannel('required', 'Site', 'pear.silverorange.com', '1.1.13');
 $package->addPackageDepWithChannel('required', 'Admin', 'pear.silverorange.com', '1.1.26');
 $package->addPackageDepWithChannel('required', 'XML_RPCAjax', 'pear.silverorange.com', '1.0.5');
 $package->addPackageDepWithChannel('required', 'Yui', 'pear.silverorange.com', '1.0.4');
