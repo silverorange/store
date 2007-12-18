7738ef8b5e2d228821cd4f4c6088d9f600299b6d
Nick Burka <nick@silverorange.com>
2007-12-18 17:30:48 +0000

    Smarter checking of the uniqueness of email addresses
    svn commit r24807

--- Store/pages/StoreCheckoutBasicInfoPage.php
+++ Store/pages/StoreCheckoutBasicInfoPage.php
@@ -113,6 +113,13 @@ class StoreCheckoutBasicInfoPage extends StoreCheckoutEditPage
 
 	protected function validEmailAddress($email)
 	{
+		// don't even bother checking validity of the email is the same as the
+		// account email. Allows fringe case where two accounts have the same
+		// email (in theory this isn't possible).
+		if ($this->app->session->isLoggedIn() &&
+			$email == $this->app->session->account->email)
+			return true;
+
 		$instance = ($this->app->hasModule('SiteMultipleInstanceModule')) ?
 			$this->app->instance->getInstance() : null;
 
