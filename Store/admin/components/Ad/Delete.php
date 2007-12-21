9967bd3b8ed13c624bfcbfb3d889afb0d422fb47
Michael Gauthier <mike@silverorange.com>
2007-12-21 16:43:01 +0000

    Subclass SiteAdDelete. Part of ticket #1808.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@25030 94490950-a86c-4493-9ccb-530f72933e47

--- Store/admin/components/Ad/Delete.php
+++ Store/admin/components/Ad/Delete.php
@@ -1,8 +1,6 @@
 <?php
 
-require_once 'Admin/pages/AdminDBDelete.php';
-require_once 'SwatDB/SwatDB.php';
-require_once 'Admin/AdminListDependency.php';
+require_once 'Site/admin/components/Ad/Delete.php';
 require_once 'Admin/AdminSummaryDependency.php';
 
 /**
@@ -12,44 +10,31 @@ require_once 'Admin/AdminSummaryDependency.php';
  * @copyright 2006-2007 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class StoreAdDelete extends AdminDBDelete
+class StoreAdDelete extends SiteAdDelete
 {
 	// process phase
-	// {{{ protected function processDBData()
+	// {{{ protected function getDeleteSql()
 
-	protected function processDBData()
+	protected function getDeleteSql()
 	{
-		parent::processDBData();
-
 		$item_list = $this->getItemList('text');
-		$sql = 'delete from Ad where id in (%s)
-			and id not in (select ad from Orders where ad is not null)';
+		$sql = sprintf('delete from Ad where id in (%s) and
+			id not in (select ad from Orders where ad is not null)',
+			$item_list);
 
-		$num = SwatDB::exec($this->app->db, sprintf($sql, $item_list));
-
-		$message = new SwatMessage(sprintf(Store::ngettext(
-			'One ad has been deleted.', '%d ads have been deleted.', $num),
-			SwatString::numberFormat($num)), SwatMessage::NOTIFICATION);
-
-		$this->app->messages->add($message);
+		return $sql;
 	}
 
 	// }}}
 
 	// build phase
-	// {{{ protected function buildInternal()
+	// {{{ protected function getDependencies()
 
-	protected function buildInternal()
+	protected function getDependencies()
 	{
-		parent::buildInternal();
+		$dep = parent::getDependencies();
 
 		$item_list = $this->getItemList('integer');
-
-		$dep = new AdminListDependency();
-		$dep->setTitle(Store::_('ad'), Store::_('ads'));
-		$dep->entries = AdminListDependency::queryEntries($this->app->db,
-			'Ad', 'integer:id', null, 'text:title', 'id',
-			'id in ('.$item_list.')', AdminDependency::DELETE);
 
 		$dep_orders = new AdminSummaryDependency();
 		$dep_orders->setTitle(Store::_('order'), Store::_('orders'));
@@ -59,12 +44,7 @@ class StoreAdDelete extends AdminDBDelete
 
 		$dep->addDependency($dep_orders);
 
-		$message = $this->ui->getWidget('confirmation_message');
-		$message->content = $dep->getMessage();
-		$message->content_type = 'text/xml';
-
-		if ($dep->getStatusLevelCount(AdminDependency::DELETE) == 0)
-			$this->switchToCancelButton();
+		return $dep;
 	}
 
 	// }}}
