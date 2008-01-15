0b595d76f95b4e4e86181dc0aad5b76d29f128f2
Nathan Fredrickson <nathan@silverorange.com>
2008-01-15 02:35:31 +0000

    Push order and account deleters up to Store.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@25862 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Store/StoreAccountDeleter.php
@@ -0,0 +1,172 @@
+<?php
+
+require_once 'Store/StorePrivateDataDeleter.php';
+require_once 'Store/dataobjects/StoreAccountWrapper.php';
+require_once 'SwatDB/SwatDB.php';
+require_once 'Store/dataobjects/StoreAccount.php';
+
+/**
+ * Removes personal data from inactive accounts
+ *
+ * @package   Store
+ * @copyright 2007-2008 silverorange
+ */
+class StoreAccountDeleter extends StorePrivateDataDeleter
+{
+	// {{{ class constants
+
+	/**
+	 * How many records to process in a single iteration
+	 *
+	 * @var integer
+	 */
+	const DATA_BATCH_SIZE = 100;
+
+	// }}}
+	// {{{ public function run()
+
+	public function run()
+	{
+		$this->app->debug("\nAccounts\n--------\n");
+
+		$total = $this->getTotal();
+		if ($total == 0) {
+			$this->app->debug("No inactive accounts found. ".
+				"No private data removed.\n");
+		} else {
+			$this->app->debug(
+				sprintf("Found %s inactive accounts for cleaning:\n\n",
+				$total));
+
+			if (!$this->app->isDryRun()) {
+
+				/*
+				 * Transactions are not used because dataobject saving already
+				 * uses transactions.
+				 */
+
+				$accounts = $this->getAccounts();
+				$count = count($accounts);
+				while ($count > 0) {
+					foreach ($accounts as $account) {
+						$this->app->debug(
+							sprintf("=> cleaning account #%s ... ",
+							$account->id));
+
+						$this->cleanAccount($account);
+						$account->save();
+						$this->app->debug("done\n");
+					}
+
+					// get next batch of accounts
+					$accounts = $this->getAccounts();
+					$count = count($accounts);
+				}
+
+			} else {
+				$this->app->debug("=> not cleaning because dry-run is on\n");
+			}
+
+			$this->app->debug("\nFinished cleaning inactive accounts.\n");
+		}
+	}
+
+	// }}}
+	// {{{ protected function cleanAccount()
+
+	/**
+	 * Clears an account of private data
+	 *
+	 * @param StoreAccount $account the account to clear.
+	 */
+	protected function cleanAccount(StoreAccount $account)
+	{
+		$sql = sprintf('delete from AccountAddress where account = %s',
+			$this->app->db->quote($account->id, 'integer'));
+
+		$addresses = SwatDB::exec($this->app->db, $sql);
+		if ($addresses > 0)
+			$this->app->debug(sprintf("%s addresses ... ", $addresses));
+
+		$account->fullname = null;
+		$account->email = null;
+		$account->phone = null;
+		$account->password = null;
+		$account->password_salt = null;
+		$account->password_tag = null;
+		$account->last_login = null;
+	}
+
+	// }}}
+	// {{{ protected function getAccounts()
+
+	protected function getAccounts()
+	{
+		$sql = sprintf('select * from Account %s %s',
+			$this->getWhereClause(),
+			$this->getLimitClause());
+
+		$accounts = SwatDB::query($this->app->db, $sql, 'StoreAccountWrapper');
+
+		return $accounts;
+	}
+
+	// }}}
+	// {{{ protected function getTotal()
+
+	protected function getTotal()
+	{
+		$sql = sprintf('select count(id) from Account %s',
+			$this->getWhereClause());
+
+		$total = SwatDB::queryOne($this->app->db, $sql);
+
+		return $total;
+	}
+
+	// }}}
+	// {{{ protected function getExpiryDate()
+
+	protected function getExpiryDate()
+	{
+		$expiry_date = new SwatDate(strtotime('-3 year'));
+
+		return $expiry_date;
+	}
+
+	// }}}
+	// {{{ protected function getWhereClause()
+
+	protected function getWhereClause()
+	{
+		$expiry_date = $this->getExpiryDate();
+		$expiry_date->toUTC();
+
+		$instance_id = $this->app->instance->getInstance()->id;
+
+		$sql = 'where last_login < %s
+			and fullname is not null
+			and instance = %s';
+
+		$sql = sprintf($sql,
+			$this->app->db->quote($expiry_date->getDate(), 'date'),
+			$this->app->db->quote($instance_id, 'integer'));
+
+		return $sql;
+	}
+
+	// }}}
+	// {{{ protected function getLimitClause()
+
+	protected function getLimitClause()
+	{
+		$sql = sprintf('limit %s',
+			$this->app->db->quote(self::DATA_BATCH_SIZE, 'integer'));
+
+		return $sql;
+	}
+
+	// }}}
+}
+
+?>
---
+++ Store/StoreOrderDeleter.php
@@ -0,0 +1,223 @@
+<?php
+
+require_once 'Store/StorePrivateDataDeleter.php';
+require_once 'Store/dataobjects/StoreOrderWrapper.php';
+require_once 'SwatDB/SwatDB.php';
+
+/**
+ * Removes personal data from expired orders
+ *
+ * @package   Store
+ * @copyright 2007-2008 silverorange
+ */
+class StoreOrderDeleter extends StorePrivateDataDeleter
+{
+	// {{{ class constants
+
+	/**
+	 * How many records to process in a single iteration
+	 *
+	 * @var integer
+	 */
+	const DATA_BATCH_SIZE = 100;
+
+	// }}}
+	// {{{ public function run()
+
+	public function run()
+	{
+		$this->app->debug("\nOrders\n------\n");
+
+		$total = $this->getTotal();
+		if ($total == 0) {
+			$this->app->debug("No expired orders found. ".
+				"No private data removed.\n");
+		} else {
+			$this->app->debug(
+				sprintf("Found %s expired orders for deletion:\n",
+				$total));
+
+			if (!$this->app->isDryRun()) {
+
+				/*
+				 * Transactions are not used because dataobject saving already
+				 * uses transactions.
+				 */
+
+				$orders = $this->getOrders();
+				$count = count($orders);
+				while ($count > 0) {
+					foreach ($orders as $order) {
+						$this->app->debug(
+							sprintf("=> cleaning order %s ... ",
+							$order->id));
+
+						$this->cleanOrder($order);
+						$order->save();
+						$this->app->debug("done\n");
+					}
+
+					// get next batch of orders
+					$orders = $this->getOrders();
+					$count = count($orders);
+				}
+
+			} else {
+				$this->app->debug("=> not cleaning because dry-run is on\n");
+			}
+
+			$this->app->debug("Finished cleaning expired orders.\n");
+		}
+	}
+
+	// }}}
+	// {{{ protected function cleanOrder()
+
+	/**
+	 * Clears an order of private data
+	 *
+	 * @param StoreOrder $order the order to clear.
+	 */
+	protected function cleanOrder(StoreOrder $order)
+	{
+		$order->email = null;
+		$order->phone = null;
+		$order->comments = null;
+		$order->notes = null;
+		$order->gift_message = null;
+
+		$order->payment_method->card_fullname = null;
+		$order->payment_method->card_number_preview = null;
+		$order->payment_method->card_number = null;
+		$order->payment_method->card_expiry = null;
+
+		$order->billing_address->fullname = '';
+		$order->billing_address->company = '';
+		$order->billing_address->phone = '';
+		$order->billing_address->line1 = '';
+		$order->billing_address->line2 = null;
+		$order->billing_address->city = '';
+		$order->billing_address->postal_code =
+			$this->cleanPostalCode($order->billing_address);
+
+		$order->shipping_address->fullname = '';
+		$order->shipping_address->company = '';
+		$order->shipping_address->phone = '';
+		$order->shipping_address->line1 = '';
+		$order->shipping_address->line2 = null;
+		$order->shipping_address->city = '';
+		$order->shipping_address->postal_code =
+			$this->cleanPostalCode($order->shipping_address);
+	}
+
+	// }}}
+	// {{{ protected function cleanPostalCode()
+
+	/**
+	 * Removes personally identifiable information from postal codes and Zip
+	 * Codes
+	 *
+	 * @param StoreAddress $address the address which contains the postal code
+	 *                               to clean.
+	 *
+	 * @return string the postal code with personally identifiable information
+	 *                 removed.
+	 */
+	protected function cleanPostalCode(StoreAddress $address)
+	{
+		$postal_code = '';
+		switch ($address->getInternalValue('country')) {
+		case 'CA':
+			// leave off local delivery unit from postal code
+			$postal_code = substr($address->postal_code, 0, 3);
+			break;
+		case 'US':
+			// leave off +4 from Zip Code
+			$postal_code = substr($address->postal_code, 0, 5);
+			break;
+		}
+
+		return $postal_code;
+	}
+
+	// }}}
+	// {{{ protected function getOrders()
+
+	protected function getOrders()
+	{
+		$sql = 'select Orders.* from Orders
+			inner join OrderAddress on Orders.billing_address = OrderAddress.id
+			%s %s';
+
+		$sql = sprintf($sql,
+			$this->getWhereClause(),
+			$this->getLimitClause());
+
+		$orders = SwatDB::query($this->app->db, $sql, 'StoreOrderWrapper');
+
+		return $orders;
+	}
+
+	// }}}
+	// {{{ protected function getTotal()
+
+	protected function getTotal()
+	{
+		$sql = 'select count(Orders.id) from Orders
+			inner join OrderAddress on Orders.billing_address = OrderAddress.id
+			%s';
+
+		$sql = sprintf($sql,
+			$this->getWhereClause());
+
+		$total = SwatDB::queryOne($this->app->db, $sql);
+
+		return $total;
+	}
+
+	// }}}
+	// {{{ protected function getExpiryDate()
+
+	protected function getExpiryDate()
+	{
+		$expiry_date = new SwatDate(strtotime('-1 year'));
+
+		return $expiry_date;
+	}
+
+	// }}}
+	// {{{ protected function getWhereClause()
+
+	protected function getWhereClause()
+	{
+		$expiry_date = $this->getExpiryDate();
+		$expiry_date->toUTC();
+
+		$instance_id = $this->app->instance->getInstance()->id;
+
+		$sql = 'where length(OrderAddress.first_name) > 0
+			and createdate < %s
+			and instance = %s';
+
+		$sql = sprintf($sql,
+			$this->app->db->quote($expiry_date->getDate(), 'date'),
+			$this->app->db->quote($instance_id, 'integer'));
+
+		return $sql;
+	}
+
+	// }}}
+	// {{{ protected function getLimitClause()
+
+	protected function getLimitClause()
+	{
+		$sql = sprintf('limit %s',
+			$this->app->db->quote(self::DATA_BATCH_SIZE, 'integer'));
+
+		return $sql;
+	}
+
+	// }}}
+}
+
+?>
