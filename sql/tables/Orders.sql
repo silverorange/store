c31b4ceb7bd73a3215d12d330b1d9e7009474d75
Nathan Fredrickson <nathan@silverorange.com>
2008-01-17 20:06:39 +0000

    Remove instance field from Store. The VanB package adds this.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@26029 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/Orders.sql
+++ sql/tables/Orders.sql
@@ -1,7 +1,6 @@
 create table Orders (
 	id serial,
 
-	instance integer references Instance(id),
 	previous_attempt integer, -- constraint added below
 	account integer default null references Account(id) on delete set null,
 	email varchar(255),
