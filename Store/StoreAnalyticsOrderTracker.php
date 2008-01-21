fc938d298c654f54b9c622217ce75f696ca52a33
Nathan Fredrickson <nathan@silverorange.com>
2008-01-21 14:58:36 +0000

    merge 26176, 26179, 26180 from trunk
    svn commit r26184

---
+++ Store/StoreAnalyticsOrderTracker.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once 'Swat/SwatString.php';
+require_once 'Store/dataobjects/StoreOrder.php';
+
+/**
+ * Generates Google Analytics order transaction tracking code for an order
+ *
+ * @package   Store
+ * @copyright 2008 silverorange
+ */
+class StoreAnalyticsOrderTracker
+{
+	/**
+	 * @var StoreOrder
+	 */
+	protected $order;
+
+	protected $affiliation;
+
+	public function __construct(StoreOrder $order, $affiliation = null)
+	{
+		$this->order = $order;
+		$this->affiliation = $affiliation;
+	}
+
+	public function getInlineJavaScript()
+	{
+		$order = $this->order;
+		$billing_address = $order->billing_address;
+
+		ob_start();
+
+		echo "\n\npageTracker._addTrans(\n",
+			"\t", SwatString::quoteJavaScriptString($order->id), ",\n",
+			"\t", SwatString::quoteJavaScriptString($this->affiliation), ",\n",
+			"\t", SwatString::quoteJavaScriptString($order->total), ",\n",
+			"\t", SwatString::quoteJavaScriptString($order->tax_total), ",\n",
+			"\t", SwatString::quoteJavaScriptString(
+				$order->shipping_total), ",\n",
+			"\t", SwatString::quoteJavaScriptString(
+				$billing_address->city), ",\n";
+
+		if ($billing_address->provstate === null)
+			echo "\t", SwatString::quoteJavaScriptString(
+				$billing_address->provstate_other), ",\n";
+		else
+			echo "\t", SwatString::quoteJavaScriptString(
+				$billing_address->provstate->title), ",\n";
+
+		echo "\t", SwatString::quoteJavaScriptString(
+			$billing_address->country->title), "\n);\n";
+
+		foreach ($order->items as $item) {
+			echo "\npageTracker._addItem(\n",
+				"\t", SwatString::quoteJavaScriptString($order->id), ",\n",
+				"\t", SwatString::quoteJavaScriptString($item->sku), ",\n",
+				"\t", SwatString::quoteJavaScriptString(
+					$item->product_title), ",\n",
+				"\t", SwatString::quoteJavaScriptString(
+					SwatString::condense($item->getDescription())), ",\n",
+				"\t", SwatString::quoteJavaScriptString($item->price), ",\n",
+				"\t", SwatString::quoteJavaScriptString($item->quantity), "\n",
+				");\n";
+		}
+
+		echo "\n\npageTracker._trackTrans();";
+
+		return ob_get_clean();
+	}
+}
+
+?>
