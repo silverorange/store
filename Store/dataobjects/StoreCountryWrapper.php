efaca694b1057ba84de66cddb2f577e4f55dfa64
Michael Gauthier <mike@silverorange.com>
2008-03-03 20:03:38 +0000

    Define some index fields.
    svn commit r28451

--- Store/dataobjects/StoreCountryWrapper.php
+++ Store/dataobjects/StoreCountryWrapper.php
@@ -18,6 +18,7 @@ class StoreCountryWrapper extends SwatDBRecordsetWrapper
 	protected function init()
 	{
 		parent::init();
+		$this->index_field = 'id';
 		$this->row_wrapper_class = SwatDBClassMap::get('StoreCountry');
 	}
 
