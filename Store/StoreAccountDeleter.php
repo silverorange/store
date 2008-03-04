1614ab9863011589669a45250b9d683f7f6e2b57
Michael Gauthier <mike@silverorange.com>
2008-03-04 15:24:51 +0000

    license.
    git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@28496 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreAccountDeleter.php
+++ Store/StoreAccountDeleter.php
@@ -12,6 +12,7 @@ require_once 'Store/dataobjects/StoreAccount.php';
  *
  * @package   Store
  * @copyright 2007-2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreAccountDeleter extends StorePrivateDataDeleter
 {
