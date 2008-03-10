eb968cee10211921f17cff564e4a30dceff07d0e
Michael Gauthier <mike@silverorange.com>
2008-03-10 16:39:19 +0000

    Documentation update to reflect removal of in_season and available from CatalogRegionBinding table.
    svn commit r28709

--- sql/views/VisibleProductView.sql
+++ sql/views/VisibleProductView.sql
@@ -1,10 +1,10 @@
 /**
- * Products are visible if they have at least one item that both exists for a
- * region, and that is enabled for that region and they are in a category and
- * their catalogue is available in the region (in season or out of season).
+ * Products are visible in a region R if they have at least one item that
+ * both exists for R, and that is enabled for R; they are in a category; and
+ * their catalogue is has a binding to R.
  *
- * This procedure is not recommended for use in the admin as it respects
- * catalogue availability.
+ * Use of this view is not recommended for use in the admin as it respects
+ * catalogue availability (as determined by CatalogRegionBinding).
  */
 create or replace view VisibleProductView as
 	select Product.id as product, ItemRegionBinding.region
