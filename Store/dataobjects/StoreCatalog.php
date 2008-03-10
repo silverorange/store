29128e4949c19c44fd34e16a0fa80d3827f3630a
Michael Gauthier <mike@silverorange.com>
2008-03-10 17:36:01 +0000

    Documentation for catalogs.
    svn commit r28714

--- Store/dataobjects/StoreCatalog.php
+++ Store/dataobjects/StoreCatalog.php
@@ -3,18 +3,29 @@
 require_once 'SwatDB/SwatDBDataObject.php';
 
 /**
+ * Catalogs group products together into management units. Catalogs are not
+ * typically visible in the frontend of a store. They often represent products
+ * specific to an actual physical catalog.
+ *
+ * Every individual product belong to exactly one catalog.
+ *
+ * A catalog is available in a region if it has a region binding in the
+ * CatalogRegionBinding table for the region. Products in unavailable
+ * catalogs are not visible on the front-end.
+ *
+ * Orthogonal to availability, catalogs are in season or out of season
+ * based on the in_season boolean.
+ *
+ * Catalogs can be cloned to create a 'working-copy' of contained products.
+ * The cloned catalog can be swapped with the parent catalog when the product
+ * changes are ready to go live.
+ *
  * @package   Store
- * @copyright 2005-2007 silverorange
+ * @copyright 2005-2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreCatalog extends SwatDBDataObject
 {
-	// {{{ constants
-
-	const STATUS_IN_SEASON = 0;
-	const STATUS_OUT_OF_SEASON = 1;
-	const STATUS_DISABLED = 2;
-
-	// }}}
 	// {{{ public properties
 
 	/**
@@ -47,7 +58,6 @@ class StoreCatalog extends SwatDBDataObject
 	protected function init()
 	{
 		$this->registerInternalProperty('clone_of', 'Catalog');
-
 		$this->table = 'Catalog';
 		$this->id_field = 'integer:id';
 	}
