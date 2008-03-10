d22537e3b453b5f0a728698cf01c45ee6aad259c
Michael Gauthier <mike@silverorange.com>
2008-03-10 19:56:20 +0000

    Clean up catalog status cell renderer.
    svn commit r28731

--- Store/admin/components/Catalog/include/StoreCatalogStatusCellRenderer.php
+++ Store/admin/components/Catalog/include/StoreCatalogStatusCellRenderer.php
@@ -1,49 +1,73 @@
 <?php
 
+require_once 'SwatDB/SwatDB.php';
+require_once 'Swat/SwatString.php';
 require_once 'Swat/SwatCellRenderer.php';
-require_once 'Store/dataobjects/StoreCatalog.php';
-require_once 'SwatDB/SwatDBClassMap.php';
 
 /**
- * Cell renderer that displays a summary of the status of a catalog
+ * Cell renderer that displays regions for which a catalog is enabled
  *
  * @package   Store
- * @copyright 2005-2007 silverorange
+ * @copyright 2005-2008 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreCatalogStatusCellRenderer extends SwatCellRenderer
 {
 	// {{{ public properties
 
+	/**
+	 * @var integer
+	 */
 	public $catalog;
+
+	/**
+	 * @var MDB2_Driver_Common
+	 */
 	public $db;
+
+	// }}}
+	// {{{ public function __construct()
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->addStyleSheet(
+			'packages/swat/styles/swat-null-text-cell-renderer.css',
+			Swat::PACKAGE_ID);
+
+	}
 
 	// }}}
 	// {{{ public function render()
 
 	public function render()
 	{
-		$catalog_class = SwatDBClassMap::get('StoreCatalog');
+		if (!$this->visible)
+			return;
 
-		$sql = sprintf('select Region.title,
-				CatalogRegionBinding.catalog is not null as available
-			from Region
-				left outer join CatalogRegionBinding on
-					Region.id = region and catalog = %s
+		parent::render();
+
+		$sql = sprintf('select Region.title
+			from Region where id in
+				(select region from CatalogRegionBinding where catalog = %s)
 			order by Region.title',
 			$this->db->quote($this->catalog, 'integer'));
 
-		$catalog_statuses = SwatDB::query($this->db, $sql);
+		$regions = SwatDB::query($this->db, $sql);
 
-		foreach ($catalog_statuses as $row) {
-			$status = ($row->available === true) ?
-				Store::_('Available') : Store::_('Unavailable');
+		if (count($regions) > 0) {
+			$region_titles = array();
+			foreach ($regions as $region) {
+				$region_titles[] =
+					SwatString::minimizeEntities($region->title);
+			}
 
-			printf('%s: %s',
-				SwatString::minimizeEntities($row->title),
-				SwatString::minimizeEntities($status));
-
-			echo '<br />';
+			echo SwatString::toList($region_titles);
+		} else {
+			$span_tag = new SwatHtmLTag('span');
+			$span_tag->class = 'swat-null-text-cell-renderer';
+			$span_tag->setContent(Store::_('<none>'));
+			$span_tag->display();
 		}
 	}
 
