268df8e705b2902bcb001c47d4112e94aa30999f
Isaac Grant <isaac@silverorange.com>
2008-03-13 14:06:30 +0000

    update Product image component tools to use new image system - what was there was pretty old code, it even seemed to predate AdminDBEdit - this refs #2132
    svn commit r28856

--- Store/admin/components/Product/ImageDelete.php
+++ Store/admin/components/Product/ImageDelete.php
@@ -2,9 +2,11 @@
 
 require_once 'Admin/pages/AdminDBDelete.php';
 require_once 'SwatDB/SwatDB.php';
-require_once 'Swat/SwatImageDisplay.php';
-require_once 'Store/dataobjects/StoreProductImage.php';
 require_once 'SwatDB/SwatDBClassMap.php';
+require_once 'Swat/SwatTableStore.php';
+require_once 'Swat/SwatDetailsStore.php';
+require_once 'Store/dataobjects/StoreProduct.php';
+require_once 'Store/dataobjects/StoreProductImage.php';
 
 /**
  * Delete confirmation page for product images
@@ -15,16 +17,36 @@ require_once 'SwatDB/SwatDBClassMap.php';
  * removed from the images table and the file is deleted.
  *
  * @package   Store
- * @copyright 2006-2007 silverorange
+ * @copyright 2006-2008 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreProductImageDelete extends AdminDBDelete
 {
 	// {{{ private properties
 
+	protected $ui_xml = 'Store/admin/components/Product/image-delete.xml';
+
+	// }}}
+	// {{{ private properties
+
+	/**
+	 * @var StoreProduct
+	 */
+	private $product;
+
+	/**
+	 * @var StoreProductImage
+	 */
+	private $image;
+
+	/**
+	 * Optional id of the product's current category. This is only used to
+	 * maintain the proper navbar breadcrumbs when getting to this page by
+	 * browsing the categories.
+	 *
+	 * @var integer
+	 */
 	private $category_id;
-	private $product_id;
-	private $product_title;
 
 	// }}}
 
@@ -34,11 +56,28 @@ class StoreProductImageDelete extends AdminDBDelete
 	protected function initInternal()
 	{
 		parent::initInternal();
-		$this->category_id = SiteApplication::initVar('category');
-		$this->product_id = SiteApplication::initVar('product');
 
-		$yes_button = $this->ui->getWidget('yes_button');
-		$yes_button->title = Store::_('Remove');
+		$this->ui_xml = $this->ui_xml;
+
+		$this->category_id = SiteApplication::initVar('category');
+
+		$product_id = SiteApplication::initVar('product');
+		$class_name = SwatDBClassMap::get('StoreProduct');
+		$this->product = new $class_name();
+		$this->product->setDatabase($this->app->db);
+
+		if (!$this->product->load($product_id))
+			throw new AdminNotFoundException(
+				sprintf('Product with id ‘%s’ not found.', $product_id));
+
+		$image_id = $this->getFirstItem();
+		$class_name = SwatDBClassMap::get('StoreProductImage');
+		$this->image = new $class_name();
+		$this->image->setDatabase($this->app->db);
+
+		if (!$this->image->load($image_id))
+			throw new AdminNotFoundException(
+				sprintf('Product mage with id ‘%s’ not found.', $image_id));
 	}
 
 	// }}}
@@ -50,46 +89,47 @@ class StoreProductImageDelete extends AdminDBDelete
 	{
 		parent::processDBData();
 
-		$image_id = $this->getFirstItem();
-
 		// we need a count products that use this image to help decide to delete
 		// the actual image or not
 		$sql = sprintf('select count(product) from ProductImageBinding
 			where image = %s',
-			$this->app->db->quote($image_id, 'integer'));
+			$this->app->db->quote($this->image->id, 'integer'));
 
 		$num_products = SwatDB::queryOne($this->app->db, $sql);
 
 		$sql = sprintf('delete from ProductImageBinding where image = %s
 			and product = %s',
-			$this->app->db->quote($image_id, 'integer'),
+			$this->app->db->quote($this->image->id, 'integer'),
 			$this->app->db->quote($this->product_id, 'integer'));
 
 		SwatDB::exec($this->app->db, $sql);
 
 		if ($num_products === 1) {
-			$sql = sprintf('delete from Image where id = %s',
-				$this->app->db->quote($image_id, 'integer'));
-
-			SwatDB::exec($this->app->db, $sql);
-
-			// delete the actual files
-			$product_image = SwatDBClassMap::get('StoreProductImage');
-			$sizes = call_user_func(array($product_image, 'getSizes'));
-
-			foreach ($sizes as $size => $dimensions)
-				unlink('../images/products/'.$size.'/'.$image_id.'.jpg');
-
+			$this->image->setFileBase('../images');
+			$this->image->delete();
+			$message_text = Store::_('One product image has been deleted.');
+		} else {
+			$message_text = Store::_('One product image has been removed.');
 		}
 
-		$message = new SwatMessage(
-			Store::_('One product image has been removed.'),
-			SwatMessage::NOTIFICATION);
-
+		$message = new SwatMessage($message_text, SwatMessage::NOTIFICATION);
 		$this->app->messages->add($message);
 	}
 
 	// }}}
+	// {{{ protected function relocate()
+
+	/**
+	 * Override the AdminDBDelete behaviour of redirecting to the component base
+	 * as there is always a details page to return to.
+	 */
+	protected function relocate()
+	{
+		AdminDBConfirmation::relocate();
+	}
+
+	// }}}
+
 
 	// build phase
 	// {{{ protected function buildInternal()
@@ -100,43 +140,26 @@ class StoreProductImageDelete extends AdminDBDelete
 
 		$this->buildNavBar();
 
-		$image_id = $this->getFirstItem();
-
 		$form = $this->ui->getWidget('confirmation_form');
 		$form->addHiddenField('category', $this->category_id);
-		$form->addHiddenField('product', $this->product_id);
+		$form->addHiddenField('product', $this->product->id);
+
+		$container = $this->ui->getWidget('confirmation_container');
+		$delete_view = $this->ui->getWidget('delete_view');
+
+		$store = new SwatTableStore();
+		$ds = new SwatDetailsStore();
+		$ds->image = $this->image;
+		$store->add($ds);
+		$delete_view->model = $store;
 
 		$message = $this->ui->getWidget('confirmation_message');
-		$image_display = new SwatImageDisplay();
-
-		$sql = sprintf('select * from Image where id = %s',
-			$this->app->db->quote($image_id, 'integer'));
-
-		$image = SwatDB::queryRow($this->app->db, $sql);
-
-		if ($image === null) {
-			throw new AdminNotFoundException(sprintf(
-				Store::_('A product image with an id of ‘%d’ does not exist.'),
-				$image_id));
-		}
-
-		ob_start();
-
-		$image_display->width = $image->thumb_width;
-		$image_display->height = $image->thumb_height;
-		$image_display->image =
-			'../images/products/thumb/'.$image->id.'.jpg';
-
-		$image_display->alt = sprintf(Store::_('Image of %s'),
-			$this->product_title);
-
-		$image_display->display();
-
-		$message->content = sprintf('<h3>%s</h3>',
-			Store::_('Remove the following image?')).
-			ob_get_clean();
-
 		$message->content_type = 'text/xml';
+		$message->content = sprintf('<strong>%s</strong>',
+			Store::_('Are you sure you want to remove the following image?'));
+
+		$yes_button = $this->ui->getWidget('yes_button');
+		$yes_button->title = Store::_('Remove');
 	}
 
 	// }}}
@@ -144,8 +167,7 @@ class StoreProductImageDelete extends AdminDBDelete
 
 	private function buildNavBar()
 	{
-		$last_entry = $this->navbar->popEntry();
-		$last_entry->title = Store::_('Remove Image');
+		$this->navbar->popEntry();
 
 		if ($this->category_id !== null) {
 			$this->navbar->popEntry();
@@ -163,23 +185,19 @@ class StoreProductImageDelete extends AdminDBDelete
 		}
 
 		if ($this->single_delete) {
-			$id = $this->product_id;
-			$this->product_title = SwatDB::queryOneFromTable($this->app->db,
-				'Product', 'text:title', 'id', $id);
-
 			if ($this->category_id === null)
-				$link = sprintf('Product/Details?id=%s', $id);
+				$link = sprintf('Product/Details?id=%s', $this->product->id);
 			else
-				$link = sprintf('Product/Details?id=%s&category=%s', $id,
-					$this->category_id);
+				$link = sprintf('Product/Details?id=%s&category=%s',
+					$this->product->id, $this->category_id);
 
-			$this->navbar->addEntry(new SwatNavBarEntry($this->product_title,
+			$this->navbar->addEntry(new SwatNavBarEntry($this->product->title,
 				$link));
 
-			$this->title = $this->product_title;
+			$this->title = $this->product->title;
 		}
 
-		$this->navbar->addEntry($last_entry);
+		$this->navbar->addEntry(new SwatNavBarEntry(Store::_('Remove Image')));
 	}
 
 	// }}}
