4a05bd3df3f4ea35fcb84310dfc2b6538c1cfb43
Nick Burka <nick@silverorange.com>
2008-04-02 21:09:52 +0000

    Fix formatting
    svn commit r29852

--- sql/views/ProductItemCountByStatusView.sql
+++ sql/views/ProductItemCountByStatusView.sql
@@ -8,10 +8,26 @@ select Product.id as product,
 	count(distinct Item_backordered.id) as count_backordered,
 	count(distinct Item_disabled.id) as count_disabled
 from Product
-	left outer join Item on Item.product = Product.id
-	left outer join Item as Item_available    on Item_available.product    = Product.id and Item_available.id    in (select id from ItemEnabledView) and Item_available.status    = 0 
-	left outer join Item as Item_outofstock   on Item_outofstock.product   = Product.id and Item_outofstock.id   in (select id from ItemEnabledView) and Item_outofstock.status   = 1 
-	left outer join Item as Item_limitedstock on Item_limitedstock.product = Product.id and Item_limitedstock.id in (select id from ItemEnabledView) and Item_limitedstock.status = 2 
-	left outer join Item as Item_backordered  on Item_backordered.product  = Product.id and Item_backordered.id  in (select id from ItemEnabledView) and Item_backordered.status  = 3 
-	left outer join Item as Item_disabled     on Item_disabled.product     = Product.id and Item_disabled.id not in (select id from ItemEnabledView)
+left outer join Item on Item.product = Product.id
+
+left outer join Item as Item_available
+	on Item_available.product = Product.id and Item_available.id
+		in (select id from ItemEnabledView) and Item_available.status = 0
+
+left outer join Item as Item_outofstock
+	on Item_outofstock.product = Product.id and Item_outofstock.id
+		in (select id from ItemEnabledView) and Item_outofstock.status   = 1
+
+left outer join Item as Item_limitedstock
+	on Item_limitedstock.product = Product.id and Item_limitedstock.id
+		in (select id from ItemEnabledView) and Item_limitedstock.status = 2
+
+left outer join Item as Item_backordered
+	on Item_backordered.product = Product.id and Item_backordered.id
+		in (select id from ItemEnabledView) and Item_backordered.status  = 3
+
+left outer join Item as Item_disabled
+	on Item_disabled.product = Product.id and Item_disabled.id not in
+		(select id from ItemEnabledView)
+
 group by Product.id;
