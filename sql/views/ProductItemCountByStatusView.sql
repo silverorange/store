761b11b6071c4f3964e5698c6694672b904547fd
Nick Burka <nick@silverorange.com>
2008-04-10 01:28:13 +0000

    Updated view with all status types both available and unavailable
    svn commit r30201

--- sql/views/ProductItemCountByStatusView.sql
+++ sql/views/ProductItemCountByStatusView.sql
@@ -23,43 +23,43 @@ left outer join Item on Item.product = Product.id
 -- available joins
 left outer join Item as Item_available
 	on Item_available.product = Product.id
-		and Item_available.id in (select id from ItemEnabledView)
+		and Item_available.id in (select item from AvailableItemView)
 
 left outer join Item as Item_available_instock
 	on Item_available_instock.product = Product.id and Item_available_instock.id
-		in (select id from ItemEnabledView) and Item_available_instock.status = 0
+		in (select item from AvailableItemView) and Item_available_instock.status = 0
 
 left outer join Item as Item_available_outofstock
 	on Item_available_outofstock.product = Product.id and Item_available_outofstock.id
-		in (select id from ItemEnabledView) and Item_available_outofstock.status   = 1
+		in (select item from AvailableItemView) and Item_available_outofstock.status   = 1
 
 left outer join Item as Item_available_limitedstock
 	on Item_available_limitedstock.product = Product.id and Item_available_limitedstock.id
-		in (select id from ItemEnabledView) and Item_available_limitedstock.status = 2
+		in (select item from AvailableItemView) and Item_available_limitedstock.status = 2
 
 left outer join Item as Item_available_backordered
 	on Item_available_backordered.product = Product.id and Item_available_backordered.id
-		in (select id from ItemEnabledView) and Item_available_backordered.status  = 3
+		in (select item from AvailableItemView) and Item_available_backordered.status  = 3
 
 -- unavailable joins
 left outer join Item as Item_unavailable
 	on Item_unavailable.product = Product.id
-		and Item_unavailable.id not in (select id from ItemEnabledView)
+		and Item_unavailable.id not in (select item from AvailableItemView)
 
 left outer join Item as Item_unavailable_instock
 	on Item_unavailable_instock.product = Product.id and Item_unavailable_instock.id
-		not in (select id from ItemEnabledView) and Item_unavailable_instock.status = 0
+		not in (select item from AvailableItemView) and Item_unavailable_instock.status = 0
 
 left outer join Item as Item_unavailable_outofstock
 	on Item_unavailable_outofstock.product = Product.id and Item_unavailable_outofstock.id
-		not in (select id from ItemEnabledView) and Item_unavailable_outofstock.status   = 1
+		not in (select item from AvailableItemView) and Item_unavailable_outofstock.status   = 1
 
 left outer join Item as Item_unavailable_limitedstock
 	on Item_unavailable_limitedstock.product = Product.id and Item_unavailable_limitedstock.id
-		not in (select id from ItemEnabledView) and Item_unavailable_limitedstock.status = 2
+		not in (select item from AvailableItemView) and Item_unavailable_limitedstock.status = 2
 
 left outer join Item as Item_unavailable_backordered
 	on Item_unavailable_backordered.product = Product.id and Item_unavailable_backordered.id
-		not in (select id from ItemEnabledView) and Item_unavailable_backordered.status  = 3
+		not in (select item from AvailableItemView) and Item_unavailable_backordered.status  = 3
 
 group by Product.id;
