8cd49da6a45c35cdb56cea54f51441c802242d53
Michael Gauthier <mike@silverorange.com>
2008-05-10 17:33:15 +0000

    Require required files. Prevents [Error message: result wrap class does not exist StoreItemWrapper]
    svn commit r32120

--- Store/dataobjects/StoreOrderItem.php
+++ Store/dataobjects/StoreOrderItem.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once 'SwatDB/SwatDBDataObject.php';
+require_once 'Store/dataobjects/StoreRegion.php';
+require_once 'Store/dataobjects/StoreItemWrapper.php';
 
 /**
  * An item in an order
