silverorange/store

commit bad2980c78b1067626b71f5e10ce210efa0604f6

Add a helper method that builds the order count for each account.
git-svn-id: file:///Users/gauthierm/svnrepo/store/trunk@28267 94490950-a86c-4493-9ccb-530f72933e47

Store/admin/components/Account/Index.php
@@ -6,7 +6,7 @@ require_once 'Site/admin/components/Account/Index.php';
  * Index page for Accounts
  *
  * @package   Store
- * @copyright 2006-2007 silverorange
+ * @copyright 2006-2008 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreAccountIndex extends SiteAccountIndex
@@ -30,13 +30,50 @@ class StoreAccountIndex extends SiteAccountIndex
 
 	protected function getSQL()
 	{
-		return 'select Account.id, Account.fullname, Account.email,
-				coalesce(AccountOrderCountView.order_count, 0) as order_count
-			from Account
-				left outer join AccountOrderCountView on
-					Account.id = AccountOrderCountView.account
+		return 'select Account.id, Account.fullname, Account.email
 			where %s
 			order by %s';
+	}
+
+	// }}}
+	// {{{ protected function getTableModel()
+
+	protected function getTableModel(SwatView $view)
+	{
+		$accounts = parent::getTableModel($view);
+
+		$order_count = $this->getOrderCount($accounts);
+
+		$store = new SwatTableStore();
+		foreach ($accounts as $account) {
+			$ds = new SwatDetailsStore($account);
+			$ds->order_count = $order_count[$ds->id];
+			$store->add($ds);
+		}
+
+		return $store;
+	}
+
+	// }}}
+	// {{{ protected function getOrderCount()
+
+	private function getOrderCount(SwatTableStore $accounts)
+	{
+		// default to zero, some accounts aren't in AccountOrderCountView
+		$count_array = array();
+		foreach ($accounts as $account) {
+			$count_array[$account->id] = 0;
+		}
+
+		$account_ids = implode(', ', array_keys($count_array));
+		$sql = sprintf('select account, order_count from
+			AccountOrderCountView where account in (%s)', $account_ids);
+
+		$counts = SwatDB::query($this->app->db, $sql);
+		foreach ($counts as $count)
+			$count_array[$count->account] = $count->order_count;
+
+		return $count_array;
 	}
 
 	// }}}

commit 56cedf1f2a019da2a9bffc4f365a4d9ce73c3e42

allow ordering accounts by order count again, this fixes a regression introduced in r28267, and fixes #2312
git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@33703 94490950-a86c-4493-9ccb-530f72933e47

Store/admin/components/Account/Index.php
@@ -30,7 +30,11 @@ class StoreAccountIndex extends SiteAccountIndex
 
 	protected function getSQL()
 	{
-		return 'select Account.id, Account.fullname, Account.email
+		return 'select Account.id, Account.fullname, Account.email,
+				coalesce(AccountOrderCountView.order_count, 0) as order_count
+			from Account
+			left outer join AccountOrderCountView on
+				Account.id = AccountOrderCountView.account
 			where %s
 			order by %s';
 	}

Store/admin/components/Account/index.xml
@@ -22,7 +22,7 @@
 						<property name="text" type="data">email</property>
 					</object>
 				</object>
-				<object class="SwatTableViewColumn" id="order_count">
+				<object class="AdminTableViewOrderableColumn" id="order_count">
 					<property name="title" translatable="yes"># of Orders</property>
 					<object class="SwatNumericCellRenderer">
 						<property name="value" type="data">order_count</property>