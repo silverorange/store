92447223e13d3b08c770dc4f489564f4be3c00f9
Charles Waddell <charles@silverorange.com>
2008-06-24 15:09:00 +0000

    Use config settings to define the expiry dates for the Privateer data deleter. Refs #2202
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@34188 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreAccountDeleter.php
+++ Store/StoreAccountDeleter.php
@@ -138,7 +138,8 @@ class StoreAccountDeleter extends StorePrivateDataDeleter
 
 	protected function getExpiryDate()
 	{
-		$expiry_date = new SwatDate(strtotime('-3 year'));
+		$expiry_accounts = $this->app->config->expiry->accounts;
+		$expiry_date = new SwatDate(strtotime('-'.$expiry_accounts));
 		$expiry_date->toUTC();
 		return $expiry_date;
 	}
