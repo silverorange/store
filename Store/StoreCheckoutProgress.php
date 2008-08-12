ae42ccf0f54d5bbb0e7b18eaade7901aabacd1cd
Michael Gauthier <mike@silverorange.com>
2008-08-12 17:38:36 +0000

    license
    svn commit r35956

--- Store/StoreCheckoutProgress.php
+++ Store/StoreCheckoutProgress.php
@@ -6,6 +6,7 @@ require_once 'Store/Store.php';
 /**
  * @package   Store
  * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreCheckoutProgress extends SwatControl
 {
