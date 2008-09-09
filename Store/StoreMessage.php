20349bca738ce475b712592add25e62b16888036
Michael Gauthier <mike@silverorange.com>
2008-09-09 17:08:29 +0000

    Update StoreMessage wrt [36654].
    svn commit r36667

--- Store/StoreMessage.php
+++ Store/StoreMessage.php
@@ -5,11 +5,11 @@ require_once 'Swat/SwatMessage.php';
 /**
  * A data class to store a message
  *
- * StoreMessage is an extension of {@link SwatMessage}.
- *
- * @package   Store
- * @copyright 2006 silverorange
- * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @package    Store
+ * @copyright  2006-2008 silverorange
+ * @license    http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @deprecated Use a {@link SwatMessage} with the type 'cart' for cart
+ *             messages.
  */
 class StoreMessage extends SwatMessage
 {
@@ -19,8 +19,10 @@ class StoreMessage extends SwatMessage
 	 * Cart notification message type
 	 *
 	 * An informative message about the cart.
+	 *
+	 * @deprecated Use the string 'cart' instead.
 	 */
-	const CART_NOTIFICATION = 100;
+	const CART_NOTIFICATION = 'cart';
 
 	// }}}
 	// {{{ public function getCSSClassString()
@@ -29,29 +31,14 @@ class StoreMessage extends SwatMessage
 	{
 		$class = parent::getCSSClassString();
 
+		// legacy
 		switch ($this->type) {
-			case StoreMessage::CART_NOTIFICATION :
-				$class.= ' store-message-cart-notification';
-				break;
+		case StoreMessage::CART_NOTIFICATION :
+			$class.= ' store-message-cart-notification';
+			break;
 		}
 
 		return $class;
-	}
-
-	// }}}
-	// {{{ protected function getTypes()
-
-	/**
-	 * Get valid message types
-	 *
-	 * @return array the valid types of a message.
-	 */
-	protected function getTypes()
-	{
-		$types = parent::getTypes();
-		$types[] = self::CART_NOTIFICATION;
-
-		return $types;
 	}
 
 	// }}}
