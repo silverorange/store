750a418529c23f0416d3fdbe511a237e75ebe51a
Nick Burka <nick@silverorange.com>
2008-11-10 16:14:03 +0000

    fix docs
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@38706 94490950-a86c-4493-9ccb-530f72933e47

--- Store/admin/components/Product/ImageEdit.php
+++ Store/admin/components/Product/ImageEdit.php
@@ -156,7 +156,7 @@ class StoreProductImageEdit extends AdminDBEdit
 	}
 
 	// }}}
-	// {{{ private function validate()
+	// {{{ private function checkManualUploads()
 
 	protected function checkManualUploads()
 	{
