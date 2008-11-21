2ca2aae05ff252eb33618e102bb793248a5f83ee
Nick Burka <nick@silverorange.com>
2008-11-21 14:21:15 +0000

    Move the logic to load quantity-discounts into the wrapper, and add code to efficiently load QuantityDiscounts for all the items in a product into the ProductWrapper
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@39396 94490950-a86c-4493-9ccb-530f72933e47

--- Store/dataobjects/StoreQuantityDiscountWrapper.php
+++ Store/dataobjects/StoreQuantityDiscountWrapper.php
@@ -12,13 +12,52 @@ require_once 'Store/dataobjects/StoreQuantityDiscount.php';
  */
 class StoreQuantityDiscountWrapper extends SwatDBRecordsetWrapper
 {
+	// {{{ public function loadSetFromDB()
+
+	public function loadSetFromDB(MDB2_Driver_Common $db,
+		array $id_set, StoreRegion $region = null, $limiting = true)
+	{
+		$quantity_discounts = null;
+		$wrapper = SwatDBClassMap::get('StoreQuantityDiscountWrapper');
+
+		if ($region === null) {
+			$sql = sprintf('select * from QuantityDiscount
+				where QuantityDiscount.item in (%s)
+				order by item, QuantityDiscount.quantity asc',
+				$db->implodeArray($id_set, 'integer'));
+
+			$quantity_discounts = SwatDB::query($db, $sql, $wrapper);
+		} else {
+			$sql = sprintf('select QuantityDiscount.*,
+					QuantityDiscountRegionBinding.price,
+					QuantityDiscountRegionBinding.region as region_id
+				from QuantityDiscount
+					%s QuantityDiscountRegionBinding on
+					quantity_discount = QuantityDiscount.id and
+					region = %s
+				where QuantityDiscount.item in (%s)
+				order by item, QuantityDiscount.quantity asc',
+				$limiting ? 'inner join' : 'left outer join',
+				$db->quote($region->id, 'integer'),
+				$db->implodeArray($id_set, 'integer'));
+
+			$quantity_discounts = SwatDB::query($db, $sql, $wrapper);
+			if ($quantity_discounts !== null)
+				foreach ($quantity_discounts as $discount)
+					$discount->setRegion($region, $limiting);
+		}
+
+		return $quantity_discounts;
+	}
+
+	// }}}
 	// {{{ protected function init()
 
 	protected function init()
 	{
 		parent::init();
-		$this->row_wrapper_class = SwatDBClassMap::get('StoreQuantityDiscount');
 
+		$this->row_wrapper_class = SwatDBClassMap::get('StoreQuantityDiscount');
 		$this->index_field = 'id';
 	}
 
