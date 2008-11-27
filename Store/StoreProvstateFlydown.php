b763be4cf36b43997b1020699e466762da36fe76
Nathan Fredrickson <nathan@silverorange.com>
2008-11-27 13:10:33 +0000

    Name to match dataobject and deprecate old class.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@39835 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreProvstateFlydown.php
+++ Store/StoreProvstateFlydown.php
@@ -1,102 +1,17 @@
 <?php
 
-require_once 'Swat/SwatFlydown.php';
-require_once 'Store.php';
+require_once 'Store/StoreProvStateFlydown.php';
 
 /**
- * A widget for selecting a provstate
+ * A deprecated widget for selecting a provstate
  *
- * This widget validates that the selected provstate is valid in a given
- * county.  To validate a provstate, the widget needs to know the country and
- * have a reference to the database. Set the
- * {@link StorePostalCodeEntry::$country} property to a known ISO-3611 code.
+ * @deprecated use StoreProvStateFlydown instead
  *
  * @package   Store
  * @copyright 2008 silverorange
  */
-class StoreProvstateFlydown extends SwatFlydown
+class StoreProvstateFlydown extends StoreProvstateFlydown
 {
-	// {{{ public function process()
-
-	/**
-	 * Processes this postal code entry widget
-	 *
-	 * The postal code is validated and formatted correctly.
-	 */
-	public function process()
-	{
-		parent::process();
-
-		if ($this->db !== null && $this->country !== null &&
-			$this->value !== null)
-				$this->validate();
-	}
-
-	// }}}
-	// {{{ public properties
-
-	/**
-	 * @var MDB2_Driver_Common
-	 */
-	protected $db;
-
-	// {{{ public properties
-	// }}}
-
-	/**
-	 * The country to validate the provstate in
-	 *
-	 * This should be a valid ISO-3611 two-digit country code.
-	 *
-	 * @var string
-	 */
-	public $country;
-
-	// }}}
-	// {{{ public function setDatabase()
-
-	public function setDatabase(MDB2_Driver_Common $db)
-	{
-		$this->db = $db;
-	}
-
-	// }}}
-	// {{{ protected function validate()
-
-	protected function validate()
-	{
-		// validate provstate by country
-		$sql = sprintf('select count(id) from ProvState
-			where id = %s and country = %s',
-			$this->db->quote($this->value, 'integer'),
-			$this->db->quote($this->country, 'text'));
-
-		$count = SwatDB::queryOne($this->db, $sql);
-
-		if ($count == 0) {
-			$country_title = SwatDB::queryOne($this->db,
-				sprintf('select title from Country where id = %s',
-				$this->db->quote($this->country)));
-
-			if ($country_title === null) {
-				$message_content = Store::_('The selected %s is '.
-					'not a province or state of the selected country.');
-			} else {
-				$message_content = sprintf(Store::_('The selected '.
-					'%%s is not a province or state of the selected '.
-					'country %s%s%s.'),
-					'<strong>', $country_title, '</strong>');
-			}
-
-			$message = new SwatMessage($message_content,
-				SwatMessage::ERROR);
-
-			$message->content_type = 'text/xml';
-			$this->addMessage($message);
-		}
-	}
-
-	// }}}
 }
 
 ?>
