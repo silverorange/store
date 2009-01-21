e62e162a2e38ca9b18c7a305a197f455e2af0980
Isaac Grant <isaac@silverorange.com>
2009-01-21 17:05:53 +0000

    base this on VisibleProductView
    svn commit r41745

--- sql/views/CategoryVisibleItemCountByRegionView.sql
+++ sql/views/CategoryVisibleItemCountByRegionView.sql
@@ -1,18 +1,15 @@
 create or replace view CategoryVisibleItemCountByRegionView as
 	select
 		CategoryDescendants.category,
-		ItemRegionBinding.region,
+		VisibleProductView.region,
 		count(distinct Item.sku) as item_count
-	from ItemRegionBinding
-		inner join Item on
-			Item.id = ItemRegionBinding.Item
+	from VisibleProductView
 		inner join Product on
-			Product.id = Item.product
-		inner join Catalog on
-			Catalog.id = Product.catalog
+			Product.id = VisibleProductView.product
+		inner join Item on
+			Item.product = Product.id
 		inner join CategoryProductBinding on
-			Product.id = CategoryProductBinding.product
+			VisibleProductView.product = CategoryProductBinding.product
 		inner join getCategoryDescendants(NULL) as CategoryDescendants on
 			CategoryDescendants.descendant = CategoryProductBinding.category
-	where ItemRegionBinding.enabled = true and Catalog.in_season = true
-	group by CategoryDescendants.category, ItemRegionBinding.region;
+	group by CategoryDescendants.category, VisibleProductView.region;
