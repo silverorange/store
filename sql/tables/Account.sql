8a306c531bf98321234ad651751649bafc541c28
Nathan Fredrickson <nathan@silverorange.com>
2009-02-10 15:45:02 +0000

    Fix up SQL to work with Creation package.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@42307 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/Account.sql
+++ sql/tables/Account.sql
@@ -7,6 +7,6 @@ alter table Account add default_billing_address integer
 alter table Account add default_shipping_address integer
 	references AccountAddress(id) on delete set null;
 
--- AccountPaymentMethod doesn'ts exist yet, so the constraint can't be added
--- here. It's added in AccountPaymentMethod.sql
-alter table Account add default_payment_method integer;
+alter table Account add default_payment_method integer
+	references AccountPaymentMethod(id) on delete set null;
+
--- sql/tables/AccountPaymentMethod.sql
+++ sql/tables/AccountPaymentMethod.sql
@@ -14,9 +14,3 @@ create table AccountPaymentMethod (
 );
 
 CREATE INDEX AccountPaymentMethod_account ON AccountPaymentMethod(account);
-
--- This table doesn't exist when Account is created, so add the contstraint now
--- that AccountPaymentMethod exists.
-alter table account add constraint "account_default_payment_method_fkey"
-	FOREIGN KEY (default_payment_method)
-	REFERENCES accountpaymentmethod(id) on delete set null;
