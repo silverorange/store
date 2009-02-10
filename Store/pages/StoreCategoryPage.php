silverorange/store

commit 1ae6ae38d8ef2501ac4ddff4a923b51a7fc178b2

add an initInternal, and set $this->category in it, also add conditional code around $this->products to allow subclassing
git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@42332 94490950-a86c-4493-9ccb-530f72933e47

Store/pages/StoreCategoryPage.php
@@ -46,6 +46,15 @@ class StoreCategoryPage extends StorePage
 	}
 
 	// }}}
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		$category_id = $this->path->getLast()->id;
+		$this->category = $this->queryCategory($category_id);
+	}
+
+	// }}}
 	// {{{ protected function getSelectedCategoryId()
 
 	protected function getSelectedCategoryId()
@@ -70,9 +79,6 @@ class StoreCategoryPage extends StorePage
 	{
 		parent::build();
 
-		$category_id = $this->path->getLast()->id;
-		$this->category = $this->queryCategory($category_id);
-
 		$this->layout->data->title =
 			SwatString::minimizeEntities($this->category->title);
 
@@ -95,7 +101,11 @@ class StoreCategoryPage extends StorePage
 				SwatString::minimizeEntities($this->category->description);
 		}
 
-		$this->products = $this->getProducts();
+		// subclasses may have loaded products already at this point, so avoid
+		// querying all products again
+		if ($this->products === null)
+			$this->products = $this->getProducts();
+
 		$this->buildPage();
 	}
 

commit 15651c8d960d82f30c890b4c551c5439ebbf7e2d

handle the top level category page better - i broke that case in r42332
git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@42353 94490950-a86c-4493-9ccb-530f72933e47

Store/pages/StoreCategoryPage.php
@@ -50,7 +50,7 @@ class StoreCategoryPage extends StorePage
 
 	protected function initInternal()
 	{
-		$category_id = $this->path->getLast()->id;
+		$category_id = $this->getSelectedCategoryId();
 		$this->category = $this->queryCategory($category_id);
 	}
 