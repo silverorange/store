23990ea6d685055994869ab569ee9664e0d4c1d8
Michael Gauthier <mike@silverorange.com>
2009-02-17 16:04:57 +0000

    Remove previous_attempt reference from Orders table.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@42603 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/Orders.sql
+++ sql/tables/Orders.sql
@@ -1,7 +1,6 @@
 create table Orders (
 	id serial,
 
-	previous_attempt integer, -- constraint added below
 	account integer default null references Account(id) on delete set null,
 	email varchar(255),
 	cc_email varchar(255),
@@ -33,10 +32,6 @@ create table Orders (
 
 	primary key (id)
 );
-
-ALTER TABLE Orders ADD CONSTRAINT Orders_previous_attempt_fk
-	FOREIGN KEY (previous_attempt) REFERENCES Orders(id) MATCH FULL
-	ON DELETE SET null;
 
 CREATE INDEX Orders_ad_index ON Orders(ad);
 CREATE INDEX Orders_account_index ON Orders(account);
