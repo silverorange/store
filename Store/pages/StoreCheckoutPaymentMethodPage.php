02b72a3e9f548dda9c328cb0cbf3ae93e3d9c839
Michael Gauthier <mike@silverorange.com>
2009-03-18 14:35:24 +0000

    ws and line length
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@43993 94490950-a86c-4493-9ccb-530f72933e47

--- Store/pages/StoreCheckoutPaymentMethodPage.php
+++ Store/pages/StoreCheckoutPaymentMethodPage.php
@@ -639,8 +639,10 @@ class StoreCheckoutPaymentMethodPage extends StoreCheckoutEditPage
 				 *  Note: We can't repopulate the card number entry since we
 				 *        only store the encrypted number in the dataobject.
 				 */
-				if ($order_payment_method->hasCardNumber())
-					$this->ui->getWidget('card_number')->show_blank_value = true;
+				if ($order_payment_method->hasCardNumber()) {
+					$card_number = $this->ui->getWidget('card_number');
+					$card_number->show_blank_value = true;
+				}
 
 				$this->ui->getWidget('card_verification_value')->value =
 					$order_payment_method->getCardVerificationValue();
