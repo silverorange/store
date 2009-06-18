40cd41aae650a1656b41d08868f4a24f7a24f3a1
Michael Gauthier <mike@silverorange.com>
2009-06-18 19:58:06 +0000

    Add method to get payment method by paypal token.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@46574 94490950-a86c-4493-9ccb-530f72933e47

--- Store/dataobjects/StoreOrderPaymentMethodWrapper.php
+++ Store/dataobjects/StoreOrderPaymentMethodWrapper.php
@@ -7,18 +7,35 @@ require_once 'Store/dataobjects/StoreOrderPaymentMethod.php';
  * A recordset wrapper class for StoreOrderPaymentMethod objects
  *
  * @package   Store
- * @copyright 2005-2007 silverorange
+ * @copyright 2005-2009 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreOrderPaymentMethodWrapper extends SwatDBRecordsetWrapper
 {
+	// {{{ public function getByPayPalToken()
+
+	public function getByPayPalToken($token)
+	{
+		$payment_method = null;
+
+		foreach ($this as $method) {
+			if ($method->getPayPalToken() == $token) {
+				$payment_method = $method;
+				break;
+			}
+		}
+
+		return $payment_method;
+	}
+
+	// }}}
 	// {{{ protected function init()
 
 	protected function init()
 	{
 		parent::init();
 		$this->index_field = 'id';
-		$this->row_wrapper_class = 
+		$this->row_wrapper_class =
 			SwatDBClassMap::get('StoreOrderPaymentMethod');
 	}
 
