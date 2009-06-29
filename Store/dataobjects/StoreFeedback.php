51ac6047fff098eb6acfadae51dad087bc26d6f3
Michael Gauthier <mike@silverorange.com>
2009-06-29 14:31:13 +0000

    Feedback dataobject and SQL schema.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@46825 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Store/dataobjects/StoreFeedback.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once 'Site/dataobjects/SiteComment.php';
+
+/**
+ * Freeform feedback about the website
+ *
+ * This class is extended from SiteComment. The following fields from
+ * SiteComment are not exposed in the UI by default:
+ *
+ * - link
+ *
+ * @package   Store
+ * @copyright 2009 silverorange
+ */
+class StoreFeedback extends SiteComment
+{
+	// {{{ public properties
+
+	/**
+	 * HTTP referrer that customer used to reach the site
+	 *
+	 * For example, this could contain a search referrer.
+	 *
+	 * @var string
+	 */
+	public $http_referrer;
+
+	// }}}
+	// {{{ protected function init()
+
+	protected function init()
+	{
+		parent::init();
+		$this->table = 'Feedback';
+	}
+
+	// }}}
+}
+
+?>
