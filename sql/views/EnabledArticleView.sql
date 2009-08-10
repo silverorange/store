2c1ec38edf815c817461a2d413457789ffd8cb5b
Nathan Fredrickson <nathan@silverorange.com>
2009-08-10 20:00:06 +0000

    Respect the Site package definition of "enabled" when extending to add per-region enabling in Store.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@47955 94490950-a86c-4493-9ccb-530f72933e47

--- sql/views/EnabledArticleView.sql
+++ sql/views/EnabledArticleView.sql
@@ -2,4 +2,5 @@ create or replace view EnabledArticleView as
 	select id, region
 		from Article
 			inner join ArticleRegionBinding on
-				Article.id = ArticleRegionBinding.article;
+				Article.id = ArticleRegionBinding.article
+		where Article.enabled = true;
