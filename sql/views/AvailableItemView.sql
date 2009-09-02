9a5785c929246e2e5081165b662ec7540a01631d
Isaac Grant <isaac@silverorange.com>
2009-09-02 15:14:03 +0000

    make AvailableItemView respect catalog state - i've reviewed everywhere we use this view, and it shouldn't break anything (and will fix a few places), but keep an eye out for weird behaviour and let me know if you notice anything
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@48913 94490950-a86c-4493-9ccb-530f72933e47

--- sql/views/AvailableItemView.sql
+++ sql/views/AvailableItemView.sql
@@ -1,7 +1,13 @@
--- items are available if they have at least one item enabled for a region, and have status = available
+-- items are available if they have at least one item enabled for a region, and
+-- have status = available, and they belong to a catalog that is enabled in one
+-- region.
 create or replace view AvailableItemView as
 	select Item.id as item, ItemRegionBinding.region
 		from Item
 			inner join ItemRegionBinding on Item.id = ItemRegionBinding.item
+			inner join Product on Item.product = Product.id
+			inner join Catalog on Product.catalog = Catalog.id
+			inner join CatalogRegionBinding on
+				Catalog.id = CatalogRegionBinding.catalog
 		where ItemRegionBinding.enabled = true
 			and (Item.status = 0);
