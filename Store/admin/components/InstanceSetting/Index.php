03f9358e7a6f5451d13566875b3c5b056759e8b2
Michael Gauthier <mike@silverorange.com>
2009-09-30 18:28:56 +0000

    Fix class docs.
    svn commit r50056

--- Store/admin/components/InstanceSetting/Index.php
+++ Store/admin/components/InstanceSetting/Index.php
@@ -3,6 +3,10 @@
 require_once 'Site/admin/components/InstanceSetting/Index.php';
 require_once dirname(__FILE__).'/include/StoreConfigPage.php';
 
+/**
+ * @package   Store
+ * @copyright 2009 silverorange
+ */
 class StoreInstanceSettingIndex extends SiteInstanceSettingIndex
 {
 	// {{{ protected function initConfigPages()
