550ffdf506e86df96d0313c3a756e57418f68240
Nathan Fredrickson <nathan@silverorange.com>
2009-12-03 19:09:35 +0000

    Store recently viewed stacks in cookie instead of session.
    svn commit r51592

--- Store/StoreRecentModule.php
+++ Store/StoreRecentModule.php
@@ -1,10 +1,6 @@
 <?php
 
 require_once 'Site/SiteApplicationModule.php';
-
-require_once 'Store/dataobjects/StoreItemWrapper.php';
-require_once 'Store/dataobjects/StoreProductWrapper.php';
-require_once 'Store/dataobjects/StoreCategoryWrapper.php';
 require_once 'Store/StoreRecentStack.php';
 
 /**
@@ -28,13 +24,7 @@ class StoreRecentModule extends SiteApplicationModule
 	{
 		$depends = parent::depends();
 		$depends[] = new SiteApplicationModuleDependency(
-			'SiteAccountSessionModule');
-
-		$depends[] = new SiteApplicationModuleDependency('SiteDatabaseModule');
-
-		if ($this->app->hasModule('SiteMultipleInstanceModule'))
-			$depends[] = new SiteApplicationModuleDependency(
-				'SiteMultipleInstanceModule');
+			'SiteCookieModule');
 
 		return $depends;
 	}
@@ -44,10 +34,9 @@ class StoreRecentModule extends SiteApplicationModule
 
 	public function init()
 	{
-		if ($this->app->session->isActive())
-			if (!isset($this->app->session->recent) ||
-				!($this->app->session->recent instanceof ArrayObject))
-					$this->app->session->recent = new ArrayObject();
+		if (!isset($this->app->cookie->recent) ||
+			!($this->app->cookie->recent instanceof ArrayObject))
+				$this->app->cookie->setCookie('recent', new ArrayObject());
 	}
 
 	// }}}
@@ -55,14 +44,17 @@ class StoreRecentModule extends SiteApplicationModule
 
 	public function add($stack_name, $id)
 	{
-		$this->app->session->activate();
 		$this->init();
 
-		if (!$this->app->session->recent->offsetExists($stack_name))
-			$this->app->session->recent->offsetSet($stack_name, new StoreRecentStack());
+		$stacks = $this->app->cookie->recent;
 
-		$stack = $this->app->session->recent->offsetGet($stack_name);
+		if (!$stacks->offsetExists($stack_name))
+			$stacks->offsetSet($stack_name, new StoreRecentStack());
+
+		$stack = $stacks->offsetGet($stack_name);
 		$stack->add($id);
+
+		$this->app->cookie->setCookie('recent', $stacks);
 	}
 
 	// }}}
@@ -70,9 +62,6 @@ class StoreRecentModule extends SiteApplicationModule
 
 	public function get($stack_name, $count = null)
 	{
-		if (!$this->app->session->isActive())
-			return null;
-
 		$this->init();
 
 		$exclude_id = null;
@@ -81,12 +70,18 @@ class StoreRecentModule extends SiteApplicationModule
 		if ($stack_name == 'products' && $page instanceof StoreProductPage)
 			$exclude_id = $page->product_id;
 
-		if (!$this->app->session->recent->offsetExists($stack_name))
-			$this->app->session->recent->offsetSet($stack_name, new StoreRecentStack());
+		$stacks = $this->app->cookie->recent;
 
-		$stack = $this->app->session->recent->offsetGet($stack_name);
+		if ($stacks->offsetExists($stack_name)) {
+			$stack = $this->app->cookie->recent->offsetGet($stack_name);
+			$values =  $stack->get($count, $exclude_id);
+		} else {
+			$stacks->offsetSet($stack_name, new StoreRecentStack());
+			$this->app->cookie->setCookie('recent', $stacks);
+			$values = null;
+		}
 
-		return $stack->get($count, $exclude_id);
+		return $values;
 	}
 
 	// }}}
