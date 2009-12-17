0c60b5a219a0ac092d315a6caf37c164cebf1a09
Isaac Grant <isaac@silverorange.com>
2009-12-17 18:41:32 +0000

    use SwatMoneyCellRenderer->null_display_value instead of doing all the work here.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@51833 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreMoneyCellRenderer.php
+++ Store/StoreMoneyCellRenderer.php
@@ -2,11 +2,11 @@
 
 require_once 'Swat/SwatMoneyCellRenderer.php';
 
-/** 
+/**
  * Money cell renderer that displays n/a when no value is available
  *
  * @package   Store
- * @copyright 2006 silverorange
+ * @copyright 2006-2009 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreMoneyCellRenderer extends SwatMoneyCellRenderer
@@ -17,23 +17,10 @@ class StoreMoneyCellRenderer extends SwatMoneyCellRenderer
 	{
 		parent::__construct();
 
+		$this->null_display_value = 'n/a';
+
 		$this->addStyleSheet('packages/swat/styles/swat.css',
 			Swat::PACKAGE_ID);
-	}
-
-	// }}}
-	// {{{ public function render()
-
-	public function render()
-	{
-		if ($this->value === null) {
-			$span_tag = new SwatHtmlTag('span');
-			$span_tag->class = 'swat-none';
-			$span_tag->setContent('n/a');
-			$span_tag->display();
-		} else {
-			parent::render();
-		}
 	}
 
 	// }}}
