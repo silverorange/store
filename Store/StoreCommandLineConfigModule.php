722f2f314b95e22707e24c556b27f5ff12a087af
Isaac Grant <isaac@silverorange.com>
2010-07-23 14:11:32 +0000

    this has moved to site. Keep the class around to not break older code.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@56583 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreCommandLineConfigModule.php
+++ Store/StoreCommandLineConfigModule.php
@@ -1,50 +1,20 @@
 <?php
 
-require_once 'Swat/exceptions/SwatException.php';
-require_once 'Swat/SwatError.php';
-require_once 'Site/SiteErrorLogger.php';
-require_once 'Site/SiteExceptionLogger.php';
-require_once 'Site/SiteConfigModule.php';
-require_once 'Date/TimeZone.php';
+require_once 'Site/SiteCommandLineConfigModule.php';
 
 /**
  * Configuration module for Store command-line applications
  *
- * Performs additional module configuration.
+ * Performs additional module configuration. This is currently empty because
+ * everything in it was moved up to SiteCommandLineConfigModule. The class
+ * remains for backwards compatibility with old code.
  *
  * @package   Store
- * @copyright 2007-2008 silverorange
+ * @copyright 2007-2010 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreCommandLineConfigModule extends SiteConfigModule
 {
-	// {{{ protected function configure()
-
-	/**
-	 * Configures modules of the application before they are initialized
-	 */
-	public function configure()
-	{
-		parent::configure();
-
-		if (isset($this->exceptions->log_location))
-			SwatException::setLogger(new SiteExceptionLogger(
-				$this->exceptions->log_location, $this->exceptions->base_uri));
-
-		if (isset($this->errors->log_location))
-			SwatError::setLogger(new SiteErrorLogger(
-				$this->errors->log_location, $this->errors->base_uri));
-
-		$this->app->database->dsn = $this->database->dsn;
-
-		$this->app->default_time_zone =
-			new Date_TimeZone($this->date->time_zone);
-
-		$this->app->default_locale = $this->i18n->locale;
-
-		setlocale(LC_ALL, $this->i18n->locale.'.UTF-8');
-	}
-
-	// }}}
 }
 
 ?>
