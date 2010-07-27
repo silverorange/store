0a944aaec3261f3afd890d26e2950485e4bfc6a0
Nick Burka <nick@silverorange.com>
2010-07-27 14:56:41 +0000

    Better styling of the sensitive/insensitve custom price entry
    svn commit r56607

--- www/javascript/store-gift-certificate-radio-list.js
+++ www/javascript/store-gift-certificate-radio-list.js
@@ -11,7 +11,28 @@ function StoreGiftCertificateRadioList(id, custom_value)
 
 		YAHOO.util.Event.addListener(this.custom_price, 'keyup',
 			this.handlePriceChange, this, true);
+
+		var radio_list = document.getElementsByName(this.custom_item.name);
+
+		for (var i = 0; i < radio_list.length; i++) {
+			YAHOO.util.Event.addListener(radio_list[i], 'change',
+				this.handlePriceChange, this, true);
+
+			YAHOO.util.Event.addListener(radio_list[i], 'keyup',
+				this.handlePriceChange, this, true);
+		}
+
+		YAHOO.util.Event.addListener(this.custom_price, 'click',
+			this.updateOption, this, true);
+
+		this.handlePriceChange();
 	}
+}
+
+StoreGiftCertificateRadioList.prototype.updateOption = function()
+{
+	this.custom_item.checked = true;
+	this.handlePriceChange();
 }
 
 StoreGiftCertificateRadioList.prototype.handlePriceChange = function()
@@ -19,5 +40,11 @@ StoreGiftCertificateRadioList.prototype.handlePriceChange = function()
 	if (this.old_custom_price_value != this.custom_price.value) {
 		this.custom_item.checked = true;
 		this.old_custom_price_value = this.custom_price.value;
+	}
+
+	if (this.custom_item.checked) {
+		YAHOO.util.Dom.removeClass(this.custom_price, 'swat-insensitive');
+	} else {
+		YAHOO.util.Dom.addClass(this.custom_price, 'swat-insensitive');
 	}
 };
