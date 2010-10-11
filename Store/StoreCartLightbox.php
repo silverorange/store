4081de33825f1717a0c492e9b1c0639a7f70b038
Nick Burka <nick@silverorange.com>
2010-10-11 14:05:18 +0000

    Make StoreCartLightbox a control so it can easily be instansiated in layouts without having to declare all the head entries and translations.
    svn commit r57908

---
+++ Store/StoreCartLightbox.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once 'Swat/SwatControl.php';
+require_once 'Swat/SwatString.php';
+
+/**
+ * Control to display a lightbox driven cart on the page
+ *
+ * @package   Store
+ * @copyright 2010 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class StoreCartLightbox extends SwatControl
+{
+	// {{{ public function __construct()
+
+	/**
+	 * Creates a new cart lightbox
+	 *
+	 * @param string $id a non-visible unique id for this widget.
+	 *
+	 * @see SwatWidget::__construct()
+	 */
+	public function __construct($id = null)
+	{
+		parent::__construct($id);
+
+		$yui = new SwatYUI(array('dom', 'event'));
+		$this->html_head_entry_set->addEntrySet($yui->getHtmlHeadEntrySet());
+
+		$this->addJavaScript('packages/swat/javascript/swat-z-index-manager.js',
+			Swat::PACKAGE_ID);
+
+		$this->addJavascript('packages/store/javascript/store-cart-lightbox.js',
+			Store::PACKAGE_ID);
+
+		$this->addStyleSheet('packages/store/styles/store-cart-lightbox.css',
+			Store::PACKAGE_ID);
+	}
+
+	// }}}
+	// {{{ public function display()
+
+	public function display()
+	{
+		parent::display();
+		Swat::displayInlineJavaScript($this->getInlineJavaScript());
+	}
+
+	// }}}
+	// {{{ protected function getInlineJavaScript()
+
+	protected function getInlineJavaScript()
+	{
+		static $translated = false;
+
+		$javascript = '';
+
+		if (!$translated) {
+			$javascript.= sprintf("StoreCartLightBox.empty_message = %s;\n",
+				SwatString::quoteJavaScriptString(
+					Store::_('Your Shopping Cart is Empty')));
+
+			$javascript.= sprintf("StoreCartLightBox.loading_message = %s;\n",
+				SwatString::quoteJavaScriptString(Store::_('Loading…')));
+
+			$javascript.= sprintf("StoreCartLightBox.submit_message = %s;\n",
+				SwatString::quoteJavaScriptString(Store::_('Updating Cart…')));
+
+			$translated = true;
+		}
+
+		$javascript.= 'var cart_lightbox = StoreCartLightBox.getInstance();';
+		return $javascript;
+	}
+
+	// }}}
+}
+
+?>
