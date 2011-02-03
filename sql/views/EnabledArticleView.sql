6411dc4dbdbb6da716b05f11e1b213c42992f4e3
Nathan Fredrickson <nathan@silverorange.com>
2011-02-03 21:49:54 +0000

    Add a way to optionally limit articles per instance.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@60135 94490950-a86c-4493-9ccb-530f72933e47

--- sql/views/EnabledArticleView.sql
+++ sql/views/EnabledArticleView.sql
@@ -1,5 +1,7 @@
 create or replace view EnabledArticleView as
-	select id, region
+	select id, region, instance
 		from Article
 			inner join ArticleRegionBinding on
-				Article.id = ArticleRegionBinding.article;
+				Article.id = ArticleRegionBinding.article
+			left outer join ArticleInstanceBinding on
+				Article.id = ArticleInstanceBinding.article;
--- sql/views/VisibleArticleView.sql
+++ sql/views/VisibleArticleView.sql
@@ -1,6 +1,8 @@
 create or replace view VisibleArticleView as
-	select id, region
+	select id, region, instance
 		from Article
 			inner join ArticleRegionBinding on
 				Article.id = ArticleRegionBinding.article
+			left outer join ArticleInstanceBinding on
+				Article.id = ArticleInstanceBinding.article
 		where visible = true;
