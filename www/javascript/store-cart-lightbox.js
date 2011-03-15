cf2a0a11b48533069a476447303db3398ce6cdca
Nick Burka <nick@silverorange.com>
2011-03-15 16:56:57 +0000

    Preload the background images. Fixes #6937
    svn commit r60759

--- www/javascript/store-cart-lightbox.js
+++ www/javascript/store-cart-lightbox.js
@@ -83,6 +83,13 @@ StoreCartLightBox.prototype.init = function()
 	YAHOO.util.Event.on(window, 'resize', this.handleWindowChange, this, true);
 
 	this.activateLinks();
+
+	// pre-load images
+	var preload = new Image();
+	preload.src = 'packages/store/images/mini-cart-background.png';
+
+	var preload_ie = new Image();
+	preload_ie.src = 'packages/store/images/mini-cart-background-ie.png';
 }
 
 // }}}
