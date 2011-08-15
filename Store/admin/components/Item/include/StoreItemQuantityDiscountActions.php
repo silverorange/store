86ef68587622736dbc54c1d18c00385cf18a0778
Michael Gauthier <mike@silverorange.com>
2011-08-15 15:19:39 +0000

    Update quantity discount actions to use composite widgets properly. Completes a TODO that's been in my inbox for 5 years.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@64660 94490950-a86c-4493-9ccb-530f72933e47

--- Store/admin/components/Item/include/StoreItemQuantityDiscountActions.php
+++ Store/admin/components/Item/include/StoreItemQuantityDiscountActions.php
@@ -10,38 +10,34 @@ require_once 'Swat/SwatActions.php';
  * actions and then goes back to the product details page.
  *
  * @package   Store
- * @copyright 2006 silverorange
+ * @copyright 2006-2011 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreItemQuantityDiscountActions extends SwatActions
 {
-	private $done_button;
-
 	public function getDoneButton()
 	{
-		return $this->done_button;
-	}
-
-	public function process()
-	{
-		parent::process();
-		$this->done_button->process();
+		return $this->getCompositeWidget('done_button');
 	}
 
 	protected function displayButton()
 	{
 		parent::displayButton();
-		echo str_repeat('&nbsp;', 2);
-		$this->done_button->display();
+
+		echo '&nbsp;&nbsp;';
+
+		$button = $this->getCompositeWidget('done_button');
+		$button->setFromStock('submit');
+		$button->title = Store::_('Done');
+		$button->display();
 	}
 
 	protected function createCompositeWidgets()
 	{
 		parent::createCompositeWidgets();
-		$this->done_button = new SwatButton($this->id.'_done_button');
-		$this->done_button->parent = $this;
-		$this->done_button->setFromStock('submit');
-		$this->done_button->title = Store::_('Done');
+
+		$button = new SwatButton($this->id.'_done_button');
+		$this->addCompositeWidget($button, 'done_button');
 	}
 }
 
