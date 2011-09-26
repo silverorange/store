97c0485c498ee16688b786129ad41cf930afdcd8
Isaac Grant <isaac@silverorange.com>
2011-09-26 10:55:47 +0000

    new base application for uploading produt listings to FTP servers. Needed in more places, and all this code is duplicated. Update bing and froogle to use it as well.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@65539 94490950-a86c-4493-9ccb-530f72933e47

--- Store/StoreFroogleUploader.php
+++ Store/StoreFroogleUploader.php
@@ -1,183 +1,55 @@
 <?php
 
-require_once 'Site/SiteCommandLineApplication.php';
-require_once 'Site/SiteDatabaseModule.php';
-require_once 'Site/dataobjects/SiteImage.php';
-require_once 'Store/Store.php';
+require_once 'Store/StoreProductFileFtpUploader.php';
 require_once 'Store/StoreFroogleGenerator.php';
-require_once 'Store/StoreCommandLineConfigModule.php';
 
 /**
  * Application to upload Froogle files to Google
  *
  * @package   Store
- * @copyright 2008 silverorange
+ * @copyright 2008-2011 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-abstract class StoreFroogleUploader extends SiteCommandLineApplication
+abstract class StoreFroogleUploader extends StoreProductFileFtpUploader
 {
-	// {{{ private property
+	// {{{ protected function getGenerator()
 
-	private $path    = '';
-	private $upload  = true;
-	private $display = false;
-
-	// }}}
-	// {{{ public function __construct()
-
-	public function __construct($id, $filename, $title, $documentation)
+	protected function getGenerator()
 	{
-		parent::__construct($id, $filename, $title, $documentation);
-
-		// add display argument
-		$display = new SiteCommandLineArgument(
-			array('-d', '--display'), 'setDisplay',
-			Store::_('Display the generated XML.'));
-
-		$this->addCommandLineArgument($display);
-
-		// add no-upload argument
-		$no_upload = new SiteCommandLineArgument(
-			array('-n', '--no-upload'), 'setNoUpload',
-			Store::_('Do not upload the generated XML to Google.'));
-
-		$this->addCommandLineArgument($no_upload);
+		return new StoreFroogleGenerator($this->db, $this->config);
 	}
 
 	// }}}
-	// {{{ public function setPath()
+	// {{{ protected function getFilename()
 
-	public function setPath($path)
+	protected function getFilename()
 	{
-		$this->path = $path;
+		return $this->config->froogle->filename;
 	}
 
 	// }}}
-	// {{{ public function setDisplay()
+	// {{{ protected function getFtpServer()
 
-	public function setDisplay()
+	protected function getFtpServer()
 	{
-		$this->display = true;
+		return $this->config->froogle->server;
 	}
 
 	// }}}
-	// {{{ public function setNoUpload()
+	// {{{ protected function getFtpUsername()
 
-	public function setNoUpload()
+	protected function getFtpUsername()
 	{
-		$this->upload = false;
+		return $this->config->froogle->username;
 	}
 
 	// }}}
-	// {{{ public function run()
+	// {{{ protected function getFtpPassword()
 
-	public function run()
+	protected function getFtpPassword()
 	{
-		parent::run();
-
-		if ($this->config->uri->cdn_base != '') {
-			SiteImage::$cdn_base = $this->config->uri->cdn_base;
-		}
-
-		$this->lock();
-
-		$filename = $this->config->froogle->filename;
-
-		$generator = $this->getGenerator();
-
-		$this->debug(sprintf(
-			Store::_('Generating Froogle feed for %s')."\n",
-			$this->config->site->title));
-
- 		$xml = $generator->generate();
-		$file = fopen($this->path.$filename, 'w');
-		if ($file === false) {
-			$this->terminate(sprintf(Store::_(
-				'Error writing file: %s', $this->path.$filename))."\n\n");
-		}
-
- 		fwrite($file, $xml);
-		fclose($file);
-
-		$this->debug(Store::_('done')."\n\n");
-
-		if ($this->display) {
-			$this->debug(Store::_('Generated XML:')."\n\n");
-
-			echo $xml;
-
-			$this->debug("\n\n");
-		}
-
-		if ($this->upload) {
-			$this->debug(Store::_('Logging into Froogle FTP ... '));
-
-			$ftp_connection = ftp_connect($this->config->froogle->server);
-
-			if ($ftp_connection === false) {
-				throw new SwatException('Unable to connect to FTP server: '.
-					$this->config->froogle->server);
-			}
-
-			$login_result = ftp_login($ftp_connection,
-				$this->config->froogle->username,
-				$this->config->froogle->password);
-
-			if ($ftp_connection == null || $login_result == null) {
-				$this->terminate(Store::_('failed to log in')."\n\n");
-			} else {
-				$this->debug(Store::_('done')."\n\n");
-			}
-
-			$this->debug(Store::_('Uploading Froogle file ... '));
-
-			$upload_result = ftp_put($ftp_connection, $filename,
-				$this->path.$filename, FTP_BINARY);
-
-			if (!$upload_result) {
-				$this->terminate(Store::_('failed uploading')."\n");
-			} else {
-				$this->debug(Store::_('done')."\n\n");
-			}
-
-			ftp_close($ftp_connection);
-		}
-
-		$this->unlock();
-
-		$this->debug(Store::_('All done.')."\n");
+		return $this->config->froogle->password;
 	}
-
-	// }}}
-	// {{{ protected function getDefaultModuleList()
-
-	protected function getDefaultModuleList()
-	{
-		return array(
-			'config'   => 'StoreCommandLineConfigModule',
-			'database' => 'SiteDatabaseModule',
-		);
-	}
-
-	// }}}
-	// {{{ protected function addConfigDefinitions()
-
-	/**
-	 * Adds configuration definitions to the config module of this application
-	 *
-	 * @param SiteConfigModule $config the config module of this application to
-	 *                                  witch to add the config definitions.
-	 */
-	protected function addConfigDefinitions(SiteConfigModule $config)
-	{
-		parent::addConfigDefinitions($config);
-		$config->addDefinitions(Store::getConfigDefinitions());
-	}
-
-	// }}}
-	// {{{ abstract protected function getGenerator()
-
-	abstract protected function getGenerator();
 
 	// }}}
 }
---
+++ Store/StoreProductFileFtpUploader.php
@@ -0,0 +1,208 @@
+<?php
+
+require_once 'Site/SiteCommandLineApplication.php';
+require_once 'Site/SiteDatabaseModule.php';
+require_once 'Site/dataobjects/SiteImage.php';
+require_once 'Store/Store.php';
+require_once 'Store/StoreCommandLineConfigModule.php';
+
+/**
+ * Abstract application to upload product listing files to a ftp server
+ *
+ * @package   Store
+ * @copyright 2011 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+abstract class StoreProductFileFtpUploader extends SiteCommandLineApplication
+{
+	// {{{ private property
+
+	private $path    = '';
+	private $upload  = true;
+	private $display = false;
+
+	// }}}
+	// {{{ public function __construct()
+
+	public function __construct($id, $filename, $title, $documentation)
+	{
+		parent::__construct($id, $filename, $title, $documentation);
+
+		// add display argument
+		$display = new SiteCommandLineArgument(
+			array('-d', '--display'), 'setDisplay',
+			Store::_('Display the generated file.'));
+
+		$this->addCommandLineArgument($display);
+
+		// add no-upload argument
+		$no_upload = new SiteCommandLineArgument(
+			array('-n', '--no-upload'), 'setNoUpload',
+			Store::_('Do not upload the generated file to the ftp server.'));
+
+		$this->addCommandLineArgument($no_upload);
+	}
+
+	// }}}
+	// {{{ public function setPath()
+
+	public function setPath($path)
+	{
+		$this->path = $path;
+	}
+
+	// }}}
+	// {{{ public function setDisplay()
+
+	public function setDisplay()
+	{
+		$this->display = true;
+	}
+
+	// }}}
+	// {{{ public function setNoUpload()
+
+	public function setNoUpload()
+	{
+		$this->upload = false;
+	}
+
+	// }}}
+	// {{{ public function run()
+
+	public function run()
+	{
+		parent::run();
+
+		if ($this->config->uri->cdn_base != '') {
+			SiteImage::$cdn_base = $this->config->uri->cdn_base;
+		}
+
+		$this->lock();
+
+		$filename  = $this->getFilename();
+		$generator = $this->getGenerator();
+		$filename_with_path = $this->path.$filename;
+
+		$this->debug(sprintf(
+			Store::_('Generating Product file for %s ... '),
+			$this->config->site->title));
+
+ 		$contents = $generator->generate();
+		if (file_put_contents($filename_with_path, $contents, LOCK_EX) ===
+			false) {
+			$this->terminate(sprintf(Store::_(
+				'Error writing file: %s', $filename_with_path))."\n\n");
+		}
+
+		$this->debug(Store::_('done')."\n\n");
+
+		if ($this->display) {
+			$this->debug(Store::_('Generated File:')."\n\n");
+
+			echo $contents;
+
+			$this->debug("\n\n");
+		}
+
+		if ($this->upload) {
+			$server   = $this->getFtpServer();
+			$username = $this->getFtpUsername();
+			$password = $this->getFtpPassword();
+
+			$this->debug(sprintf(Store::_('Logging into %s as %s ... ',
+				$server, $username)));
+
+			$ftp_connection = ftp_connect($server);
+
+			if ($ftp_connection === false) {
+				throw new SwatException('Unable to connect to FTP server: '.
+					$server);
+			}
+
+			$login_result = ftp_login($ftp_connection,
+				$username,
+				$password);
+
+			if ($ftp_connection == null || $login_result == null) {
+				$this->terminate(Store::_('failed to log in')."\n\n");
+			} else {
+				$this->debug(Store::_('done')."\n\n");
+			}
+
+			$this->debug(Store::_('Uploading Product file ... '));
+
+			$upload_result = ftp_put($ftp_connection, $filename,
+				$filename_with_path, FTP_BINARY);
+
+			if (!$upload_result) {
+				$this->terminate(Store::_('failed uploading')."\n");
+			} else {
+				$this->debug(Store::_('done')."\n\n");
+			}
+
+			ftp_close($ftp_connection);
+		}
+
+		$this->unlock();
+
+		$this->debug(Store::_('All done.')."\n");
+	}
+
+	// }}}
+	// {{{ abstract protected function getGenerator()
+
+	abstract protected function getGenerator();
+
+	// }}}
+	// {{{ abstract protected function getFilename()
+
+	abstract protected function getFilename();
+
+	// }}}
+	// {{{ abstract protected function getFtpServer()
+
+	abstract protected function getFtpServer();
+
+	// }}}
+	// {{{ abstract protected function getFtpUsername()
+
+	abstract protected function getFtpUsername();
+
+	// }}}
+	// {{{ abstract protected function getFtpPassword()
+
+	abstract protected function getFtpPassword();
+
+	// }}}
+
+	// boilerplate
+	// {{{ protected function getDefaultModuleList()
+
+	protected function getDefaultModuleList()
+	{
+		return array(
+			'config'   => 'StoreCommandLineConfigModule',
+			'database' => 'SiteDatabaseModule',
+		);
+	}
+
+	// }}}
+	// {{{ protected function addConfigDefinitions()
+
+	/**
+	 * Adds configuration definitions to the config module of this application
+	 *
+	 * @param SiteConfigModule $config the config module of this application to
+	 *                                  witch to add the config definitions.
+	 */
+	protected function addConfigDefinitions(SiteConfigModule $config)
+	{
+		parent::addConfigDefinitions($config);
+		$config->addDefinitions(Store::getConfigDefinitions());
+	}
+
+	// }}}
+}
+
+?>
