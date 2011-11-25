9f2aabfe7a5ee2558669b0f8fb6e1e3ac6af5e99
Michael Gauthier <mike@silverorange.com>
2011-11-25 03:44:12 +0000

    Update StoreAdminMenuView WRT Admin r67153.
    svn commit r67154

--- Store/admin/StoreAdminMenuView.php
+++ Store/admin/StoreAdminMenuView.php
@@ -1,19 +1,18 @@
 <?php
 
-require_once 'Admin/AdminMenuView.php';
-
 require_once 'Swat/SwatForm.php';
 require_once 'Swat/SwatFrame.php';
 require_once 'Swat/SwatFormField.php';
 require_once 'Swat/SwatEntry.php';
 require_once 'Swat/SwatButton.php';
 require_once 'Swat/SwatHtmlTag.php';
+require_once 'Admin/AdminMenuView.php';
 
 /**
  * An admin menu view that has an item search box at the top
  *
  * @package   Store
- * @copyright 2006-2007 silverorange
+ * @copyright 2006-2011 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreAdminMenuView extends AdminMenuView
@@ -36,14 +35,81 @@ class StoreAdminMenuView extends AdminMenuView
 	/**
 	 * Creates a new store admin menu view with an item search box at the top
 	 *
-	 * @param AdminMenuStore $store the menu store to view.
 	 * @param string $id the unique identifier of this menu-view.
 	 */
-	public function __construct(AdminMenuStore $store, $id = null)
+	public function __construct($id = null)
 	{
-		parent::__construct($store, $id);
+		parent::__construct($id);
+		$this->html_head_entry_set->addEntry(new SwatStyleSheetHtmlHeadEntry(
+			'packages/store/admin/styles/store-quick-search.css',
+			Store::PACKAGE_ID));
+	}
 
-		$this->store = $store;
+	// }}}
+	// {{{public function getForm()
+
+	/**
+	 * Gets the form containing the item search box of this menu
+	 *
+	 * @return SwatForm
+	 */
+	public function getForm()
+	{
+		return $this->getCompositeWidget('form');
+	}
+
+	// }}}
+	// {{{ public function getItemEntry()
+
+	/**
+	 * Gets the item search box of this menu
+	 *
+	 * @return SwatEntry
+	 */
+	public function getItemEntry()
+	{
+		$this->confirmCompositeWidgets();
+		return $this->item_entry;
+	}
+
+	// }}}
+	// {{{ public function display()
+
+	/**
+	 * Displays this admin menu
+	 *
+	 * The store admin menu displays an item search form on top of the menu.
+	 */
+	public function display()
+	{
+		if (!$this->visible) {
+			return;
+		}
+
+		SwatControl::display();
+
+		$menu_div = new SwatHtmlTag('div');
+		$menu_div->id = $this->id;
+		$menu_div->class = 'admin-menu';
+		$menu_div->open();
+
+		// only show if we have access to products
+		if ($this->store->getComponentByName('Product') !== null) {
+			$form = $this->getCompositeWidget('form');
+			$form->display();
+		}
+
+		$this->displayMenuContent();
+
+		$menu_div->close();
+	}
+
+	// }}}
+	// {{{ protected function createCompositeWidgets()
+
+	protected function createCompositeWidgets()
+	{
+		parent::createCompositeWidgets();
 
 		$entry = new SwatEntry('quick_search_item');
 		$entry->size = 2;
@@ -65,76 +131,7 @@ class StoreAdminMenuView extends AdminMenuView
 
 		$this->form = $form;
 
-		$this->html_head_entry_set->addEntry(new SwatStyleSheetHtmlHeadEntry(
-			'packages/store/admin/styles/store-quick-search.css',
-			Store::PACKAGE_ID));
-	}
-
-	// }}}
-	// {{{public function getForm()
-
-	/**
-	 * Gets the form containing the item search box of this menu
-	 *
-	 * @return SwatForm
-	 */
-	public function getForm()
-	{
-		return $this->form;
-	}
-
-	// }}}
-	// {{{ public function getItemEntry()
-
-	/**
-	 * Gets the item search box of this menu
-	 *
-	 * @return SwatEntry
-	 */
-	public function getItemEntry()
-	{
-		return $this->item_entry;
-	}
-
-	// }}}
-	// {{{ public function display()
-
-	/**
-	 * Displays this admin menu
-	 *
-	 * The store admin menu displays an item search form on top of the menu.
-	 */
-	public function display()
-	{
-		$this->displayShowLink();
-
-		$menu_div = new SwatHtmlTag('div');
-		$menu_div->id = $this->id;
-		$menu_div->class = 'admin-menu';
-		$menu_div->open();
-
-		$this->displayHideLink();
-
-		// only show if we have access to products
-		if ($this->store->getComponentByName('Product') !== null) {
-			$this->form->init();
-			$this->form->display();
-		}
-
-		$this->displayMenuContent();
-		Swat::displayInlineJavaScript($this->getInlineJavaScript());
-
-		$menu_div->close();
-	}
-
-	// }}}
-	// {{{ public function getHtmlHeadEntrySet()
-
-	public function getHtmlHeadEntrySet()
-	{
-		$set = parent::getHtmlHeadEntrySet();
-		$set->addEntrySet($this->form->getHtmlHeadEntrySet());
-		return $set;
+		$this->addCompositeWidget($this->form, 'form');
 	}
 
 	// }}}
