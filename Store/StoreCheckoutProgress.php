cf332e7025c5a28866f75efa10d2084f4b306bb2
Michael Gauthier <mike@silverorange.com>
2012-07-18 21:21:13 +0000

    More configurable StoreCheckoutProgress. You can now set the steps and optionally make the steps linked. Default steps are the same as berfore.
    svn commit r73132

--- Store/StoreCheckoutProgress.php
+++ Store/StoreCheckoutProgress.php
@@ -5,7 +5,7 @@ require_once 'Store/Store.php';
 
 /**
  * @package   Store
- * @copyright 2007 silverorange
+ * @copyright 2007-2012 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreCheckoutProgress extends SwatControl
@@ -17,12 +17,33 @@ class StoreCheckoutProgress extends SwatControl
 	 */
 	public $current_step = 0;
 
+	/**
+	 * @var array
+	 */
+	public $steps = array();
+
 	// }}}
 	// {{{ public function __construct()
 
 	public function __construct($id = null)
 	{
 		parent::__construct($id);
+
+		$this->steps = array(
+			'1' => array(
+				'title' => Store::_('Your Information'),
+				'link'  => null,
+			),
+			'2' => array(
+				'title' => Store::_('Review Order'),
+				'link'  => null,
+			),
+			'3' => array(
+				'title' => Store::_('Order Completed'),
+				'link'  => null,
+			),
+		);
+
 		$this->addStyleSheet(
 			'packages/store/styles/store-checkout-progress.css',
 			Store::PACKAGE_ID);
@@ -40,20 +61,40 @@ class StoreCheckoutProgress extends SwatControl
 
 		$ol_tag = new SwatHtmlTag('ol');
 		$ol_tag->id = $this->id;
-		if ($this->current_step > 0)
-			$ol_tag->class = ' store-checkout-progress-step'.$this->current_step;
 
+		if ($this->current_step > 0) {
+			$ol_tag->class = ' store-checkout-progress-step'.
+				$this->current_step;
+		}
 
 		echo '<div class="store-checkout-progress">';
 		$ol_tag->open();
 
-		foreach ($this->getSteps() as $id => $step) {
+		foreach ($this->steps as $id => $step) {
 			$li_tag = new SwatHtmlTag('li');
 			$li_tag->class = 'store-checkout-progress-step'.$id;
 			$li_tag->open();
 
-			printf('<span class="title"><span class="number">%s.</span> %s</span>',
-				$id, $step);
+			if (isset($step['link']) && $step['link'] != '') {
+				printf(
+					'<a class="title" href="%s">'.
+					'<span class="number">%s</span> '.
+					'<span class="content">%s</span>'.
+					'</a>',
+					SwatString::minimizeEntities($step['link']),
+					SwatString::minimizeEntities($id),
+					SwatString::minimizeEntities($step['title'])
+				);
+			} else {
+				printf(
+					'<span class="title">'.
+					'<span class="number">%s</span> '.
+					'<span class="content">%s</span>'.
+					'</span>',
+					SwatString::minimizeEntities($id),
+					SwatString::minimizeEntities($step['title'])
+				);
+			}
 
 			$li_tag->close();
 		}
@@ -62,18 +103,6 @@ class StoreCheckoutProgress extends SwatControl
 		echo '<div class="store-checkout-progress-clear"></div>';
 		echo '</div>';
 
-	}
-
-	// }}}
-	// {{{ private static function getSteps()
-
-	private static function getSteps()
-	{
-		return array(
-			'1' => Store::_('Your Information'),
-			'2' => Store::_('Review Order'),
-			'3' => Store::_('Order Completed'),
-		);
 	}
 
 	// }}}
