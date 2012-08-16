6c58d655da83e704732ec20227eb5907fd97b871
Michael Gauthier <mike@silverorange.com>
2012-08-16 21:17:52 +0000

    More country data to be used for better international address entry. Data for province/state field title, province/state required, city field title, city required, postal code field title. Data collected from Wikipedia and bitboost.com.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@73663 94490950-a86c-4493-9ccb-530f72933e47

--- Store/dataobjects/StoreCountry.php
+++ Store/dataobjects/StoreCountry.php
@@ -37,36 +37,226 @@ class StoreCountry extends SwatDBDataObject
 	/**
 	 * Whether or not this country has a postal code system
 	 *
-	 * Some countries, such as ithe Republic of Ireland do not.
+	 * Some countries, such as the Republic of Ireland do not.
 	 *
 	 * @var boolean
 	 */
 	public $has_postal_code;
 
 	// }}}
-	// {{{ public function getRegionSelectTitle()
+	// {{{ public function getRegionTitle()
 
-	public function getRegionSelectTitle()
+	public function getRegionTitle()
 	{
 		switch ($this->id) {
-		case 'DE':
-			$select_title = Store::_('Select a Land …');
+		case 'AU':
+		case 'MY':
+			$title = Store::_('State/Territory');
 			break;
-		case 'US':
+		case 'AR':
+		case 'BE':
+		case 'CN':
+		case 'CR':
+		case 'CZ':
+		case 'ES':
+		case 'ID':
+		case 'IT':
+		case 'KP':
+		case 'NZ':
+		case 'UY':
+		case 'VE':
+			$title = Store::_('Province');
+			break;
+		case 'AT':
+		case 'BR':
+		case 'IN':
 		case 'MX':
-			$select_title = Store::_('Select a State …');
+		case 'OM':
+		case 'US':
+			$title = Store::_('State');
 			break;
 		case 'CA':
-		case 'AU':
-			$select_title = Store::_('Select a Province …');
+			$title = Store::_('Province/Territory');
+			break;
+		case 'CL':
+			$title = Store::_('Municipality');
+			break;
+		case 'DE':
+			$title = Store::_('Land');
+			break;
+		case 'DK':
+		case 'FI':
+		case 'GL':
+			$title = Store::_('Postal District');
+			break;
+		case 'ES':
+		case 'IE':
+			$title = Store::_('County');
+			break;
+		case 'FR':
+		case 'ZA':
+			$title = Store::_('Locality');
+			break;
+		case 'IL':
+		case 'PK':
+			$title = Store::_('District');
+			break;
+		case 'LV':
+			$title = Store::_('Amalgameted Municipality');
+			break;
+		case 'PT':
+			$title = Store::_('Territory');
+			break;
+		case 'RO':
+			$title = Store::_('County/Sector');
+			break;
+		case 'TW':
+			$title = Store::_('Island');
 			break;
 		case 'UK':
+			$title = Store::_('Post Town');
+			break;
 		default:
-			$select_title = Store::_('Select a Region …');
+			$title = Store::_('Region');
 			break;
 		}
 
-		return $select_title;
+		return $title;
+	}
+
+	// }}}
+	// {{{ public function getRegionRequired()
+
+	public function getRegionRequired()
+	{
+		switch ($this->id) {
+		case 'AU':
+		case 'BR':
+		case 'CA':
+		case 'CN':
+		case 'CR':
+		case 'IT':
+		case 'JP':
+		case 'KP':
+		case 'MX':
+		case 'MY':
+		case 'RO':
+		case 'RU':
+		case 'UK':
+		case 'US':
+			$required = true;
+			break;
+		default:
+			$required = false;
+			break;
+		}
+
+		return $required;
+	}
+
+	// }}}
+	// {{{ public function getRegionVisible()
+
+	public function getRegionVisible()
+	{
+		switch ($this->id) {
+		case 'EE':
+		case 'IL':
+		case 'DE':
+		case 'FR':
+		case 'BE':
+		case 'NZ':
+		case 'CR':
+		case 'CZ':
+		case 'FJ':
+		case 'IS':
+		case 'LU':
+		case 'NL':
+		case 'NO':
+		case 'PL':
+		case 'SG':
+		case 'SE':
+		case 'AE':
+		case 'AT':
+		case 'OM':
+			$visible = false;
+			break;
+		default:
+			$visible = true;
+			break;
+		}
+
+		return $visible;
+	}
+
+	// }}}
+	// {{{ public function getPostalCodeTitle()
+
+	public function getPostalCodeTitle()
+	{
+		switch ($this->id) {
+		case 'AU':
+		case 'CN':
+		case 'IE':
+		case 'NZ':
+		case 'SG':
+		case 'UK':
+			$title = Store::_('Postcode');
+			break;
+		case 'PK':
+			$title = Store::_('Post Code');
+			break;
+		case 'US':
+			$title = Store::_('ZIP Code');
+			break;
+		default:
+			$title = Store::_('Postal Code');
+			break;
+		}
+
+		return $title;
+	}
+
+	// }}}
+	// {{{ public function getPostalCodeRequired()
+
+	public function getPostalCodeRequired()
+	{
+		return $this->has_postal_code;
+	}
+
+	// }}}
+	// {{{ public function getCityTitle()
+
+	public function getCityTitle()
+	{
+		switch ($this->id) {
+		case 'UK':
+			$title = Store::_('Locality');
+			break;
+		default:
+			$title = Store::_('City');
+			break;
+		}
+
+		return $title;
+	}
+
+	// }}}
+	// {{{ public function getCityRequired()
+
+	public function getCityRequired()
+	{
+		switch ($this->id) {
+		case 'UK':
+			$required = false;
+			break;
+		default:
+			$required = true;
+			break;
+		}
+
+		return $required;
 	}
 
 	// }}}
