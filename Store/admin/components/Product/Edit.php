silverorange/store

commit 9d9d0bb1f4f4cccce3e275a90cc8e4a37bc84535

Show the canonical category path on product details and show a warning on product edit when editing a product that belongs to multiple categories.
git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@74025 94490950-a86c-4493-9ccb-530f72933e47

Store/admin/components/Product/Edit.php
@@ -74,16 +74,37 @@ class StoreProductEdit extends AdminDBEdit
 
 	protected function initProduct()
 	{
-		$class_name = SwatDBClassMap::get('StoreProduct');
-		$this->product = new $class_name();
-		$this->product->setDatabase($this->app->db);
-
-		if ($this->id !== null) {
-			if (!$this->product->load($this->id))
-				throw new AdminNotFoundException(sprintf(
-					Store::_('A product with an id of ‘%d’ does not exist.'),
-					$this->id));
+		if ($this->id === null) {
+			throw new AdminNotFoundException(
+				Store::_('No product id specified.')
+			);
 		}
+
+		$sql = sprintf(
+			'select Product.*, ProductPrimaryCategoryView.primary_category,
+				getCategoryPath(ProductPrimaryCategoryView.primary_category)
+					as path
+			from Product
+				left outer join ProductPrimaryCategoryView
+					on Product.id = ProductPrimaryCategoryView.product
+			where id = %s',
+			$this->app->db->quote($this->id, 'integer')
+		);
+
+		$row = SwatDB::queryRow($this->app->db, $sql);
+
+		if ($row === null) {
+			throw new AdminNotFoundException(
+				sprintf(
+					Store::_('A product with an id of ‘%s’ does not exist.'),
+					$this->id
+				)
+			);
+		}
+
+		$product_class = SwatDBClassMap::get('StoreProduct');
+		$this->product = new $product_class($row);
+		$this->product->setDatabase($this->app->db);
 	}
 
 	// }}}
@@ -333,19 +354,54 @@ class StoreProductEdit extends AdminDBEdit
 
 		// orphan product warning
 		if ($this->id === null && $this->category_id === null) {
-			$message = new SwatMessage(Store::_(
-				'This product is not being created inside a category.'),
-				'warning');
+			$message = new SwatMessage(
+				Store::_(
+					'This product is not being created inside a category.'
+				),
+				'warning'
+			);
 
 			$message->secondary_content = Store::_(
-				'Though it may be possible to purchase from this product on '.
-				'the front-end, it will not be possible to browse to this '.
-				'product on the front-end.');
+				'It may be possible to purchase this product on the front-'.
+				'end (e.g. through the quick-order), but browsing to this '.
+				'product will not be possible.'
+			);
 
-			$this->ui->getWidget('orphan_note')->add($message,
-				SwatMessageDisplay::DISMISS_OFF);
+			$this->ui->getWidget('edit_warnings')->add(
+				$message,
+				SwatMessageDisplay::DISMISS_OFF
+			);
 		}
 
+		// multiple category warning
+		if (count($this->product->categories) > 1) {
+			$message = new SwatMessage(
+				SwatString::minimizeEntities(
+					Store::_(
+						'This product belongs to multiple categories.'
+					)
+				),
+				'warning'
+			);
+
+			ob_start();
+			echo '<p>';
+			echo SwatString::minimizeEntities(
+				Store::_('Changes to product content will appear in:')
+			);
+			echo '</p>';
+
+			$this->displayCategories($this->product->categories, true);
+			$message->secondary_content = ob_get_clean();
+
+			$message->content_type = 'text/xml';
+
+			$this->ui->getWidget('edit_warnings')->add(
+				$message,
+				SwatMessageDisplay::DISMISS_OFF
+			);
+		}
+		
 		$form = $this->ui->getWidget('edit_form');
 		if ($this->id === null && !$form->isProcessed())
 			$this->smartDefaultCatalog();
@@ -499,6 +555,40 @@ class StoreProductEdit extends AdminDBEdit
 		foreach ($attributes_field->replicators as $id => $title)
 			$attributes_field->getWidget('attributes', $id)->values =
 				$attribute_values;
+	}
+
+	// }}}
+	// {{{ protected function displayCategories()
+
+	protected function displayCategories(StoreCategoryWrapper $categories,
+		$display_canonical = false)
+	{
+		$primary_category = $this->product->primary_category;
+		$multiple_categories = (count($categories) > 1);
+
+		echo '<ul class="product-categories">';
+
+		foreach ($categories as $category) {
+			$navbar = new SwatNavBar();
+			$navbar->separator = ' › ';
+			$navbar->addEntries($category->getAdminNavBarEntries());
+
+			echo '<li>';
+			$navbar->display();
+
+			if ($display_canonical &&
+				$multiple_categories &&
+				$category->id === $primary_category->id) {
+				$span_tag = new SwatHtmlTag('span');
+				$span_tag->class = 'canonical-category';
+				$span_tag->setContent(Store::_('Canonical Path'));
+				$span_tag->display();
+			}
+
+			echo '</li>';
+		}
+
+		echo '</ul>';
 	}
 
 	// }}}

Store/admin/components/Product/Details.php
@@ -119,14 +119,31 @@ class StoreProductDetails extends AdminIndex
 
 	private function loadProduct()
 	{
-		$product_class = SwatDBClassMap::get('StoreProduct');
-		$product = new $product_class();
-		$product->setDatabase($this->app->db);
+		$sql = sprintf(
+			'select Product.*, ProductPrimaryCategoryView.primary_category,
+				getCategoryPath(ProductPrimaryCategoryView.primary_category)
+					as path
+			from Product
+				left outer join ProductPrimaryCategoryView
+					on Product.id = ProductPrimaryCategoryView.product
+			where id = %s',
+			$this->app->db->quote($this->id, 'integer')
+		);
 
-		if (!$product->load($this->id))
-			throw new AdminNotFoundException(sprintf(
-				Store::_('A product with an id of ‘%d’ does not exist.'),
-				$this->id));
+		$row = SwatDB::queryRow($this->app->db, $sql);
+
+		if ($row === null) {
+			throw new AdminNotFoundException(
+				sprintf(
+					Store::_('A product with an id of ‘%s’ does not exist.'),
+					$this->id
+				)
+			);
+		}
+
+		$product_class = SwatDBClassMap::get('StoreProduct');
+		$product = new $product_class($row);
+		$product->setDatabase($this->app->db);
 
 		return $product;
 	}
@@ -736,13 +753,21 @@ class StoreProductDetails extends AdminIndex
 	{
 		$ds = new SwatDetailsStore($product);
 
-		ob_start();
-		$this->displayCategories($product->categories);
-		$ds->categories = ob_get_clean();
+		if (count($product->categories) === 0) {
+			$ds->categories = null;
+		} else {
+			ob_start();
+			$this->displayCategories($product->categories, true);
+			$ds->categories = ob_get_clean();
+		}
 
-		ob_start();
-		$this->displayCategories($product->featured_categories);
-		$ds->featured_categories = ob_get_clean();
+		if (count($product->featured_categories) === 0) {
+			$ds->featured_categories = null;
+		} else {
+			ob_start();
+			$this->displayCategories($product->featured_categories);
+			$ds->featured_categories = ob_get_clean();
+		}
 
 		// format the bodytext
 		$ds->bodytext = SwatString::condense(SwatString::toXHTML(
@@ -823,6 +848,40 @@ class StoreProductDetails extends AdminIndex
 	}
 
 	// }}}
+	// {{{ protected function displayCategories()
+
+	protected function displayCategories(StoreCategoryWrapper $categories,
+		$display_canonical = false)
+	{
+		$primary_category = $this->product->primary_category;
+		$multiple_categories = (count($categories) > 1);
+
+		echo '<ul class="product-categories">';
+
+		foreach ($categories as $category) {
+			$navbar = new SwatNavBar();
+			$navbar->separator = ' › ';
+			$navbar->addEntries($category->getAdminNavBarEntries());
+
+			echo '<li>';
+			$navbar->display();
+
+			if ($display_canonical &&
+				$multiple_categories &&
+				$category->id === $primary_category->id) {
+				$span_tag = new SwatHtmlTag('span');
+				$span_tag->class = 'canonical-category';
+				$span_tag->setContent(Store::_('Canonical Path'));
+				$span_tag->display();
+			}
+
+			echo '</li>';
+		}
+
+		echo '</ul>';
+	}
+
+	// }}}
 	// {{{ private function buildProduct()
 
 	private function buildProduct()
@@ -856,10 +915,10 @@ class StoreProductDetails extends AdminIndex
 		$toolbar = $prototype_tool_link->parent;
 		$toolbar->remove($prototype_tool_link);
 
-		$category_path = null;
-		$first_category = $product->categories->getFirst();
-		if ($first_category !== null)
-			$category_path = $first_category->path.'/';
+//		$category_path = null;
+//		$first_category = $product->categories->getFirst();
+//		if ($first_category !== null)
+//			$category_path = $first_category->path.'/';
 
 		foreach ($this->regions as $region) {
 			$locale = $region->getFirstLocale();
@@ -878,14 +937,12 @@ class StoreProductDetails extends AdminIndex
 				if ($region_count > 1) {
 					$tool_link->value = $locale->getURLLocale().
 						$this->app->config->store->path.
-						$category_path.
-						$product->shortname;
+						$product->path;
 
 					$tool_link->title.= sprintf(' (%s)', $region->title);
 				} else {
 					$tool_link->value = $this->app->config->store->path.
-						$category_path.
-						$product->shortname;
+						$product->path;
 				}
 
 				// since we check the VisibleProductView for this, this will
@@ -896,34 +953,6 @@ class StoreProductDetails extends AdminIndex
 
 				$toolbar->packEnd($tool_link);
 			}
-		}
-	}
-
-	// }}}
-	// {{{ private function displayCategories()
-
-	private function displayCategories($categories)
-	{
-		if (count($categories) == 1) {
-			$category = $categories->getFirst();
-			$navbar = new SwatNavBar();
-			$navbar->addEntries($category->getAdminNavBarEntries());
-			$navbar->display();
-
-		// multiple categories, show in list
-		} elseif (count($categories) > 1) {
-			echo '<ul>';
-
-			foreach ($categories as $category) {
-				$navbar = new SwatNavBar();
-				$navbar->addEntries($category->getAdminNavBarEntries());
-
-				echo '<li>';
-				$navbar->display();
-				echo '</li>';
-			}
-
-			echo '</ul>';
 		}
 	}
 

Store/admin/components/Product/edit.xml
@@ -4,7 +4,7 @@
 	<widget class="SwatMessageDisplay" id="message_display" />
 	<widget class="SwatFrame" id="edit_frame">
 		<property name="title" translatable="yes">Product</property>
-		<widget class="SwatMessageDisplay" id="orphan_note" />
+		<widget class="SwatMessageDisplay" id="edit_warnings" />
 		<widget class="SwatForm" id="edit_form">
 			<widget class="SwatFormField">
 				<property name="title" translatable="yes">Title</property>

www/admin/styles/store-product-details-page.css
@@ -49,3 +49,24 @@ div.store-sale-discount-original-price {
 	color: #999;
 	text-decoration: line-through;
 }
+
+.product-categories li {
+	margin-bottom: 6px;
+}
+
+.product-categories li .swat-nav-bar {
+	display: inline;
+}
+
+.product-categories .canonical-category {
+	margin-left: 6px;
+	display: inline-block;
+	background: #4e9a06;
+	color: #fff;
+	padding: 2px 4px;
+	font-size: 11px;
+	text-transform: uppercase;
+	-webkit-border-radius: 2px;
+	-moz-border-radius: 2px;
+	border-radius: 2px;
+}

www/admin/styles/store-product-edit-page.css
@@ -3,3 +3,28 @@
 	margin-right: 2em;
 }
 
+#edit_warnings,
+#edit_warnings .swat-message {
+	margin-top: 0;
+}
+
+.product-categories li {
+	margin-bottom: 6px;
+}
+
+.product-categories li .swat-nav-bar {
+	display: inline;
+}
+
+.product-categories .canonical-category {
+	margin-left: 6px;
+	display: inline-block;
+	background: #4e9a06;
+	color: #fff;
+	padding: 2px 4px;
+	font-size: 11px;
+	text-transform: uppercase;
+	-webkit-border-radius: 2px;
+	-moz-border-radius: 2px;
+	border-radius: 2px;
+}

commit 9799836eb6b12f392a92fd792b875c64eb1c4fe4

Make product editing work for creating a new product again. Was broken in r74025
git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@74995 94490950-a86c-4493-9ccb-530f72933e47

Store/admin/components/Product/Edit.php
@@ -74,37 +74,38 @@ class StoreProductEdit extends AdminDBEdit
 
 	protected function initProduct()
 	{
-		if ($this->id === null) {
-			throw new AdminNotFoundException(
-				Store::_('No product id specified.')
-			);
-		}
-
-		$sql = sprintf(
-			'select Product.*, ProductPrimaryCategoryView.primary_category,
-				getCategoryPath(ProductPrimaryCategoryView.primary_category)
-					as path
-			from Product
-				left outer join ProductPrimaryCategoryView
-					on Product.id = ProductPrimaryCategoryView.product
-			where id = %s',
-			$this->app->db->quote($this->id, 'integer')
-		);
-
-		$row = SwatDB::queryRow($this->app->db, $sql);
-
-		if ($row === null) {
-			throw new AdminNotFoundException(
-				sprintf(
-					Store::_('A product with an id of ‘%s’ does not exist.'),
-					$this->id
-				)
-			);
-		}
-
 		$product_class = SwatDBClassMap::get('StoreProduct');
-		$this->product = new $product_class($row);
-		$this->product->setDatabase($this->app->db);
+
+		if ($this->id === null) {
+			$this->product = new $product_class();
+			$this->product->setDatabase($this->app->db);
+		} else {
+			$sql = sprintf(
+				'select Product.*, ProductPrimaryCategoryView.primary_category,
+					getCategoryPath(ProductPrimaryCategoryView.primary_category)
+						as path
+				from Product
+					left outer join ProductPrimaryCategoryView
+						on Product.id = ProductPrimaryCategoryView.product
+				where id = %s',
+				$this->app->db->quote($this->id, 'integer')
+			);
+
+			$row = SwatDB::queryRow($this->app->db, $sql);
+
+			if ($row === null) {
+				throw new AdminNotFoundException(
+					sprintf(
+						Store::_(
+							'A product with an id of ‘%s’ does not exist.'),
+						$this->id
+					)
+				);
+			}
+
+			$this->product = new $product_class($row);
+			$this->product->setDatabase($this->app->db);
+		}
 	}
 
 	// }}}