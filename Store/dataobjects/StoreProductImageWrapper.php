1585e1671316d34169963ad34ab6ce8fbcaaff39
Charles Waddell <charles@silverorange.com>
2012-11-29 17:09:17 +0000

    Add license.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@76827 94490950-a86c-4493-9ccb-530f72933e47

--- Store/dataobjects/StoreProductImageWrapper.php
+++ Store/dataobjects/StoreProductImageWrapper.php
@@ -7,7 +7,8 @@ require_once 'Store/dataobjects/StoreProductImage.php';
  * A recordset wrapper class for StoreProductImage objects
  *
  * @package   Store
- * @copyright 2006-2008 silverorange
+ * @copyright 2006-2012 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  * @see       StoreProductImage
  */
 class StoreProductImageWrapper extends SiteImageWrapper
