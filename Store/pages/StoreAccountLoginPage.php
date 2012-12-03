d063b057fe9bf4333de0cf03f1e5391ec5ff23c2
Michael Gauthier <mike@silverorange.com>
2012-12-03 16:15:45 +0000

    Move Store feature on account login page down to Store. Fixes a TODO.
    svn commit r76884

---
+++ Store/pages/StoreAccountLoginPage.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'Site/SiteAccountLoginPage.php';
+
+/**
+ * @package   Store
+ * @copyright 2012 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class StoreAccountLoginPage extends SiteAccountLoginPage
+{
+	// {{{ protected function postLoginProcess()
+
+	protected function postLoginProcess()
+	{
+		parent::postLoginProcess();
+
+		// Save cart on login before redirecting. If your cart is updated
+		// by the account login process this ensures it is saved.
+		if ($this->app->hasModule('StoreCartModule')) {
+			$cart = $this->app->getModule('StoreCartModule');
+			$cart->save();
+		}
+	}
+
+	// }}}
+}
+
+?>
