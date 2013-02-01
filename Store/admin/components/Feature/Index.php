silverorange/store

commit 1c3a409a67752612dc2b0266e40fa7fb3a12d49b

instance as grouping. fix change priority links sensitivity. hide region if only one
git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@77923 94490950-a86c-4493-9ccb-530f72933e47

Store/admin/components/Feature/Index.php
@@ -10,7 +10,7 @@ require_once 'Store/dataobjects/StoreFeatureWrapper.php';
  * Index page for Features
  *
  * @package   Store
- * @copyright 2010-2011 silverorange
+ * @copyright 2010-2013 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class StoreFeatureIndex extends AdminIndex
@@ -74,18 +74,49 @@ class StoreFeatureIndex extends AdminIndex
 	// }}}
 
 	// build phase
+	// {{{ protected function buildInternal()
+
+	protected function buildInternal()
+	{
+		parent::buildInternal();
+
+		$view = $this->ui->getWidget('index_view');
+
+		if ($view->hasGroup('instance')) {
+			$view->getGroup('instance')->visible =
+				($this->app->getInstance() === null &&
+					$this->app->hasModule('SiteMultipleInstanceModule'));
+		}
+
+		if ($view->hasColumn('region')) {
+			$sql = 'select count(id) from Region';
+			$region_count = SwatDB::queryOne($this->app->db, $sql);
+
+			$view->getColumn('region')->visible = ($region_count > 1);
+		}
+	}
+
+	// }}}
 	// {{{ protected function getTableModel()
 
 	protected function getTableModel(SwatView $view)
 	{
 		$instance_where = ($this->app->getInstanceId() === null) ?
-			'1 = 1': sprintf('instance = %s',
-				$this->app->db->quote($this->app->getInstanceId(), 'integer'));
+			'1 = 1':
+			sprintf(
+				'instance = %s',
+				$this->app->db->quote($this->app->getInstanceId(), 'integer')
+			);
 
-		$sql = sprintf('select * from Feature
+		$sql = sprintf(
+			'select Feature.*, Instance.title as instance_title
+			from Feature
+			left outer join instance on Instance.id = Feature.instance
 			where %s
-			order by display_slot, priority, start_date',
-			$instance_where);
+			order by instance_title nulls first, display_slot, priority,
+				start_date',
+			$instance_where
+		);
 
 		$wrapper = SwatDBClassMap::get('StoreFeatureWrapper');
 		$features = SwatDB::query($this->app->db, $sql, $wrapper);
@@ -93,38 +124,28 @@ class StoreFeatureIndex extends AdminIndex
 		$store = new SwatTableStore();
 		$counts = array();
 
-		$instance_count = SwatDB::queryOne($this->app->db,
-			'select count(id) from Instance');
-
-		if ($this->app->getInstanceId() === null && $instance_count > 0) {
-			$view = $this->ui->getWidget('index_view');
-			$view->getColumn('instance')->visible = true;
-		}
-
 		foreach ($features as $feature) {
 			$ds = new SwatDetailsStore($feature);
 
-			if (!isset($counts[$ds->display_slot]))
-				$counts[$ds->display_slot] = 0;
+			if (!isset($counts[$ds->instance->id][$ds->display_slot])) {
+				$counts[$ds->instance->id][$ds->display_slot] = 0;
+			}
 
-			$counts[$ds->display_slot]++;
+			$counts[$ds->instance->id][$ds->display_slot]++;
 
-			if ($feature->region === null)
+			if ($feature->region === null) {
 				$ds->region = 'All';
-			else
-				$ds->region = $feature->region->title;
-
-			if ($feature->instance !== null) {
-				$ds->instance_title = $feature->instance->title;
 			} else {
-				$ds->instance_title = null;
+				$ds->region = $feature->region->title;
 			}
 
 			$store->add($ds);
 		}
 
-		foreach ($store as $ds)
-			$ds->priority_sensitive = ($counts[$ds->display_slot] > 1);
+		foreach ($store as $ds) {
+			$ds->priority_sensitive =
+				($counts[$ds->instance->id][$ds->display_slot] > 1);
+		}
 
 		return $store;
 	}

Store/admin/components/Feature/index.xml
@@ -13,6 +13,12 @@
 				</widget>
 			</widget>
 			<widget class="SwatTableView" id="index_view">
+				<object class="SwatTableViewGroup" id="instance_group">
+					<property name="group_by">instance.id</property>
+					<object class="SwatTextCellRenderer" id="instance_renderer">
+						<property name="text" type="data">instance.title</property>
+					</object>
+				</object>
 				<object class="SwatTableViewGroup">
 					<property name="id">display_slot</property>
 					<property name="group_by">display_slot</property>
@@ -38,13 +44,6 @@
 						<property name="text" type="data" translatable="yes">shortname</property>
 						<property name="link">Feature/Edit?id=%s</property>
 						<property name="link_value" type="data">id</property>
-					</object>
-				</object>
-				<object class="SwatTableViewOrderableColumn" id="instance">
-					<property name="title" translatable="yes">Instance</property>
-					<property name="visible" type="boolean">false</property>
-					<object class="SwatTextCellRenderer">
-						<property name="text" type="data">instance_title</property>
 					</object>
 				</object>
 				<object class="SwatTableViewOrderableColumn" id="title">

commit a90fc050465916e3d8733cd6e9bb9634b6dd0abe

Fix instance handling for sites without multiple intances. This was broken in r77923 which assumed that $instance->id existed.
git-svn-id: file:///Users/gauthierm/svnrepo/public/store/trunk@78337 94490950-a86c-4493-9ccb-530f72933e47

Store/admin/components/Feature/Index.php
@@ -143,12 +143,18 @@ class StoreFeatureIndex extends AdminIndex
 
 		foreach ($features as $feature) {
 			$ds = new SwatDetailsStore($feature);
-
-			if (!isset($counts[$ds->instance->id][$ds->display_slot])) {
-				$counts[$ds->instance->id][$ds->display_slot] = 0;
+			$instance_id = $feature->getInternalValue('instance');
+			if ($instance_id === null) {
+				$instance_id = 0;
 			}
 
-			$counts[$ds->instance->id][$ds->display_slot]++;
+			$ds->instance_id = $instance_id;
+
+			if (!isset($counts[$instance_id][$ds->display_slot])) {
+				$counts[$instance_id][$ds->display_slot] = 0;
+			}
+
+			$counts[$instance_id][$ds->display_slot]++;
 
 			if ($feature->region === null) {
 				$ds->region = 'All';
@@ -161,7 +167,7 @@ class StoreFeatureIndex extends AdminIndex
 
 		foreach ($store as $ds) {
 			$ds->priority_sensitive =
-				($counts[$ds->instance->id][$ds->display_slot] > 1);
+				($counts[$ds->instance_id][$ds->display_slot] > 1);
 		}
 
 		return $store;