36e08df57aae7a830fc19aa0ba9c9c4a369b0441
Keith Burgoyne <keith@silverorange.com>
2013-07-24 16:19:04 -0300

    prepare for release of 2.0.2 | package party

--- package.php
+++ package.php
@@ -2,7 +2,7 @@
 
 require_once 'PEAR/PackageFileManager2.php';
 
-$version = '2.0.1';
+$version = '2.0.2';
 $notes = <<<EOT
 see ChangeLog
 EOT;
@@ -19,7 +19,7 @@ PEAR::setErrorHandling(PEAR_ERROR_DIE);
 
 $result = $package->setOptions(
 	array(
-		'filelistgenerator' => 'svn',
+		'filelistgenerator' => 'file',
 		'simpleoutput'      => true,
 		'baseinstalldir'    => '/',
 		'packagedirectory'  => './',
