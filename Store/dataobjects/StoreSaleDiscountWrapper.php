e5a3aab18c7c6ab97c5e8886f7484528647edbca
Nick Burka <nick@silverorange.com>
2016-02-11 14:11:27 -0400

    Add sale-discount wrapper

---
+++ Store/dataobjects/StoreSaleDiscountWrapper.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'SwatDB/SwatDBRecordsetWrapper.php';
+require_once 'Store/dataobjects/StoreSaleDiscount.php';
+
+/**
+ * A recordset wrapper class for StoreSaleDiscount objects
+ *
+ * @package   Store
+ * @copyright 2016 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @see       StoreSaleDiscount
+ */
+class StoreSaleDiscountWrapper extends SwatDBRecordsetWrapper
+{
+	// {{{ protected function init()
+
+	protected function init()
+	{
+		parent::init();
+		$this->index_field = 'id';
+		$this->row_wrapper_class = SwatDBClassMap::get('StoreSaleDiscount');
+	}
+
+	// }}}
+}
+
+?>
