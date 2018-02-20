14acd6715efd8492597c7a4859a1e13d588a2f34
Nick Burka <nick@silverorange.com>
2018-02-20 17:12:26 -0400

    handle more intl variations

--- www/javascript/store-checkout-address-page.js
+++ www/javascript/store-checkout-address-page.js
@@ -98,6 +98,21 @@ StoreCheckoutAddressPage.prototype.initAutoComplete = function(prefix)
 		document.getElementById(prefix + name).value = value;
 	}
 
+	function getEstablishment(place) {
+		var establishment = '';
+
+		for (var i = 0; i < place.types.length; i++) {
+			if (place.name &&
+				place.types[i] === 'establishment' ||
+				place.types[i] === 'point_of_interest') {
+				establishment = place.name;
+				break;
+			}
+		}
+
+		return establishment;
+	}
+
 	function fillInAddress() {
 		var place = autocomplete.getPlace();
 		var components = place.address_components;
@@ -108,8 +123,10 @@ StoreCheckoutAddressPage.prototype.initAutoComplete = function(prefix)
 		var parts = {};
 		for (var i = 0; i < place.address_components.length; i++) {
 			var addressType = place.address_components[i].types[0];
-			parts[addressType] = place.address_components[i].short_name;i
+			parts[addressType] = place.address_components[i].short_name;
 		}
+
+		console.log(parts);
 
 		var line1 = '';
 		if (parts.route) {
@@ -122,8 +139,9 @@ StoreCheckoutAddressPage.prototype.initAutoComplete = function(prefix)
 			setValue('address_line1', line1);
 		}
 
-		if (place.name && place.name != line1) {
-			setValue('address_line2', place.name);
+		var establishment = getEstablishment(place);
+		if (establishment) {
+			setValue('address_line2', establishment);
 		} else {
 			setValue('address_line2', '');
 		}
@@ -133,6 +151,9 @@ StoreCheckoutAddressPage.prototype.initAutoComplete = function(prefix)
 		} else if (parts.sublocality_level_1) {
 			// Brooklyn, NYC doesn't use parts.locality
 			setValue('address_city', parts.sublocality_level_1);
+		} else if (parts.administrative_area_level_1) {
+			// Istanbul uses administrative_area_level_1
+			setValue('address_city', parts.administrative_area_level_1);
 		} else {
 			setValue('address_city', '');
 		}
