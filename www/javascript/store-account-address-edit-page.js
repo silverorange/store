728bdd0b69b4cdba93465d4cc9ded3d1196a4b6c
Nick Burka <nick@silverorange.com>
2018-02-23 10:25:08 -0400

    fix field names

--- www/javascript/store-account-address-edit-page.js
+++ www/javascript/store-account-address-edit-page.js
@@ -2,8 +2,8 @@ YAHOO.util.Event.onDOMReady(function() {
 	new StoreGoogleAddressAutoComplete(
 		'address_line1',
 		{
-			line1: 'address_line1',
-			line2: 'address_line2',
+			line1: 'line1',
+			line2: 'line2',
 			city: 'city',
 			postal_code: 'postal_code',
 			provstate_entry: 'provstate_entry',
