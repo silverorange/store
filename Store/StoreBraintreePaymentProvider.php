f193685898e22813e1124d6b7929316a0d98f5c8
Charles Waddell <charles@silverorange.com>
2018-04-10 13:25:06 -0300

    Display the whole contents of the response

--- Store/StoreBraintreePaymentProvider.php
+++ Store/StoreBraintreePaymentProvider.php
@@ -485,7 +485,7 @@ class StoreBraintreePaymentProvider extends StorePaymentProvider
 			return $e;
 		}
 
-		return new StorePaymentBraintreeException($response->message);
+		return new StorePaymentBraintreeException(print_r($response, true));
 	}
 
 	// }}}
